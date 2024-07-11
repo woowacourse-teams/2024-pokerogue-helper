1c9774cbd38fcf532150ccadbe1f5b5d3baefa4f
murjune <sis9221302@gmail.com>
2024-07-11 13:39:14 +0900

    build: gradle 버전 관리

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,20 +1,40 @@
+import org.jetbrains.kotlin.konan.properties.Properties
+
 plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.kotlinx.serialization)
+    alias(libs.plugins.android.junit5)
+    alias(libs.plugins.ktlint)
+    id("kotlin-parcelize")
 }
 
+val properties =
+    Properties().apply {
+        load(rootProject.file("local.properties").inputStream())
+    }
+
 android {
-    namespace = "poke.rogue.helper"
-    compileSdk = 34
+    namespace = libs.versions.applicationId.get()
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
-        applicationId = "poke.rogue.helper"
-        minSdk = 26
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
+        applicationId = libs.versions.applicationId.get()
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        versionName = libs.versions.appVersion.get()
+        versionCode = libs.versions.versionCode.get().toInt()
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunnerArguments["runnerBuilder"] =
+            "de.mannodermaus.junit5.AndroidJUnit5Builder"
+
+// TODO 서버 URL 나오면 설정
+//        buildConfigField(
+//            "String",
+//            "SHOPPING_BASE_URL",
+//            properties.getProperty("SHOPPING_BASE_URL"),
+//        )
     }
 
     buildTypes {
@@ -27,22 +47,52 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+    packaging {
+        resources {
+            excludes += "META-INF/**"
+            excludes += "win32-x86*/**"
+        }
+    }
+    buildFeatures {
+        buildConfig = true
+        dataBinding = true
+    }
+    testOptions {
+        animationsDisabled = true
     }
 }
 
 dependencies {
-
+    // androidx
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.startup)
+    implementation(libs.androidx.lifecycle)
+    implementation(libs.androidx.lifecycle.viewmodel)
+    implementation(libs.kotlin.coroutines.android)
+    // retrofit & okhttp
+    implementation(libs.bundles.retrofit)
+    implementation(platform(libs.okhttp.bom))
+    implementation(libs.okhttp.logging.interceptor)
+    // third party
+    implementation(libs.kotlin.serialization.json)
+    implementation(libs.timber)
+    implementation(libs.coil.core)
+    implementation(libs.glide)
+    implementation(libs.splash.screen)
+    // unit test
+    testImplementation(libs.bundles.unit.test)
+    testImplementation(libs.mockk.webserver)
+    // android test
+    androidTestImplementation(libs.bundles.android.test)
+    androidTestRuntimeOnly(libs.junit5.android.test.runner)
 }
