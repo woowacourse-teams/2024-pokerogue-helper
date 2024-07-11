e6c4655837177b351ad9825c0f16a3eed0de3b1f
JoYehyun <jo990714@naver.com>
2024-07-11 13:21:52 +0900

    ui: 타입 선택 바텀 시트 xml 구현

---
+++ android/app/src/main/java/poke/rogue/helper/TypeChoiceBottomSheetFragment.kt
@@ -0,0 +1,21 @@
+package poke.rogue.helper
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class TypeChoiceBottomSheetFragment : BottomSheetDialogFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_type_choice_bottom_sheet, container, false)
+    }
+}
