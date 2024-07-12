280dc96e7b3fed8f80deb37731da824c9357cae0
murjune <sis9221302@gmail.com>
2024-07-12 17:34:07 +0900

    #6 초기 세팅
    build: rest-assured 의존성 추가
    
    chore: 중복된 gitignore 삭제
    
    pr 템플릿
    
    android pr builder
    docs: project 단 gitignore
    
    build: gradle 버전 관리
    
    feat: PokeRogueHelperApp
    
    chore: Manifest INTERNET 설정
    
    chore: Packaging base
    
    utils: 프로젝트 ui 에서 사용할 util 추가
    
    utils: test util
    
    style: ktFormat
    
    fix: 깃 충돌 해결
    
    fix: PR Builder 수정
    
    delete: assertj 삭제
    
    build: room 설정 및 glide-compiler
    
    refactor: 코드리뷰 반영
    
    - GridSpacingItemDecoration 주석 삭제
    - RecyclerViewItemCountAssertion 주석 삭제
    
    refactor: withItemCount, performClickHolderAt 함수 RecyclerViewUtil 로 이동
    
    - 기존 ViewInteractonExtensions
    
    refactor: getDeviceSize -> deviceSize로 네이밍 수정
    
    refactor: getDescription 에 View Id 추가
    
    refactor: matchDescendantWithText 로 네이밍 수정
    
    refactor: CommonBindingAdapter 로 네이밍 수정
    
    build: FlexboxLayout 추가

---
+++ android/app/src/main/java/poke/rogue/helper/presentation/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package poke.rogue.helper.presentation.home
+
+import android.os.Bundle
+import poke.rogue.helper.R
+import poke.rogue.helper.databinding.ActivityHomeBinding
+import poke.rogue.helper.presentation.base.BindingActivity
+
+class HomeActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
