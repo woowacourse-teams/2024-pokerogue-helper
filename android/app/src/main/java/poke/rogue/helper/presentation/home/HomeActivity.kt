080725e68794a7884fd863d6297422df8864c00c
JoYehyun <jo990714@naver.com>
2024-07-13 01:23:12 +0900

    feat: 상성 화면으로 이동 로직(임시용) 구현
    - 상성 화면 확인을 위한 작업

--- android/app/src/main/java/poke/rogue/helper/presentation/home/HomeActivity.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/home/HomeActivity.kt
@@ -1,12 +1,19 @@
 package poke.rogue.helper.presentation.home
 
+import android.content.Intent
 import android.os.Bundle
 import poke.rogue.helper.R
 import poke.rogue.helper.databinding.ActivityHomeBinding
 import poke.rogue.helper.presentation.base.BindingActivity
+import poke.rogue.helper.presentation.type.TypeActivity
 
 class HomeActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.tvTmp.setOnClickListener {
+            Intent(this, TypeActivity::class.java).apply {
+                startActivity(this)
+            }
+        }
     }
 }
