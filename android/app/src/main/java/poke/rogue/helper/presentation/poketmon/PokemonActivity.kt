1f375d13214d38e740cd74518047c6c7266ba48c
sh1mj1 <7wlgns@gmail.com>
2024-07-12 20:36:33 +0900

    ui: 포켓몬 도감에 쓰이는 액티비티 파일을 생성한다

---
+++ android/app/src/main/java/poke/rogue/helper/presentation/poketmon/PokemonActivity.kt
@@ -0,0 +1,12 @@
+package poke.rogue.helper.presentation.poketmon
+
+import android.os.Bundle
+import poke.rogue.helper.R
+import poke.rogue.helper.databinding.ActivityPokemonBinding
+import poke.rogue.helper.presentation.base.BindingActivity
+
+class PokemonActivity : BindingActivity<ActivityPokemonBinding>(R.layout.activity_pokemon) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
