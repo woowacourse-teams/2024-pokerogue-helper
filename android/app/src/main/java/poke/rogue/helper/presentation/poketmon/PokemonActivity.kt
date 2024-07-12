987abf02076c253f9b618e2096af5a427e01ef4c
sh1mj1 <7wlgns@gmail.com>
2024-07-12 20:57:29 +0900

    ui: 포켓몬 도감 목록 프래그먼트 파일을 생성하여 연결한다

--- android/app/src/main/java/poke/rogue/helper/presentation/poketmon/PokemonActivity.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/poketmon/PokemonActivity.kt
@@ -1,6 +1,8 @@
 package poke.rogue.helper.presentation.poketmon
 
 import android.os.Bundle
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import poke.rogue.helper.R
 import poke.rogue.helper.databinding.ActivityPokemonBinding
 import poke.rogue.helper.presentation.base.BindingActivity
@@ -8,5 +10,13 @@ import poke.rogue.helper.presentation.base.BindingActivity
 class PokemonActivity : BindingActivity<ActivityPokemonBinding>(R.layout.activity_pokemon) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                replace<PokemonListFragment>(
+                    R.id.fragment_container_pokemon,
+                )
+            }
+        }
     }
 }
---
+++ android/app/src/main/java/poke/rogue/helper/presentation/poketmon/PokemonListFragment.kt
@@ -0,0 +1,7 @@
+package poke.rogue.helper.presentation.poketmon
+
+import poke.rogue.helper.R
+import poke.rogue.helper.databinding.FragmentPokemonListBinding
+import poke.rogue.helper.presentation.base.BindingFragment
+
+class PokemonListFragment : BindingFragment<FragmentPokemonListBinding>(R.layout.fragment_pokemon_list)
