a3ce3bc9ab1f33771fce3784370a53c7aaec8606
sh1mj1 <7wlgns@gmail.com>
2024-07-13 23:25:28 +0900

    ui: 포켓몬 목록 프래그먼트에 리사이클러뷰 생성해서 연결

--- android/app/src/main/java/poke/rogue/helper/presentation/poketmon/PokemonListFragment.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/poketmon/PokemonListFragment.kt
@@ -1,7 +1,33 @@
 package poke.rogue.helper.presentation.poketmon
 
+import android.os.Bundle
+import android.view.View
 import poke.rogue.helper.R
 import poke.rogue.helper.databinding.FragmentPokemonListBinding
 import poke.rogue.helper.presentation.base.BindingFragment
 
-class PokemonListFragment : BindingFragment<FragmentPokemonListBinding>(R.layout.fragment_pokemon_list)
+class PokemonListFragment : BindingFragment<FragmentPokemonListBinding>(R.layout.fragment_pokemon_list) {
+    private val pokemonAdapter by lazy { PokemonAdapter() }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        initAdapter()
+        initDummyPokemons()
+    }
+
+    // TODO: remove null-assertion
+    private fun initAdapter() {
+        binding?.apply {
+            rvPokemonList.adapter = pokemonAdapter
+        }
+    }
+
+    private fun initDummyPokemons() {
+        pokemonAdapter.submitList(
+            List(10) { PokemonUiModel.DUMMY },
+        )
+    }
+}
