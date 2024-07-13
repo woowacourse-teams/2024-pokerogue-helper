50125964f66cbedc867047adc36d9576e40d319d
sh1mj1 <7wlgns@gmail.com>
2024-07-13 23:33:24 +0900

    ui: 포켓몬 목록 리사이클러뷰 어댑터에 데코레이터를 추가한다

--- android/app/src/main/java/poke/rogue/helper/presentation/poketmon/PokemonListFragment.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/poketmon/PokemonListFragment.kt
@@ -2,9 +2,11 @@ package poke.rogue.helper.presentation.poketmon
 
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
 import poke.rogue.helper.R
 import poke.rogue.helper.databinding.FragmentPokemonListBinding
 import poke.rogue.helper.presentation.base.BindingFragment
+import poke.rogue.helper.presentation.util.view.GridSpacingItemDecoration
 
 class PokemonListFragment : BindingFragment<FragmentPokemonListBinding>(R.layout.fragment_pokemon_list) {
     private val pokemonAdapter by lazy { PokemonAdapter() }
@@ -18,10 +20,18 @@ class PokemonListFragment : BindingFragment<FragmentPokemonListBinding>(R.layout
         initDummyPokemons()
     }
 
-    // TODO: remove null-assertion
     private fun initAdapter() {
         binding?.apply {
-            rvPokemonList.adapter = pokemonAdapter
+            // TODO: 화면 사이즈에 대응해서 spanCount 와 spacing 조정 여부
+            val spanCount = 3
+            val spacing = 7
+            val includeEdge = false
+
+            rvPokemonList.apply {
+                adapter = pokemonAdapter
+                layoutManager = GridLayoutManager(context, spanCount)
+                addItemDecoration(GridSpacingItemDecoration(spanCount, spacing, includeEdge))
+            }
         }
     }
 
