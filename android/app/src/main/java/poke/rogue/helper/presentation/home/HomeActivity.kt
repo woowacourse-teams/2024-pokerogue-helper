1cf0986908b1ec12902e05738e1b7a3648e43130
kkosang <eiwoqp77@gmail.com>
2024-07-14 03:11:01 +0900

    ui: toolbar 구현

--- android/app/src/main/java/poke/rogue/helper/presentation/home/HomeActivity.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/home/HomeActivity.kt
@@ -1,12 +1,44 @@
 package poke.rogue.helper.presentation.home
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import poke.rogue.helper.R
 import poke.rogue.helper.databinding.ActivityHomeBinding
 import poke.rogue.helper.presentation.base.BindingActivity
+import poke.rogue.helper.presentation.util.context.stringOf
+import poke.rogue.helper.presentation.util.context.toast
 
 class HomeActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setSupportActionBar(binding.toolbarHome.toolbar)
+        supportActionBar?.let { actionBar ->
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setDisplayShowTitleEnabled(false)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.it_toolbar_pokerogue -> {
+                toast(stringOf(R.string.toolbar_pokerogue))
+                val intent =
+                    Intent(Intent.ACTION_VIEW, Uri.parse(stringOf(R.string.home_pokerogue_url)))
+                startActivity(intent)
+            }
+
+            R.id.it_toolbar_feedback -> {
+                toast(stringOf(R.string.toolbar_feedback))
+            }
+        }
+        return true
     }
 }
