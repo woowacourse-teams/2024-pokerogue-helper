8544d013cdce813410c13d2b64a9de70aa91b54c
sh1mj1 <7wlgns@gmail.com>
2024-07-13 23:07:45 +0900

    refactor: PokemonUiModel 의 더미 이미지 url 변경

--- android/app/src/main/java/poke/rogue/helper/presentation/poketmon/PokemonUiModel.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/poketmon/PokemonUiModel.kt
@@ -9,14 +9,14 @@ data class PokemonUiModel(
     companion object {
         private const val DUMMY_POKEMON_NAME = "피카츄"
         private const val DUMMY_POKEMON_IMAGE_URL =
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
 
         val DUMMY =
             PokemonUiModel(
                 id = -1,
                 name = DUMMY_POKEMON_NAME,
                 imageUrl = DUMMY_POKEMON_IMAGE_URL,
-                type = listOf("전기", "불"),
+                type = listOf("전기", "노말"),
             )
     }
 }
