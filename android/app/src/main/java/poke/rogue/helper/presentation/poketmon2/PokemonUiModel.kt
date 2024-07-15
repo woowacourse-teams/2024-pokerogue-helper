2c35c4fedb0977633ba2f5620cd2492551fc057b
murjune <sis9221302@gmail.com>
2024-07-15 16:40:51 +0900

    심지 커밋 스틸
    ui: 포켓몬 도감 목록 프래그먼트 파일을 생성하여 연결한다
    
    chore: 타입에 임시로 사용할 긴 이미지 추가
    
    ui: 포켓몬 목록 화면의 item 을 보여준다
    
    - 일단 타입이 두 가지가 있다고 가정하고 화면을 그린다
    
    chore: 타입에 임시로 사용할 작은 동그란 이미지 추가
    
    refactor: PokemonUiModel 의 더미 이미지 url 변경
    
    ui: 포켓몬 도감 목록 화면의 item 의 배경, 텍스트 색상을 변경한다
    
    ui: 포켓몬 목록 프래그먼트에 리사이클러뷰 생성해서 연결
    
    ui: 포켓몬 목록 리사이클러뷰 어댑터에 데코레이터를 추가한다
    
    ui: 포켓몬 목록 프래그먼트에 서치 바를 추가한다
    
    chore: 사용하지 않는 타입 아이콘 제거, 문자열 리소스로 변경

---
+++ android/app/src/main/java/poke/rogue/helper/presentation/poketmon2/PokemonUiModel.kt
@@ -0,0 +1,22 @@
+package poke.rogue.helper.presentation.poketmon2
+
+data class PokemonUiModel(
+    val id: Int,
+    val name: String,
+    val imageUrl: String,
+    val type: List<String>,
+) {
+    companion object {
+        private const val DUMMY_POKEMON_NAME = "피카츄"
+        private const val DUMMY_POKEMON_IMAGE_URL =
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
+
+        val DUMMY =
+            PokemonUiModel(
+                id = -1,
+                name = DUMMY_POKEMON_NAME,
+                imageUrl = DUMMY_POKEMON_IMAGE_URL,
+                type = listOf("전기", "노말"),
+            )
+    }
+}
