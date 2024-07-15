d3c333e0d5a57b39e20585fa0966ae6690da4452
SangHyun Ko <87695921+kkosang@users.noreply.github.com>
2024-07-15 16:21:26 +0900

    [AN-UI] 홈 화면 UI 구현 (#11)
    * ui: toolbar와 관련된 drawable 추가
    
    * ui: toolbar xml 작성
    
    * ui: toolbar 구현
    
    * ui: home과 관련된 drawable 추가
    
    * ui: home xml 작성
    
    * feat: 메뉴 클릭시 화면 이동
    
    * chore: xml id 컨벤션
    
    * chore: 코드리뷰 반영
    
    * ui: home drawable 추가
    
    * ui: 스타일 적용
    
    ---------
    
    Co-authored-by: murjune <sis9221302@gmail.com>

--- android/app/src/main/java/poke/rogue/helper/presentation/home/HomeActivity.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/home/HomeActivity.kt
@@ -1,19 +1,57 @@
 package poke.rogue.helper.presentation.home
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import poke.rogue.helper.R
 import poke.rogue.helper.databinding.ActivityHomeBinding
 import poke.rogue.helper.presentation.base.BindingActivity
-import poke.rogue.helper.presentation.type.TypeActivity
+import poke.rogue.helper.presentation.util.context.stringOf
+import poke.rogue.helper.presentation.util.context.toast
 
 class HomeActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding.tvTmp.setOnClickListener {
-            Intent(this, TypeActivity::class.java).apply {
-                startActivity(this)
+        setSupportActionBar(binding.toolbarHome.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        initClickListeners()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_toolbar_pokerogue -> {
+                navigateToPokeRogue()
+            }
+
+            R.id.item_toolbar_feedback -> {
+                toast(R.string.toolbar_feedback)
             }
         }
+        return true
+    }
+
+    private fun initClickListeners() {
+        binding.apply {
+            ibtnHomeLogo.setOnClickListener { navigateToPokeRogue() }
+            cvHomeType.setOnClickListener { toast("상성 페이지로 이동") }
+            cvHomeDex.setOnClickListener { toast("도감 페이지로 이동") }
+            /*  cvHomeAbility.setOnClickListener { toast("특성 페이지로 이동") }
+              cvHomeTip.setOnClickListener { toast("꿀팁 페이지로 이동") }*/
+        }
+    }
+
+    private fun navigateToPokeRogue() {
+        toast(R.string.toolbar_pokerogue)
+        val intent =
+            Intent(Intent.ACTION_VIEW, Uri.parse(stringOf(R.string.home_pokerogue_url)))
+        startActivity(intent)
     }
 }
