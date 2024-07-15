b50e140d534e7eee8534563f04ce5b01a3c63a5f
murjune <sis9221302@gmail.com>
2024-07-15 17:28:20 +0900

    ui: toolbar

---
+++ android/app/src/main/java/poke/rogue/helper/presentation/poketmon2/PokemonActivity.kt
@@ -0,0 +1,62 @@
+package poke.rogue.helper.presentation.poketmon2
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import poke.rogue.helper.R
+import poke.rogue.helper.databinding.ActivityPokemonBinding
+import poke.rogue.helper.presentation.base.BindingActivity
+import poke.rogue.helper.presentation.util.context.drawableOf
+import poke.rogue.helper.presentation.util.context.stringOf
+import poke.rogue.helper.presentation.util.context.toast
+
+class PokemonActivity : BindingActivity<ActivityPokemonBinding>(R.layout.activity_pokemon) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initViews()
+//        if (savedInstanceState == null) {
+//            supportFragmentManager.commit {
+//                replace<PokemonListFragment>(R.id.fragment_container_pokemon)
+//            }
+//        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_toolbar_pokerogue -> {
+                navigateToPokeRogue()
+            }
+
+            R.id.item_toolbar_feedback -> {
+                toast(R.string.toolbar_feedback)
+            }
+
+            android.R.id.home -> {
+                toast("뒤로가기")
+                finish()
+            }
+        }
+        return true
+    }
+
+    private fun initViews() =
+        with(binding) {
+            setSupportActionBar(toolbarHome.toolbar)
+            toolbarHome.toolbar.overflowIcon = drawableOf(R.drawable.ic_menu)
+            supportActionBar?.setDisplayShowTitleEnabled(false)
+        }
+
+    private fun navigateToPokeRogue() {
+        toast(R.string.toolbar_pokerogue)
+        val intent =
+            Intent(Intent.ACTION_VIEW, Uri.parse(stringOf(R.string.home_pokerogue_url)))
+        startActivity(intent)
+    }
+}
