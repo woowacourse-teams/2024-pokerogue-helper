3dfea73c96d12a45a5503113a51d0764889a1555
JoYehyun <jo990714@naver.com>
2024-07-15 15:25:06 +0900

    feat: 상성 결과 색상 설정 기능

--- android/app/src/main/java/poke/rogue/helper/presentation/type/mapper/TypeMatchedResultMapper.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/type/mapper/TypeMatchedResultMapper.kt
@@ -1,5 +1,6 @@
 package poke.rogue.helper.presentation.type.mapper
 
+import poke.rogue.helper.R
 import poke.rogue.helper.data.model.MatchedResult
 
 fun MatchedResult.displayName(): String {
@@ -7,6 +8,15 @@ fun MatchedResult.displayName(): String {
         MatchedResult.STRONG -> "강한"
         MatchedResult.WEAK -> "약한"
         MatchedResult.INEFFECTIVE -> "무효한"
-        MatchedResult.NORMAL -> "일반"
+        MatchedResult.NORMAL -> throw IllegalStateException("")
+    }
+}
+
+fun MatchedResult.displayColor(): Int {
+    return when (this) {
+        MatchedResult.STRONG -> R.color.white
+        MatchedResult.WEAK -> R.color.white
+        MatchedResult.INEFFECTIVE -> R.color.white
+        MatchedResult.NORMAL -> throw IllegalStateException("")
     }
 }
