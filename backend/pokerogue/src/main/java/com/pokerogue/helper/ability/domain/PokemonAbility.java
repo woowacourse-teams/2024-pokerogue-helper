59944931805274b4cc3bb77c2cd060cdf6ceca0a
Eugene Jang <kyunellroll@gmail.com>
2024-07-15 15:20:58 +0900

    feat: PokemonAbilityMapping 추가

--- backend/pokerogue/src/main/java/com/pokerogue/helper/ability/domain/PokemonAbility.java
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/ability/domain/PokemonAbility.java
@@ -1,11 +1,15 @@
 package com.pokerogue.helper.ability.domain;
 
+import com.pokerogue.helper.pokemon.domain.PokemonAbilityMapping;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,4 +29,7 @@ public class PokemonAbility {
 
     @Column(name = "short_description", nullable = false)
     private String shortDescription;
+
+    @OneToMany(mappedBy = "pokemon_ability", fetch = FetchType.LAZY)
+    private List<PokemonAbilityMapping> pokemonAbilityMapping;
 }
--- backend/pokerogue/src/main/java/com/pokerogue/helper/pokemon/domain/PokemonAbilityMapping.java
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/pokemon/domain/PokemonAbilityMapping.java
@@ -1,4 +1,32 @@
 package com.pokerogue.helper.pokemon.domain;
 
+import com.pokerogue.helper.ability.domain.PokemonAbility;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "pokemon_ability_mapping")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PokemonAbilityMapping {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "pokemon_id", nullable = false)
+    private Pokemon pokemon;
+
+    @ManyToOne
+    @JoinColumn(name = "pokemon_ability_id", nullable = false)
+    private PokemonAbility pokemonAbility;
 }
