2949b1b0b46a87dc6351383eb221f187aab4a894
JoYehyun <jo990714@naver.com>
2024-07-16 11:38:43 +0900

    feat: 타입 선택 바텀 시트 데이터 및 이벤트 핸들러 연결

--- android/app/src/main/java/poke/rogue/helper/presentation/type/typeselection/TypeSelectionBottomSheetFragment.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/type/typeselection/TypeSelectionBottomSheetFragment.kt
@@ -1,12 +1,17 @@
 package poke.rogue.helper.presentation.type.typeselection
 
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import poke.rogue.helper.databinding.FragmentTypeChoiceBottomSheetBinding
 import poke.rogue.helper.local.DummyTypeData
+import poke.rogue.helper.presentation.type.TypeViewModel
+import poke.rogue.helper.presentation.type.model.SelectorType
 import poke.rogue.helper.presentation.type.model.TypeUiModel.Companion.toUiModel
 import poke.rogue.helper.presentation.util.view.GridSpacingItemDecoration
 import poke.rogue.helper.presentation.util.view.dp
@@ -14,8 +19,26 @@ import poke.rogue.helper.presentation.util.view.dp
 class TypeSelectionBottomSheetFragment : BottomSheetDialogFragment() {
     private var _binding: FragmentTypeChoiceBottomSheetBinding? = null
     private val binding get() = requireNotNull(_binding)
+    private val selectorType: SelectorType by lazy {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            arguments?.getSerializable(KEY_SELECTION_TYPE, SelectorType::class.java)
+        } else {
+            arguments?.getSerializable(KEY_SELECTION_TYPE) as? SelectorType
+        } ?: throw IllegalArgumentException("InValid TypeSelector")
+    }
+    private lateinit var sharedViewModel: TypeViewModel
+
     private val adapter by lazy {
-        TypeSelectionAdapter(DummyTypeData.allTypes.map { it.toUiModel() })
+        TypeSelectionAdapter(
+            DummyTypeData.allTypes.map { it.toUiModel() },
+            selectorType,
+            sharedViewModel,
+        )
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        sharedViewModel = ViewModelProvider(requireActivity(), TypeViewModel.factory()).get(TypeViewModel::class.java)
     }
 
     override fun onCreateView(
@@ -49,5 +72,12 @@ class TypeSelectionBottomSheetFragment : BottomSheetDialogFragment() {
 
     companion object {
         const val TAG = "type_selection_bottom_sheet_fragment"
+        private const val KEY_SELECTION_TYPE = "selection_type"
+
+        fun newInstance(selectorType: SelectorType): TypeSelectionBottomSheetFragment {
+            return TypeSelectionBottomSheetFragment().apply {
+                arguments = Bundle().apply { putSerializable(KEY_SELECTION_TYPE, selectorType) }
+            }
+        }
     }
 }
