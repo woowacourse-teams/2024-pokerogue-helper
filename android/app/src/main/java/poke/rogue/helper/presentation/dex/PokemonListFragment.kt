38fa2a95458f6ac3ee5a41a22ac78e05da883e1d
sh1mj1 <7wlgns@gmail.com>
2024-07-18 13:41:57 +0900

    refactor(PokemonListFragment): 포켓몬 어댑터에 리스너 함수의 참조가 아닌 뷰모델을 넘긴다

--- android/app/src/main/java/poke/rogue/helper/presentation/dex/PokemonListFragment.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/dex/PokemonListFragment.kt
@@ -24,7 +24,7 @@ class PokemonListFragment :
     }
 
     private val pokemonAdapter: PokemonAdapter by lazy {
-        PokemonAdapter(viewModel::onClickPokemon)
+        PokemonAdapter(viewModel)
     }
 
     override fun onViewCreated(
