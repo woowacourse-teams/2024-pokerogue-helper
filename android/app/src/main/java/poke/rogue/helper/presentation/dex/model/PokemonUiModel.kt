dd7820e816d0f2a1d0c7970176fbac8e38ba058c
sh1mj1 <7wlgns@gmail.com>
2024-07-18 11:07:40 +0900

    feat(presentation/.../PokemonUiModel): id, 도감번호, 이름, 이미지, 타입 목록을 가진다

--- android/app/src/main/java/poke/rogue/helper/presentation/dex/model/PokemonUiModel.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/dex/model/PokemonUiModel.kt
@@ -1,19 +1,21 @@
 package poke.rogue.helper.presentation.dex.model
 
 import poke.rogue.helper.data.model.Pokemon
-import poke.rogue.helper.data.model.Type
+import poke.rogue.helper.presentation.type.model.TypeUiModel
 
 data class PokemonUiModel(
     val id: Long,
+    val dexNumber: Long,
     val name: String,
     val imageUrl: String,
-    val types: List<Type>,
+    val types: List<TypeUiModel>,
 )
 
 fun Pokemon.toUi(): PokemonUiModel =
     PokemonUiModel(
         id = id,
+        dexNumber = dexNumber,
         name = name,
         imageUrl = imageUrl,
-        types = types,
+        types = types.mapNotNull(TypeUiModel::fromType),
     )
