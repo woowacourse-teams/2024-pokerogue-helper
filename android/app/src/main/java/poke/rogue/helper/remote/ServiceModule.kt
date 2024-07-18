7a7052f9b1028332ba952fb9f6c8c2d71d751a2b
murjune <sis9221302@gmail.com>
2024-07-18 13:12:37 +0900

    feat: Service Module

---
+++ android/app/src/main/java/poke/rogue/helper/remote/ServiceModule.kt
@@ -0,0 +1,22 @@
+package poke.rogue.helper.remote
+
+import poke.rogue.helper.remote.service.AbilityService
+import poke.rogue.helper.remote.service.PokeDexService
+import retrofit2.create
+
+object ServiceModule {
+
+    fun pokeDexService(): PokeDexService = ServicePool.pokeDexService
+
+    fun abilityService(): AbilityService = ServicePool.abilityService
+
+
+    private object ServicePool {
+        val pokeDexService: PokeDexService by lazy {
+            RetrofitModule.retrofit().create()
+        }
+        val abilityService: AbilityService by lazy {
+            RetrofitModule.retrofit().create()
+        }
+    }
+}
