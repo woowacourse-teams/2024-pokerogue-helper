69e8046f1ec86cafa294c5885f20ba18d95ddbb8
sh1mj1 <7wlgns@gmail.com>
2024-07-18 11:11:00 +0900

    feat(presentation/.../PokemonDetailUiModel): 포켓몬 상세 정보 ui 모델을 가진다

---
+++ android/app/src/main/java/poke/rogue/helper/presentation/dex/model/PokemonDetailUiModel.kt
@@ -0,0 +1,20 @@
+package poke.rogue.helper.presentation.dex.model
+
+import poke.rogue.helper.data.model.PokemonDetail
+import poke.rogue.helper.data.model.Stat
+
+data class PokemonDetailUiModel(
+    val pokemon: PokemonUiModel,
+    val stats: List<StatUiModel>,
+    val abilities: List<String>,
+    val height: Float,
+    val weight: Float,
+)
+
+fun PokemonDetail.toUi(): PokemonDetailUiModel = PokemonDetailUiModel(
+    pokemon = pokemon.toUi(),
+    stats = stats.map(Stat::toUi),
+    abilities = abilities,
+    height = height,
+    weight = weight,
+)
