c1ef63c59b54c536f69527aef8affcf8f7669b5b
sh1mj1 <7wlgns@gmail.com>
2024-07-20 00:42:06 +0900

    refactor: 포켓몬 상세 데이터 클래스에 있던 더미 데이터를 fake 데이터 소스로 이동
    - fake 포켓몬 상세 레포지토리가 fakepokemonDataSource 를 주 생성자로 가진다

--- android/app/src/main/java/poke/rogue/helper/data/model/PokemonDetail.kt
+++ android/app/src/main/java/poke/rogue/helper/data/model/PokemonDetail.kt
@@ -6,24 +6,4 @@ data class PokemonDetail(
     val abilities: List<String>,
     val height: Float,
     val weight: Float,
-) {
-    companion object {
-        val DUMMY =
-            PokemonDetail(
-                pokemon = Pokemon.DUMMY,
-                stats =
-                    listOf(
-                        Stat("HP", 45),
-                        Stat("공격", 49),
-                        Stat("방어", 49),
-                        Stat("특수공격", 65),
-                        Stat("특수방어", 65),
-                        Stat("스피드", 45),
-                        Stat("총합", 318),
-                    ),
-                abilities = listOf("그래스메이커", "심록", "엽록소"),
-                height = 0.7f,
-                weight = 6.9f,
-            )
-    }
-}
+)
--- android/app/src/main/java/poke/rogue/helper/data/repository/FakePokemonDetailRepository.kt
+++ android/app/src/main/java/poke/rogue/helper/data/repository/FakePokemonDetailRepository.kt
@@ -1,7 +1,10 @@
 package poke.rogue.helper.data.repository
 
+import poke.rogue.helper.data.datasource.FakePokemonDetailDataSource
 import poke.rogue.helper.data.model.PokemonDetail
 
-class FakePokemonDetailRepository : PokemonDetailRepository {
-    override fun pokemonDetail(id: Long): PokemonDetail = PokemonDetail.DUMMY
+class FakePokemonDetailRepository(
+    private val fakePokemonDetailDataSource: FakePokemonDetailDataSource,
+) : PokemonDetailRepository {
+    override fun pokemonDetail(id: Long): PokemonDetail = fakePokemonDetailDataSource.pokemonDetail(id)
 }
