c32b3d4ce04e05c9468ba9b537c280976b407d1a
unifolio0 <boho82ha@gmail.com>
2024-07-20 11:02:22 +0900

    feat: 포켓몬 타입 업데이트 기능

--- backend/pokerogue/src/main/java/com/pokerogue/helper/external/client/PokeClient.java
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/external/client/PokeClient.java
@@ -3,6 +3,7 @@ package com.pokerogue.helper.external.client;
 import com.pokerogue.helper.external.dto.CountResponse;
 import com.pokerogue.helper.external.dto.ListResponse;
 import com.pokerogue.helper.external.dto.ability.AbilityResponse;
+import com.pokerogue.helper.external.dto.type.TypeResponse;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
@@ -18,4 +19,13 @@ public interface PokeClient {
 
     @GetExchange("/ability/{id}")
     AbilityResponse getAbilityResponse(@PathVariable String id);
+
+    @GetExchange("/type/?offset=0&limit={size}")
+    ListResponse getTypeList(@PathVariable String size);
+
+    @GetExchange("/type")
+    CountResponse getTypeListSize();
+
+    @GetExchange("/type/{id}")
+    TypeResponse getTypeResponse(@PathVariable String id);
 }
