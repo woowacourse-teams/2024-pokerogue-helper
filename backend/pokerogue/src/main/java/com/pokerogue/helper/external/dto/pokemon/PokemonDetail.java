0886f6a5aec84cc76996cec614277b724fa943ed
unifolio0 <boho82ha@gmail.com>
2024-07-20 18:34:09 +0900

    refactor: 클래스명 수정, 변수명 수정, for문 stream으로 수정

---
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/external/dto/pokemon/PokemonDetail.java
@@ -0,0 +1,7 @@
+package com.pokerogue.helper.external.dto.pokemon;
+
+import com.pokerogue.helper.external.dto.NameAndUrl;
+import java.util.List;
+
+public record PokemonDetail(String name, int weight, int height, NameAndUrl species, int hp, int attack, int defense, int speed, int specialAttack, int specialDefense, int totalStats, List<String> abilityNameList, List<String> typeNameList) {
+}
