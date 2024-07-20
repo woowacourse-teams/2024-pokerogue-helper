da543d26ff3315ac0e0dd65b27b9e47d0eb806c2
unifolio0 <boho82ha@gmail.com>
2024-07-20 13:11:48 +0900

    feat: 없는 타입, 특성을 찾을 시 예외 처리 기능

--- backend/pokerogue/src/main/java/com/pokerogue/helper/global/exception/ErrorMessage.java
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/global/exception/ErrorMessage.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorMessage {
-    POKEMON_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 포켓몬을 찾지 못했습니다.");
+    POKEMON_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 포켓몬을 찾지 못했습니다."),
+    POKEMON_ABILITY_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 특성을 찾지 못했습니다."),
+    POKEMON_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 타입을 찾지 못했습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
