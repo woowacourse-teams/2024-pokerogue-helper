6a268ac8b480fabf69e4c394b40196a31cbad6a5
unifolio0 <boho82ha@gmail.com>
2024-07-21 19:12:36 +0900

    fix: sql query 오류 수정

--- backend/pokerogue/src/main/java/com/pokerogue/helper/pokemon/domain/Pokemon.java
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/pokemon/domain/Pokemon.java
@@ -70,11 +70,12 @@ public class Pokemon {
     @OneToMany(mappedBy = "pokemon", fetch = FetchType.LAZY)
     private List<PokemonAbilityMapping> pokemonAbilityMappings;
 
-    public Pokemon(Long pokedexNumber, String name, Integer weight, Integer height, Integer hp, Integer speed,
+    public Pokemon(Long pokedexNumber, String name, String koName, Integer weight, Integer height, Integer hp, Integer speed,
                    Integer attack, Integer defense, Integer specialAttack, Integer specialDefense,
                    Integer totalStats, String image) {
         this.pokedexNumber = pokedexNumber;
         this.name = name;
+        this.koName = koName;
         this.weight = weight;
         this.height = height;
         this.hp = hp;
