e01aefdf1c445dc9b7f38de0c060d18d4cb252b6
unifolio0 <boho82ha@gmail.com>
2024-07-22 17:02:08 +0900

    refactor: AllArgsConstructor 어노테이션 삭제

--- backend/pokerogue/src/main/java/com/pokerogue/helper/type/domain/PokemonType.java
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/type/domain/PokemonType.java
@@ -7,13 +7,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@AllArgsConstructor
 @Table(name = "pokemon_type")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PokemonType {
