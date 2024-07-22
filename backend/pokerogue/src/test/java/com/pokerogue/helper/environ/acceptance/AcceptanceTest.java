607064409048735e188ba59a0418333a01cd4a5b
jongmee <ra123221@gmail.com>
2024-07-22 23:59:57 +0900

    test: 테스트 super class에 DatabaseCleaner 의존성 제거

--- backend/pokerogue/src/test/java/com/pokerogue/helper/environ/acceptance/AcceptanceTest.java
+++ backend/pokerogue/src/test/java/com/pokerogue/helper/environ/acceptance/AcceptanceTest.java
@@ -1,10 +1,7 @@
 package com.pokerogue.helper.environ.acceptance;
 
-import com.pokerogue.helper.environ.DatabaseCleaner;
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
@@ -16,16 +13,8 @@ public class AcceptanceTest {
     @LocalServerPort
     private int port;
 
-    @Autowired
-    private DatabaseCleaner databaseCleaner;
-
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
-    }
-
-    @AfterEach
-    void tearDown() {
-        databaseCleaner.clear();
     }
 }
