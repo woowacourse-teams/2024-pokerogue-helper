f989ca9cc2992c319d07037cd05b60796eb44f22
jongmee <ra123221@gmail.com>
2024-07-22 23:59:25 +0900

    test: PokemonService 테스트 추가

---
+++ backend/pokerogue/src/test/java/com/pokerogue/helper/pokemon/service/PokemonServiceTest.java
@@ -0,0 +1,52 @@
+package com.pokerogue.helper.pokemon.service;
+
+import com.pokerogue.helper.environ.service.ServiceTest;
+import com.pokerogue.helper.global.exception.ErrorMessage;
+import com.pokerogue.helper.global.exception.GlobalCustomException;
+import com.pokerogue.helper.pokemon.dto.PokedexResponse;
+import com.pokerogue.helper.pokemon.dto.PokemonResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class PokemonServiceTest extends ServiceTest {
+
+    @Autowired
+    private PokemonService pokemonService;
+
+    @Test
+    @DisplayName("모든 포켓몬 목록을 조회한다.")
+    void findPokemons() {
+        List<PokemonResponse> pokemons = pokemonService.findPokemons();
+
+        assertThat(pokemons).isNotEmpty();
+    }
+
+    @Test
+    @DisplayName("한 포켓몬의 도감 상세 정보를 조회한다.")
+    void findPokedexDetails() {
+        PokedexResponse pokedexDetails = pokemonService.findPokedexDetails(1L);
+
+        assertAll(() -> {
+            assertThat(pokedexDetails.pokedexNumber()).isNotNull();
+            assertThat(pokedexDetails.pokemonTypeResponses()).isNotEmpty();
+            assertThat(pokedexDetails.pokemonAbilityNames()).isNotEmpty();
+        });
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 포켓몬의 상세 정보를 조회하면 예외가 발생한다.")
+    void findPokedexDetailsWhenNotExist() {
+        Long pokemonId = 100L;
+
+        assertThatThrownBy(() -> pokemonService.findPokedexDetails(pokemonId))
+                .isInstanceOf(GlobalCustomException.class)
+                .hasMessage(ErrorMessage.POKEMON_NOT_FOUND.getMessage());
+    }
+}
