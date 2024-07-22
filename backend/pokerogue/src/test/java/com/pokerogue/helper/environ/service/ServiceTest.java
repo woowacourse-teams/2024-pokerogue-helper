7ff9e561939d7453b16776e287565567de629a91
jongmee <ra123221@gmail.com>
2024-07-22 23:59:57 +0900

    test: 테스트 super class에 DatabaseCleaner 의존성 제거

--- backend/pokerogue/src/test/java/com/pokerogue/helper/environ/service/ServiceTest.java
+++ backend/pokerogue/src/test/java/com/pokerogue/helper/environ/service/ServiceTest.java
@@ -1,20 +1,9 @@
 package com.pokerogue.helper.environ.service;
 
-import com.pokerogue.helper.environ.DatabaseCleaner;
-import org.junit.jupiter.api.AfterEach;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles(value = "local-mysql")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public abstract class ServiceTest {
-
-    @Autowired
-    private DatabaseCleaner databaseCleaner;
-
-    @AfterEach
-    void tearDown() {
-        databaseCleaner.clear();
-    }
 }
