5dcb5c0201c67f9a40b6893fbd08adc01c671eae
sh1mj1 <7wlgns@gmail.com>
2024-07-22 16:32:48 +0900

    feat: 포켓몬 이름으로 검색한 포켓몬 결과 목록을 구한다

--- android/app/src/main/java/poke/rogue/helper/data/repository/FakePokemonListRepository.kt
+++ android/app/src/main/java/poke/rogue/helper/data/repository/FakePokemonListRepository.kt
@@ -1,5 +1,7 @@
 package poke.rogue.helper.data.repository
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import poke.rogue.helper.data.datasource.FakePokemonListDataSource
 import poke.rogue.helper.data.model.Pokemon
 
@@ -7,4 +9,10 @@ class FakePokemonListRepository(
     private val pokemonListDataSource: FakePokemonListDataSource,
 ) : PokemonListRepository {
     override fun pokemons(): List<Pokemon> = pokemonListDataSource.pokemons()
+
+    override fun searchPokemons(query: String): Flow<List<Pokemon>> {
+        return flow {
+            emit(pokemons().filter { it.name.contains(query, ignoreCase = true) })
+        }
+    }
 }
--- android/app/src/main/java/poke/rogue/helper/data/repository/PokemonListRepository.kt
+++ android/app/src/main/java/poke/rogue/helper/data/repository/PokemonListRepository.kt
@@ -1,7 +1,10 @@
 package poke.rogue.helper.data.repository
 
+import kotlinx.coroutines.flow.Flow
 import poke.rogue.helper.data.model.Pokemon
 
 interface PokemonListRepository {
     fun pokemons(): List<Pokemon>
+
+    fun searchPokemons(query: String): Flow<List<Pokemon>>
 }
