0439f5719ff5c638f16a53f1e959d2e051290dcb
unifolio0 <boho82ha@gmail.com>
2024-07-22 17:10:45 +0900

    refactor: api 변경, 메서드명 변경, GetMapping을 PostMapping으로 변경

--- backend/pokerogue/src/main/java/com/pokerogue/helper/type/controller/PokemonTypeController.java
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/type/controller/PokemonTypeController.java
@@ -1,21 +1,23 @@
 package com.pokerogue.helper.type.controller;
 
-import com.pokerogue.helper.type.dto.PokemonTypeResponse;
-import com.pokerogue.helper.type.service.PokemonTypeService;
+import com.pokerogue.helper.type.domain.PokemonType;
+import com.pokerogue.helper.util.DataSettingService;
 import com.pokerogue.helper.util.dto.ApiResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class PokemonTypeController{
+public class PokemonTypeController {
 
-    private final PokemonTypeService pokemonTypeService;
+    private final DataSettingService dataSettingService;
 
-    @GetMapping("/api/v1/types")
-    public ApiResponse<List<PokemonTypeResponse>> typeList() {
-        return new ApiResponse<>("타입 리스트 불러오기에 성공했습니다.", pokemonTypeService.findTypes());
+    @PostMapping("/api/v1/setting/types")
+    public ApiResponse<List<PokemonType>> saveTypes() {
+        List<PokemonType> savedPokemonTypes = dataSettingService.savePokemonTypes();
+
+        return new ApiResponse<>("타입 정보 업데이트", savedPokemonTypes);
     }
 }
