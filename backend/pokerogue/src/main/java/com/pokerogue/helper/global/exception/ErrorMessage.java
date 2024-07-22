8a41d49b6ab14242d27c26c86af9b79ff0bf676c
woojong <dwax1324@gmail.com>
2024-07-22 18:37:05 +0900

    feat: 상세 꿀팁 API를 불러오는 기능

--- backend/pokerogue/src/main/java/com/pokerogue/helper/global/exception/ErrorMessage.java
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/global/exception/ErrorMessage.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorMessage {
-    POKEMON_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 포켓몬을 찾지 못했습니다.");
+    POKEMON_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 포켓몬을 찾지 못했습니다."),
+    ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 꿀팁을 찾지 못했습니다.");
+
 
     private final HttpStatus httpStatus;
     private final String message;
