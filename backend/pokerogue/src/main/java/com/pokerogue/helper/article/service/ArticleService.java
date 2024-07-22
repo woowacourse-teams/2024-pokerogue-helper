25a65cd729dd1f2203a557eb17b79aa111923a96
woojong <dwax1324@gmail.com>
2024-07-22 18:37:05 +0900

    feat: 상세 꿀팁 API를 불러오는 기능

--- backend/pokerogue/src/main/java/com/pokerogue/helper/article/service/ArticleService.java
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/article/service/ArticleService.java
@@ -2,7 +2,10 @@ package com.pokerogue.helper.article.service;
 
 import com.pokerogue.helper.article.domain.Article;
 import com.pokerogue.helper.article.dto.ArticleResponse;
+import com.pokerogue.helper.article.dto.ArticleDetailsResponse;
 import com.pokerogue.helper.article.repository.ArticleRepository;
+import com.pokerogue.helper.global.exception.ErrorMessage;
+import com.pokerogue.helper.global.exception.GlobalCustomException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,5 +22,12 @@ public class ArticleService {
         return articles.stream()
                 .map(ArticleResponse::from)
                 .toList();
+    }
+
+    public ArticleDetailsResponse findArticleDetails(final Long id) {
+        Article article = articleRepository.findById(id)
+                .orElseThrow(() -> new GlobalCustomException(ErrorMessage.ARTICLE_NOT_FOUND));
+
+        return ArticleDetailsResponse.from(article);
     }
 }
