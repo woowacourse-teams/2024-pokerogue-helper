675ffa3dd138a9e2697df714ce5d40a676106851
sh1mj1 <7wlgns@gmail.com>
2024-07-23 17:12:04 +0900

    test: 포켓몬 응답과 포켓몬 응답 목록을 생성하는 test fixture

--- android/app/src/test/java/poke/rogue/helper/TestFixture.kt
+++ android/app/src/test/java/poke/rogue/helper/TestFixture.kt
@@ -1,5 +1,6 @@
 package poke.rogue.helper
 
+import poke.rogue.helper.remote.dto.response.pokemon.PokemonResponse
 import poke.rogue.helper.remote.dto.response.type.PokemonTypeResponse
 
 /**
@@ -26,3 +27,35 @@ fun pokemonTypeResponse(typeName: String) =
  *```
  */
 fun pokemonTypeResponses(vararg typeNames: String) = typeNames.map(::pokemonTypeResponse)
+
+/**
+ * Test fixture for [PokemonResponse].
+ * @param id 포켓몬의 id. default: `1`
+ * @param pokedexNumber 포켓몬의 도감 번호. default: `id * 10`
+ * @param name 포켓몬의 이름. default: `pokemon$id`
+ * @param image 포켓몬의 이미지. default: `logo$id`
+ * @param types 포켓몬의 타입 목록. default: 풀, 독
+ * @return [PokemonResponse]
+ */
+fun pokemonResponse(
+    id: Long = 1,
+    pokedexNumber: Long = id * 10,
+    name: String = "pokemon$id",
+    image: String = "logo$id",
+    types: List<PokemonTypeResponse> = pokemonTypeResponses("grass", "poison"),
+) = PokemonResponse(
+    id = id,
+    pokedexNumber = pokedexNumber,
+    name = name,
+    image = image,
+    types = types,
+)
+
+/**
+ * Test fixture for [List]<[PokemonResponse]>.
+ * @param ids 포켓몬의 id 목록
+ * @return [List]<[PokemonResponse]>
+ *
+ * also see [pokemonResponse]
+ */
+fun pokemonResponses(vararg ids: Long): List<PokemonResponse> = ids.map(::pokemonResponse)
