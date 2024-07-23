a75f0a3c4cda91e58ee3db6c49ee88755394677f
unifolio0 <boho82ha@gmail.com>
2024-07-23 19:48:07 +0900

    refactor: 메서드 체이닝 감소, 불필요한 필드 삭제

--- backend/pokerogue/src/main/java/com/pokerogue/external/pokemon/dto/pokemon/AbilityDataUrl.java
+++ backend/pokerogue/src/main/java/com/pokerogue/external/pokemon/dto/pokemon/AbilityDataUrl.java
@@ -3,4 +3,8 @@ package com.pokerogue.external.pokemon.dto.pokemon;
 import com.pokerogue.external.pokemon.dto.DataUrl;
 
 public record AbilityDataUrl(DataUrl ability) {
+
+    public String getName() {
+        return ability.name();
+    }
 }
