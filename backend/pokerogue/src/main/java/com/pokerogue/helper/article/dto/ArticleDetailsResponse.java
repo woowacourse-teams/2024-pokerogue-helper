ae296dd8d156e7f84a5f93cb501e5296b8afa735
woojong <dwax1324@gmail.com>
2024-07-23 14:24:45 +0900

    refactor: 매개변수의 final 삭제

--- backend/pokerogue/src/main/java/com/pokerogue/helper/article/dto/ArticleDetailsResponse.java
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/article/dto/ArticleDetailsResponse.java
@@ -4,7 +4,7 @@ import com.pokerogue.helper.article.domain.Article;
 
 public record ArticleDetailsResponse(Long id, String thumbnail, String title, String content) {
 
-    public static ArticleDetailsResponse from(final Article article) {
+    public static ArticleDetailsResponse from(Article article) {
         return new ArticleDetailsResponse(article.getId(), article.getThumbnail(), article.getTitle(),
                 article.getThumbnail());
     }
