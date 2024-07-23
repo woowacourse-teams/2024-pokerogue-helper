a4f386666374c6454728a95115bb9e73b2375acc
unifolio0 <boho82ha@gmail.com>
2024-07-23 11:18:16 +0900

    chore: 주석 설명 추가

--- backend/pokerogue/src/main/java/com/pokerogue/helper/external/dto/ability/AbilityResponse.java
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/external/dto/ability/AbilityResponse.java
@@ -4,6 +4,7 @@ import com.pokerogue.helper.external.dto.Name;
 import java.util.List;
 
 // https://pokeapi.co/api/v2/ability/{id}
+// 해당 id의 특성을 받아오는 Dto
 public record AbilityResponse(
         List<FlavorTextEntry> flavor_text_entries,
         String name,
--- backend/pokerogue/src/main/java/com/pokerogue/helper/external/dto/type/TypeResponse.java
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/external/dto/type/TypeResponse.java
@@ -4,5 +4,6 @@ import com.pokerogue.helper.external.dto.Name;
 import java.util.List;
 
 // https://pokeapi.co/api/v2/type/{id}
+// 해당 id의 타입을 받아오는 Dto
 public record TypeResponse(String name, List<Name> names) {
 }
