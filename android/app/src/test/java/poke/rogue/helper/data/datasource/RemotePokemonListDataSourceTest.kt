f0aed00f3f6277116125d9b532f3223762cd05ac
sh1mj1 <7wlgns@gmail.com>
2024-07-23 20:23:39 +0900

    test(RemotePokemonListDataSourceTest): 포켓몬 이름으로 쿼리된 포켓몬 목록을 불러온다

--- android/app/src/test/java/poke/rogue/helper/data/datasource/RemotePokemonListDataSourceTest.kt
+++ android/app/src/test/java/poke/rogue/helper/data/datasource/RemotePokemonListDataSourceTest.kt
@@ -29,4 +29,21 @@ class RemotePokemonListDataSourceTest {
             val actualPokemonDatas = pokemons(1, 2, 3, 4, 5)
             expectedPokemonDatas shouldBe actualPokemonDatas
         }
+
+    @Test
+    fun `포켓몬 이름으로 쿼리된 포켓몬 목록을 불러온다`() =
+        runTest {
+            // given
+            val query = "1"
+            val mockPokemonResponses = pokemonResponses(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+            val mockResponse = BaseResponse(message = "포켓몬 리스트 불러오기에 성공했습니다.", data = mockPokemonResponses)
+            coEvery { mockService.pokemons() } returns mockResponse
+
+            // when
+            val expectedPokemonDatas = dataSource.pokemons(query)
+
+            // then
+            val actionPokemonDatas = pokemons(1, 10)
+            expectedPokemonDatas shouldBe actionPokemonDatas
+        }
 }
