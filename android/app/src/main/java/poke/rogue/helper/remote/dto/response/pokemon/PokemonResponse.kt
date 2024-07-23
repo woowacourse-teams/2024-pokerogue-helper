9e514456f9e317ee4c64636c9bbeef98256c2278
sh1mj1 <7wlgns@gmail.com>
2024-07-23 17:12:55 +0900

    feat: 포켓몬 응답, 포켓몬 응답 목록을 데이터로 매핑한다

--- android/app/src/main/java/poke/rogue/helper/remote/dto/response/pokemon/PokemonResponse.kt
+++ android/app/src/main/java/poke/rogue/helper/remote/dto/response/pokemon/PokemonResponse.kt
@@ -2,14 +2,27 @@ package poke.rogue.helper.remote.dto.response.pokemon
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import poke.rogue.helper.data.model.Pokemon
 import poke.rogue.helper.remote.dto.response.type.PokemonTypeResponse
+import poke.rogue.helper.remote.dto.response.type.toData
 
 @Serializable
 data class PokemonResponse(
     val id: Long,
-    val image: String,
-    val name: String,
     val pokedexNumber: Long,
+    val name: String,
+    val image: String,
     @SerialName("pokemonTypeResponses")
     val types: List<PokemonTypeResponse>,
 )
+
+fun PokemonResponse.toData(): Pokemon =
+    Pokemon(
+        id = id,
+        dexNumber = pokedexNumber,
+        name = name,
+        imageUrl = image,
+        types = types.map(PokemonTypeResponse::toData),
+    )
+
+fun List<PokemonResponse>.toData(): List<Pokemon> = map(PokemonResponse::toData)
