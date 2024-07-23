a55a3be1120bc05f2aa0c0ec1ecdbecc0a6d150b
kkosang <eiwoqp77@gmail.com>
2024-07-24 00:52:23 +0900

    feat: 특성 response를 data로 매핑

---
+++ android/app/src/main/java/poke/rogue/helper/data/model/Ability.kt
@@ -0,0 +1,7 @@
+package poke.rogue.helper.data.model
+
+data class Ability(
+    val id: Long = 0,
+    val name: String,
+    val description: String,
+)
--- android/app/src/main/java/poke/rogue/helper/remote/dto/response/ability/AbilityResponse.kt
+++ android/app/src/main/java/poke/rogue/helper/remote/dto/response/ability/AbilityResponse.kt
@@ -2,6 +2,7 @@ package poke.rogue.helper.remote.dto.response.ability
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import poke.rogue.helper.data.model.Ability
 
 @Serializable
 data class AbilityResponse(
@@ -9,4 +10,11 @@ data class AbilityResponse(
     val name: String,
     @SerialName("shortDescription")
     val description: String,
-)
+) {
+    fun toData(): Ability =
+        Ability(
+            id = id,
+            name = name,
+            description = description,
+        )
+}
