eeae170240d0f5bd4b1de53c1d937d811e11494d
woojong <dwax1324@gmail.com>
2024-07-24 15:05:18 +0900

    style: 개행 수정

--- backend/pokerogue/src/main/java/com/pokerogue/helper/type/dto/PokemonMatchingResponse.java
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/type/dto/PokemonMatchingResponse.java
@@ -3,6 +3,7 @@ package com.pokerogue.helper.type.dto;
 import com.pokerogue.helper.type.domain.PokemonTypeMatching;
 
 public record PokemonMatchingResponse(String from, String to, Double result) {
+
     public static PokemonMatchingResponse from(PokemonTypeMatching pokemonTypeMatching) {
         return new PokemonMatchingResponse(
                 pokemonTypeMatching.getTypeFrom(),
