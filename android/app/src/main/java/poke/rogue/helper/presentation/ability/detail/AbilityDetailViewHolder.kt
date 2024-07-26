aec0848792ae371df5900b0de31e093f6d87c89e
kkosang <eiwoqp77@gmail.com>
2024-07-27 02:54:49 +0900

    feat: 포켓몬 타입 표시

--- android/app/src/main/java/poke/rogue/helper/presentation/ability/detail/AbilityDetailViewHolder.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/ability/detail/AbilityDetailViewHolder.kt
@@ -2,11 +2,16 @@ package poke.rogue.helper.presentation.ability.detail
 
 import androidx.recyclerview.widget.RecyclerView
 import poke.rogue.helper.databinding.ItemAbilityDetailPokemonBinding
+import poke.rogue.helper.presentation.ability.detail.type.AbilityDetailTypeAdapter
 import poke.rogue.helper.presentation.dex.model.PokemonUiModel
 
 class AbilityDetailViewHolder(private val binding: ItemAbilityDetailPokemonBinding) :
     RecyclerView.ViewHolder(binding.root) {
     fun bind(pokemonUiModel: PokemonUiModel) {
         binding.pokemon = pokemonUiModel
+        binding.rvPokeTypeList.adapter =
+            AbilityDetailTypeAdapter().apply {
+                submitList(pokemonUiModel.types)
+            }
     }
 }
