37a3bc38fe6f3ac5cf5ce17a7bee19f42777af27
murjune <sis9221302@gmail.com>
2024-07-30 17:47:59 +0900

    build: project 단 pacelize, jbm, library 추가

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -16,9 +16,12 @@ buildscript {
 
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.kotlinx.serialization) apply false
+    alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.ktlint) apply false
     alias(libs.plugins.android.junit5) apply false
 }
