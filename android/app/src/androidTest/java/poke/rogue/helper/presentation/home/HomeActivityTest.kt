14dfd01018a512a891347823ad53f6774932a052
kkosang <eiwoqp77@gmail.com>
2024-08-05 16:34:05 +0900

    test: home 회전 테스트 추가

---
+++ android/app/src/androidTest/java/poke/rogue/helper/presentation/home/HomeActivityTest.kt
@@ -0,0 +1,68 @@
+package poke.rogue.helper.presentation.home
+
+import android.content.pm.ActivityInfo
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import poke.rogue.helper.R
+
+@RunWith(AndroidJUnit4::class)
+class HomeActivityTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(HomeActivity::class.java)
+
+    @Test
+    fun 화면_회전_시에도_로고가_표시된다() {
+        // given
+        onView(withId(R.id.iv_home_land_logo))
+            .check(matches(isDisplayed()))
+
+        // when
+        activityRule.scenario.onActivity { activity ->
+            activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        }
+
+        // then
+        onView(withId(R.id.iv_home_land_logo))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun 화면_회전_시에도_타입_메뉴_버튼이_보인다() {
+        // given
+        onView(withId(R.id.cv_home_land_type))
+            .check(matches(isDisplayed()))
+
+        // when
+        activityRule.scenario.onActivity { activity ->
+            activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        }
+
+        // then
+        onView(withId(R.id.cv_home_land_type))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun 화면_회전_시에도_꿀팁_메뉴_버튼이_보인다() {
+        // given
+        onView(withId(R.id.cv_home_land_tip))
+            .check(matches(isDisplayed()))
+
+        // when
+        activityRule.scenario.onActivity { activity ->
+            activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        }
+
+        // then
+        onView(withId(R.id.cv_home_land_tip))
+            .check(matches(isDisplayed()))
+    }
+}
