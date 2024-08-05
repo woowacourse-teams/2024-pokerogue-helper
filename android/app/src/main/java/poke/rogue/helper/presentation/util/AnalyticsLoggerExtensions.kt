4d66cc4cb3ed6fd77eb7d61e457431ba6916f5c2
murjune <sis9221302@gmail.com>
2024-08-05 16:56:59 +0900

    utils: Analytics Extensions

---
+++ android/app/src/main/java/poke/rogue/helper/presentation/util/AnalyticsLoggerExtensions.kt
@@ -0,0 +1,31 @@
+package poke.rogue.helper.presentation.util
+
+import androidx.fragment.app.Fragment
+import poke.rogue.helper.analytics.AnalyticsEvent
+import poke.rogue.helper.analytics.AnalyticsLogger
+
+
+fun AnalyticsLogger.logClickEvent(eventName: String) {
+    logEvent(
+        AnalyticsEvent(
+            type = AnalyticsEvent.Types.ACTION,
+            extras = listOf(
+                AnalyticsEvent.Param(AnalyticsEvent.ParamKeys.ACTION_NAME, eventName),
+            ),
+        )
+    )
+}
+
+inline fun <reified T: Fragment> AnalyticsLogger.logScreenView() {
+    logEvent(
+        AnalyticsEvent(
+            type = AnalyticsEvent.Types.SCREEN_VIEW,
+            extras = listOf(
+                AnalyticsEvent.Param(
+                    AnalyticsEvent.ParamKeys.SCREEN_NAME,
+                    T::class.java.simpleName
+                ),
+            ),
+        ),
+    )
+}
