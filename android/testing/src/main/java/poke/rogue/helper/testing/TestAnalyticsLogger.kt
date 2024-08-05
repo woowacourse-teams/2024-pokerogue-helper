486156cf539b67b6b3b942d16dcb501e330eaf8f
murjune <sis9221302@gmail.com>
2024-08-05 16:56:30 +0900

    test: TestLogger

---
+++ android/testing/src/main/java/poke/rogue/helper/testing/TestAnalyticsLogger.kt
@@ -0,0 +1,22 @@
+package poke.rogue.helper.testing
+
+import poke.rogue.helper.analytics.AnalyticsEvent
+import poke.rogue.helper.analytics.AnalyticsLogger
+
+class TestAnalyticsLogger : AnalyticsLogger {
+
+    private val events = mutableListOf<AnalyticsEvent>()
+    private val errors = mutableListOf<Throwable>()
+
+    override fun logEvent(event: AnalyticsEvent) {
+        events.add(event)
+    }
+
+    override fun logError(throwable: Throwable, message: String?) {
+        errors.add(throwable)
+    }
+
+    fun hasLogged(event: AnalyticsEvent) = event in events
+
+    fun hasLoggedError(throwable: Throwable) = throwable in errors
+}
