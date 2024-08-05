c7b415771fb17651057c90984134908511a41482
murjune <sis9221302@gmail.com>
2024-08-05 16:56:22 +0900

    feat: Version 별 AnalyticsLogger

---
+++ android/analytics/src/main/java/poke/rogue/helper/analytics/AnalyticsLogger.kt
@@ -0,0 +1,23 @@
+package poke.rogue.helper.analytics
+
+private const val DEBUG_MODE = "debug"
+private const val ALPHA_MODE = "alpha"
+private const val BETA_MODE = "beta"
+private const val RELEASE_MODE = "release"
+
+/** Analytics API surface */
+interface AnalyticsLogger {
+    fun logEvent(event: AnalyticsEvent)
+
+    fun logError(throwable: Throwable, message: String? = null)
+}
+
+fun analyticsLogger(): AnalyticsLogger {
+    return when (BuildConfig.BUILD_TYPE) {
+        DEBUG_MODE -> DebugAnalyticsLogger
+        ALPHA_MODE -> AlphaAnalyticsLogger
+        BETA_MODE -> FireBaseAnalyticsLogger
+        RELEASE_MODE -> FireBaseAnalyticsLogger
+        else -> error("Unknown build type")
+    }
+}
