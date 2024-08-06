b56ccfd0fe3c08a215949048badeded10aead0cc
sh1mj1 <7wlgns@gmail.com>
2024-08-06 13:51:36 +0900

    fix: PoketmonViewHolder -> PokemonViewHolder 네이밍 변경

--- android/app/src/main/java/poke/rogue/helper/presentation/dex/PokemonViewHolder.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/dex/PokemonViewHolder.kt
@@ -6,7 +6,7 @@ import poke.rogue.helper.presentation.dex.model.PokemonUiModel
 import poke.rogue.helper.presentation.type.view.TypeChip
 import poke.rogue.helper.presentation.util.view.dp
 
-class PoketmonViewHolder(
+class PokemonViewHolder(
     private val binding: ItemPokemonListPokemonBinding,
     onClickPokeMonItem: PokemonListNavigateHandler,
 ) : RecyclerView.ViewHolder(binding.root) {
