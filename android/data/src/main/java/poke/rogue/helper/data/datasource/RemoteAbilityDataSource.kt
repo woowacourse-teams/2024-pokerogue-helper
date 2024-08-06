78c0f5eaffd0e5ff1c821ab06d61babf1ad75181
murjune <sis9221302@gmail.com>
2024-08-06 19:20:57 +0900

    fix: 존재하지 않음 경우  filtering

--- android/data/src/main/java/poke/rogue/helper/data/datasource/RemoteAbilityDataSource.kt
+++ android/data/src/main/java/poke/rogue/helper/data/datasource/RemoteAbilityDataSource.kt
@@ -7,7 +7,10 @@ import poke.rogue.helper.remote.ServiceModule
 import poke.rogue.helper.remote.service.AbilityService
 
 class RemoteAbilityDataSource(private val abilityService: AbilityService) {
-    suspend fun abilities(): List<Ability> = abilityService.abilities().data.map { it.toData() }
+    suspend fun abilities(): List<Ability> =
+        abilityService.abilities().data
+            .map { it.toData() }
+            .filter { it.description.contains("존재하지 않습니다").not() } // TODO: 서버가 처리하도록 변경
 
     suspend fun abilityDetail(id: Long): AbilityDetail = abilityService.ability(id).data.toData()
 
@@ -15,7 +18,8 @@ class RemoteAbilityDataSource(private val abilityService: AbilityService) {
         private var instance: RemoteAbilityDataSource? = null
 
         fun instance(): RemoteAbilityDataSource {
-            return instance ?: RemoteAbilityDataSource(ServiceModule.abilityService()).also { instance = it }
+            return instance
+                ?: RemoteAbilityDataSource(ServiceModule.abilityService()).also { instance = it }
         }
     }
 }
