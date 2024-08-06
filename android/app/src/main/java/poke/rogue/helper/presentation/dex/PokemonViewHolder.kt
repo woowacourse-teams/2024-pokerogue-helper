566db4f1f13ef6bcc192a5b2cc5bc35f8f05bc1d
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2024-08-06 17:38:22 +0900

    [AN-UI] 포켓몬 상세, 목록 화면의 타입 위치 수정 (#136)
    * refactor: pokemon detail 화면의 view 계층 개선
    
    * refactor: pokemon detail 화면에서 type chip 사용
    
    * refactor: pokemon detail 타입을 보여주는 방법 변경
    
    - 포켓몬 이름 아래에 타입들을 보여준다
    - 리사이클러뷰 제거, LinearLayout 에 동적으로 타입들을 추가
    
    * refactor: pokemon detail 화면에서 타입을 추가하는 역할을 어댑터에 위임
    
    * refactor: item pokemon list pokemon 프리뷰 화면의 타입 디자인 개선
    
    - 타입을 포켓몬 이름 아래에 위치시킨다
    - 무거운 리사이클러뷰를 삭제한다
    - 어댑터의 함수 파라미터에 글자와 아이콘 크기를 넘긴다
    
    * chore: PokemonTypesAdapter 의 패키지 위치 변경
    
    * fix: 아이템 뷰에 타입이 중복해서 추가되는 경우를 방지한다
    
    - viewGroup.removeAllViews() 메서드 추가
    
    * feat: layoutParams 의 높이와 너비를 지정하는 바인딩 어댑터
    
    * refactor: TypeChip 에 ui 설정 데이터 클래스를 만들고 이를 item_type 에 데이터바인딩
    
    * refactor: 상성 보기에서 사용하는 xml 파일을 변경한다
    
    * chore: 타입 사이의 공간 spacing 네이밍
    
    * fix:포켓몬 목록에서 포켓몬 사진 padding 조정
    
    * fix: PoketmonViewHolder -> PokemonViewHolder 네이밍 변경
    
    * refactor: 포켓몬 상세 xml 파일 뷰 계층 제거
    
    - 스탯을 제외하고 뷰 계층 제거
    
    * refactor: 포켓몬 상세 포켓몬 이미지 cropped 가 아닌 이미지 사용
    
    * fix: 포켓몬 목록 리사이클러뷰 아이템 사이 간격 조정
    
    * fix: 타입 사이의 spacing 의 크기를 dp 로 적용
    
    * refactor: PokemonTypeViewConfiguration 의 프로퍼티 네이밍 변경, spacingBetweenTypes 은 어댑터의 파라미터로 옮긴다

---
+++ android/app/src/main/java/poke/rogue/helper/presentation/dex/PokemonViewHolder.kt
@@ -0,0 +1,43 @@
+package poke.rogue.helper.presentation.dex
+
+import androidx.recyclerview.widget.RecyclerView
+import poke.rogue.helper.databinding.ItemPokemonListPokemonBinding
+import poke.rogue.helper.presentation.dex.model.PokemonUiModel
+import poke.rogue.helper.presentation.type.view.TypeChip
+import poke.rogue.helper.presentation.util.view.dp
+
+class PokemonViewHolder(
+    private val binding: ItemPokemonListPokemonBinding,
+    onClickPokeMonItem: PokemonListNavigateHandler,
+) : RecyclerView.ViewHolder(binding.root) {
+    init {
+        binding.listener = onClickPokeMonItem
+    }
+
+    fun bind(pokemonUiModel: PokemonUiModel) {
+        binding.pokemon = pokemonUiModel
+        val typesLayout = binding.layoutItemPokemonPokemonTypes
+
+        val pokemonTypesAdapter =
+            PokemonTypesAdapter(
+                context = binding.root.context,
+                viewGroup = typesLayout,
+            )
+
+        pokemonTypesAdapter.addTypes(
+            types = pokemonUiModel.types,
+            config = typesUiConfig,
+            spacingBetweenTypes = 0.dp,
+        )
+    }
+
+    companion object {
+        private val typesUiConfig =
+            TypeChip.PokemonTypeViewConfiguration(
+                hasBackGround = true,
+                nameSize = 8.dp,
+                iconSize = 18.dp,
+                spacing = 0.dp,
+            )
+    }
+}
