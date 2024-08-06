623ec64fcaed36e977b1225caba6486963b529fd
unifolio0 <boho82ha@gmail.com>
2024-08-06 16:12:30 +0900

    refactor: 사용하지 않는 메서드 삭제

--- backend/pokerogue/src/main/java/com/pokerogue/helper/type/repository/PokemonTypeRepository.java
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/type/repository/PokemonTypeRepository.java
@@ -1,10 +1,7 @@
 package com.pokerogue.helper.type.repository;
 
 import com.pokerogue.helper.type.domain.PokemonType;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PokemonTypeRepository extends JpaRepository<PokemonType, Long> {
-
-    Optional<PokemonType> findByName(String name);
 }
