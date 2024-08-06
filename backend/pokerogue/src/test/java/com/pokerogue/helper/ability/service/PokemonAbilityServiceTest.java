5009ead74dbe80ec82a6cbab6dd21188fe9555d2
jongmee <ra123221@gmail.com>
2024-08-06 15:10:25 +0900

    fix: 같은 특성을 가진 포켓몬 목록 조회 시 id 정렬 추가

--- backend/pokerogue/src/test/java/com/pokerogue/helper/ability/service/PokemonAbilityServiceTest.java
+++ backend/pokerogue/src/test/java/com/pokerogue/helper/ability/service/PokemonAbilityServiceTest.java
@@ -1,18 +1,27 @@
 package com.pokerogue.helper.ability.service;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 import com.pokerogue.environment.service.ServiceTest;
+import com.pokerogue.helper.ability.dto.PokemonAbilityWithPokemonsResponse;
+import com.pokerogue.helper.ability.dto.SameAbilityPokemonResponse;
+import com.pokerogue.helper.ability.repository.PokemonAbilityRepository;
 import com.pokerogue.helper.global.exception.ErrorMessage;
 import com.pokerogue.helper.global.exception.GlobalCustomException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class PokemonAbilityServiceTest extends ServiceTest {
 
     @Autowired
     private PokemonAbilityService pokemonAbilityService;
+
+    @Autowired
+    private PokemonAbilityRepository pokemonAbilityRepository;
 
     @Test
     @DisplayName("존재하지 않는 포켓몬의 특성을 조회하면 예외가 발생한다.")
@@ -20,5 +29,18 @@ class PokemonAbilityServiceTest extends ServiceTest {
         assertThatThrownBy(() -> pokemonAbilityService.findAbilityDetails(Long.MAX_VALUE))
                 .isInstanceOf(GlobalCustomException.class)
                 .hasMessage(ErrorMessage.POKEMON_ABILITY_NOT_FOUND.getMessage());
+    }
+
+    @Test
+    @DisplayName("같은 특성을 가진 포켓몬 목록을 조회할 때 id 순으로 정렬한다.")
+    void findPokemonsOrderByIdInAbilityDetails() {
+        Long abilityId = pokemonAbilityRepository.findAll().get(0).getId();
+
+        PokemonAbilityWithPokemonsResponse pokemonAbilityWithPokemons = pokemonAbilityService.findAbilityDetails(abilityId);
+
+        List<Long> pokemonIds = pokemonAbilityWithPokemons.pokemons().stream()
+                .map(SameAbilityPokemonResponse::id)
+                .toList();
+        assertThat(pokemonIds).isSortedAccordingTo(Long::compare);
     }
 }
