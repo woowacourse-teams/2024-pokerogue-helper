f86f4bb2e1a3cd8e3f75e5182200e98dab23764b
woojong <dwax1324@gmail.com>
2024-08-06 22:09:25 +0900

    fix: 중복 타입 제거

--- backend/pokerogue/src/main/java/com/pokerogue/helper/ability/service/PokemonAbilityService.java
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/ability/service/PokemonAbilityService.java
@@ -63,6 +63,7 @@ public class PokemonAbilityService {
                         pokemon -> pokemon,
                         pokemon -> pokemon.getPokemonTypeMappings().stream()
                                 .map(PokemonTypeMapping::getPokemonType)
+                                .distinct()
                                 .toList()
                 ));
     }
