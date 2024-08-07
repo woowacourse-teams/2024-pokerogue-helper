7b8c8313b9aed2e47291c72dc39c5e80bc2c113f
jongmee <ra123221@gmail.com>
2024-08-07 13:29:27 +0900

    fix: 포켓몬이 없는 특성 단건 조회 쿼리 수정

--- backend/pokerogue/src/test/java/com/pokerogue/helper/ability/repository/PokemonAbilityRepositoryTest.java
+++ backend/pokerogue/src/test/java/com/pokerogue/helper/ability/repository/PokemonAbilityRepositoryTest.java
@@ -8,8 +8,12 @@ import com.pokerogue.helper.ability.domain.PokemonAbility;
 import com.pokerogue.helper.pokemon.domain.Pokemon;
 import com.pokerogue.helper.pokemon.domain.PokemonAbilityMapping;
 import com.pokerogue.helper.pokemon.domain.PokemonTypeMapping;
+import com.pokerogue.helper.pokemon.repository.PokemonAbilityMappingRepository;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +22,9 @@ class PokemonAbilityRepositoryTest extends RepositoryTest {
 
     @Autowired
     private PokemonAbilityRepository pokemonAbilityRepository;
+
+    @Autowired
+    private PokemonAbilityMappingRepository pokemonAbilityMappingRepository;
 
     @Test
     @DisplayName("특성에 해당하는 포켓몬과 포켓몬 타입들을 조회한다.")
@@ -39,5 +46,30 @@ class PokemonAbilityRepositoryTest extends RepositoryTest {
                                 .map(PokemonTypeMapping::getPokemonType))
                         .toList()).isNotEmpty()
         );
+    }
+
+    @Test
+    @DisplayName("포켓몬이 없는 특성을 조회한다.")
+    void findByIdNotHavingPokemon() {
+        Long abilityId = findAbilityIdNotHavingPokemon();
+
+        Optional<PokemonAbility> queriedAbility = pokemonAbilityRepository.findByIdWithPokemonAndPokemonTypes(
+                abilityId);
+
+        assertThat(queriedAbility).isNotEmpty();
+    }
+
+    private Long findAbilityIdNotHavingPokemon() {
+        List<Long> allAbilityIds = pokemonAbilityRepository.findAll().stream()
+                .map(PokemonAbility::getId)
+                .toList();
+        Set<Long> abilityIdsHavingPokemon = pokemonAbilityMappingRepository.findAll().stream()
+                .map(PokemonAbilityMapping::getPokemonAbility)
+                .map(PokemonAbility::getId)
+                .collect(Collectors.toCollection(HashSet::new));
+        return allAbilityIds.stream()
+                .filter(id -> !abilityIdsHavingPokemon.contains(id))
+                .findAny()
+                .get();
     }
 }
