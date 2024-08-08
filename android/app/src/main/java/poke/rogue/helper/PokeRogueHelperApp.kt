7ab08711c56269ee894d2a5ff2e63ff8107c5570
murjune <sis9221302@gmail.com>
2024-08-08 13:18:21 +0900

    fix: Firebase Dev 버전 로깅 툴 해제

--- android/app/src/main/java/poke/rogue/helper/PokeRogueHelperApp.kt
+++ android/app/src/main/java/poke/rogue/helper/PokeRogueHelperApp.kt
@@ -1,12 +1,17 @@
 package poke.rogue.helper
 
 import android.app.Application
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.crashlytics.ktx.crashlytics
+import com.google.firebase.ktx.Firebase
 import timber.log.Timber
 
 class PokeRogueHelperApp : Application() {
     override fun onCreate() {
         super.onCreate()
         initTimber()
+        initFirebase()
+        error("This is an error")
     }
 
     private fun initTimber() {
@@ -19,5 +24,34 @@ class PokeRogueHelperApp : Application() {
                 },
             )
         }
+    }
+
+    private fun initFirebase() {
+        when (BuildConfig.BUILD_TYPE) {
+            DEBUG_MODE -> {
+                Firebase.analytics.setAnalyticsCollectionEnabled(false)
+                Firebase.crashlytics.setCrashlyticsCollectionEnabled(false)
+            }
+
+            ALPHA_MODE -> {
+                Firebase.analytics.setAnalyticsCollectionEnabled(false)
+                Firebase.crashlytics.setCrashlyticsCollectionEnabled(true)
+            }
+
+            BETA_MODE, RELEASE_MODE -> {
+                Firebase.analytics.setAnalyticsCollectionEnabled(true)
+                Firebase.crashlytics.setCrashlyticsCollectionEnabled(true)
+            }
+
+            else -> error("Unknown build type")
+        }
+
+    }
+
+    companion object {
+        private const val DEBUG_MODE = "debug"
+        private const val ALPHA_MODE = "alpha"
+        private const val BETA_MODE = "beta"
+        private const val RELEASE_MODE = "release"
     }
 }
