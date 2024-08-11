4a384431aa9dc07ef9897680b68832a39cd04538
sh1mj1 <7wlgns@gmail.com>
2024-08-11 15:44:44 +0900

    feat: 포켓몬 상세 액티비티 이미지와 이름, 툴바 위치

---
+++ android/app/src/main/java/poke/rogue/helper/presentation/dex/detail/PokemonDetailActivity.kt
@@ -0,0 +1,12 @@
+package poke.rogue.helper.presentation.dex.detail
+
+import android.os.Bundle
+import poke.rogue.helper.R
+import poke.rogue.helper.databinding.ActivityPokemonDetailBinding
+import poke.rogue.helper.presentation.base.BindingActivity
+
+class PokemonDetailActivity : BindingActivity<ActivityPokemonDetailBinding>(R.layout.activity_pokemon_detail) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
