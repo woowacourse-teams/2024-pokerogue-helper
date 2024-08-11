53a228367613af4c4d79d0cdccb6e4b0c1a9cfb3
kkosang <eiwoqp77@gmail.com>
2024-08-11 18:53:18 +0900

    feat: 아이템 화면 추가

---
+++ android/app/src/main/java/poke/rogue/helper/presentation/item/ItemActivity.kt
@@ -0,0 +1,20 @@
+package poke.rogue.helper.presentation.item
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.widget.Toolbar
+import poke.rogue.helper.R
+import poke.rogue.helper.databinding.ActivityItemBinding
+import poke.rogue.helper.presentation.toolbar.ToolbarActivity
+
+class ItemActivity : ToolbarActivity<ActivityItemBinding>(R.layout.activity_item) {
+
+    override val toolbar: Toolbar
+        get() = binding.toolbarItem
+
+    companion object {
+        fun intent(context: Context): Intent {
+            return Intent(context, ItemActivity::class.java)
+        }
+    }
+}
