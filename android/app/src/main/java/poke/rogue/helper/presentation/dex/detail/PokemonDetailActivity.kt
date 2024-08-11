253a68ac1a747b421cde9a713ff615fbaf48eac9
sh1mj1 <7wlgns@gmail.com>
2024-08-11 17:13:53 +0900

    feat: 포켓몬 상세 하단에 탭 레이아웃과 뷰 페이저 스켈레톤

--- android/app/src/main/java/poke/rogue/helper/presentation/dex/detail/PokemonDetailActivity.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/dex/detail/PokemonDetailActivity.kt
@@ -3,6 +3,11 @@ package poke.rogue.helper.presentation.dex.detail
 import android.os.Bundle
 import android.widget.LinearLayout.LayoutParams
 import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayoutMediator
 import poke.rogue.helper.R
 import poke.rogue.helper.data.repository.DefaultDexRepository
 import poke.rogue.helper.databinding.ActivityPokemonDetailBinding
@@ -13,6 +18,7 @@ import poke.rogue.helper.presentation.util.context.stringOf
 import poke.rogue.helper.presentation.util.repeatOnStarted
 import poke.rogue.helper.presentation.util.view.dp
 import poke.rogue.helper.presentation.util.view.loadImageWithProgress
+import timber.log.Timber
 
 class PokemonDetailActivity : BindingActivity<ActivityPokemonDetailBinding>(R.layout.activity_pokemon_detail) {
     private val viewModel by viewModels<PokemonDetailViewModel> {
@@ -25,16 +31,13 @@ class PokemonDetailActivity : BindingActivity<ActivityPokemonDetailBinding>(R.la
         super.onCreate(savedInstanceState)
         viewModel.updatePokemonDetail(intent.getLongExtra(POKEMON_ID, 2606))
 
+        initViewPager()
+
         pokemonTypesAdapter =
             PokemonTypesAdapter(
                 context = this,
                 viewGroup = binding.layoutPokemonDetailPokemonTypes,
             )
-
-        binding.ivPokemonDetailPokemon.loadImageWithProgress(
-            imageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-            progressIndicator = binding.progressIndicatorPokemonDetail,
-        )
 
         repeatOnStarted {
             viewModel.uiState.collect { pokemonDetail ->
@@ -46,6 +49,36 @@ class PokemonDetailActivity : BindingActivity<ActivityPokemonDetailBinding>(R.la
                 }
             }
         }
+    }
+
+    private fun initViewPager() {
+        // ViewPager2 Adapter 셋팅
+        val viewPager2Adatper = ViewPager2Adapter(this)
+        viewPager2Adatper.addFragment(PokemonStatFragment())
+        viewPager2Adatper.addFragment(PokemonEvolutionFragment())
+        viewPager2Adatper.addFragment(PokemonMovesFragment())
+        viewPager2Adatper.addFragment(PokemonInformationFragment())
+
+        // Adapter 연결
+        binding.pagerPokemonDetail.apply {
+            adapter = viewPager2Adatper
+
+            registerOnPageChangeCallback(
+                object : ViewPager2.OnPageChangeCallback() {
+                },
+            )
+        }
+
+        // ViewPager, TabLayout 연결
+        TabLayoutMediator(binding.tabLayoutPokemonDetail, binding.pagerPokemonDetail) { tab, position ->
+            Timber.d("ViewPager position: $position")
+            when (position) {
+                0 -> tab.text = "Stat"
+                1 -> tab.text = "Evolve"
+                2 -> tab.text = "Moves"
+                3 -> tab.text = "Information"
+            }
+        }.attach()
     }
 
     private fun bindPokemonDetail(pokemonDetail: PokemonDetailUiState.Success) {
@@ -78,5 +111,29 @@ class PokemonDetailActivity : BindingActivity<ActivityPokemonDetailBinding>(R.la
                 iconSize = 20.dp,
                 hasBackGround = false,
             )
+    }
+}
+
+class ViewPager2Adapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+    var fragments: ArrayList<Fragment> = ArrayList()
+
+    override fun getItemCount(): Int {
+        return fragments.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragments[position]
+    }
+
+    fun addFragment(fragment: Fragment) {
+        fragments.add(fragment)
+        notifyItemInserted(fragments.size - 1)
+        // TODO: notifyItemInserted!!
+    }
+
+    fun removeFragement() {
+        fragments.removeLast()
+        notifyItemRemoved(fragments.size)
+        // TODO: notifyItemRemoved!!
     }
 }
