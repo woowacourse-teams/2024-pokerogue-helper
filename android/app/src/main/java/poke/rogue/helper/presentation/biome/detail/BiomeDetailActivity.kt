d4beaef6b46190a328a461fcc1628e921920b6eb
kkosang <eiwoqp77@gmail.com>
2024-08-13 23:45:15 +0900

    ui: tablayout xml 작성 및 연결

--- android/app/src/main/java/poke/rogue/helper/presentation/biome/detail/BiomeDetailActivity.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/biome/detail/BiomeDetailActivity.kt
@@ -2,17 +2,44 @@ package poke.rogue.helper.presentation.biome.detail
 
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
+import com.google.android.material.tabs.TabLayoutMediator
 import poke.rogue.helper.R
 import poke.rogue.helper.databinding.ActivityBiomeDetailBinding
+import poke.rogue.helper.presentation.biome.BiomeDetailPagerAdapter
+import poke.rogue.helper.presentation.biome.model.BiomeUiModel
 import poke.rogue.helper.presentation.toolbar.ToolbarActivity
+import timber.log.Timber
 
 class BiomeDetailActivity :
     ToolbarActivity<ActivityBiomeDetailBinding>(R.layout.activity_biome_detail) {
+    private lateinit var pagerAdapter: BiomeDetailPagerAdapter
+
     override val toolbar: Toolbar
         get() = binding.toolbarBiomeDetail
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val biomeId = intent.getIntExtra(BIOME_ID, 1)
+        initAdapter()
+
+    }
+
+    private fun initAdapter() {
+        val detail = BiomeUiModel.DUMMYS[1]
+        binding.biomeUiModel = detail
+
+        pagerAdapter = BiomeDetailPagerAdapter(this)
+        binding.vpBiome.apply {
+            adapter = pagerAdapter
+        }
+
+        TabLayoutMediator(binding.tablayoutBiomeDetail, binding.vpBiome) { tab, position ->
+            when (position) {
+                0 -> tab.text = "출현 포켓몬"
+                1 -> tab.text = "관장 및 보스"
+                2 -> tab.text = "다음 바이옴"
+            }
+        }.attach()
     }
 
     companion object {
