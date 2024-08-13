887612c423bab805c7378b1a4d9d127d8d8c44ee
SangHyun Ko <87695921+kkosang@users.noreply.github.com>
2024-08-13 10:28:11 +0900

    [0.2.0.alpha/AN_UI] 바이옴 리스트 화면 추가 (#214)
    * ui: 홈 메뉴와 관련된 아이콘 추가
    
    * ui: 추가된 메뉴 xml 작성
    
    * feat: 바이옴 화면 추가
    
    * feat: 아이템 화면 추가
    
    * feat: 홈 화면과 연결
    
    * style: ktFormat
    
    * ui: 바이옴 아이템 및 페이지 xml 작성
    
    * feat: 바이옴 xml 연결
    
    * feat: 바이옴 상세 화면 구현 및 연결
    
    * feat: 바이옴 디테일 화면 이동
    
    * style: ktFormat
    
    * feat: dummy data 추가
    
    * refactor: 불필요한 부분 삭제

---
+++ android/app/src/main/java/poke/rogue/helper/presentation/biome/BiomeViewModel.kt
@@ -0,0 +1,23 @@
+package poke.rogue.helper.presentation.biome
+
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import poke.rogue.helper.analytics.AnalyticsLogger
+import poke.rogue.helper.analytics.analyticsLogger
+import poke.rogue.helper.presentation.error.ErrorViewModel
+
+class BiomeViewModel(logger: AnalyticsLogger = analyticsLogger()) :
+    ErrorViewModel(logger),
+    BiomeUiEventHandler {
+    private val _navigationToDetailEvent = MutableSharedFlow<Long>()
+    val navigationToDetailEvent: SharedFlow<Long> = _navigationToDetailEvent.asSharedFlow()
+
+    override fun navigateToDetail(biomeId: Long) {
+        viewModelScope.launch {
+            _navigationToDetailEvent.emit(biomeId)
+        }
+    }
+}
