db8732d242af550cdc4f10a9ec83e7eeee741dd0
JoYehyun <jo990714@naver.com>
2024-08-15 00:42:33 +0900

    ui: 배틀 페이지 액티비티 구현

--- android/app/src/main/java/poke/rogue/helper/presentation/battle/BattleActivity.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/battle/BattleActivity.kt
@@ -1,11 +1,49 @@
 package poke.rogue.helper.presentation.battle
 
+import WeatherSpinnerAdapter
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import androidx.activity.viewModels
 import androidx.appcompat.widget.Toolbar
 import poke.rogue.helper.R
 import poke.rogue.helper.databinding.ActivityBattleBinding
+import poke.rogue.helper.presentation.battle.model.WeatherUiModel
 import poke.rogue.helper.presentation.toolbar.ToolbarActivity
 
 class BattleActivity : ToolbarActivity<ActivityBattleBinding>(R.layout.activity_battle) {
+    private val viewModel by viewModels<BattleViewModel>()
     override val toolbar: Toolbar
         get() = binding.toolbarBattle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initView()
+        initSpinner()
+    }
+
+    private fun initView() {
+        binding.vm = viewModel
+        binding.lifecycleOwner = this
+    }
+
+    private fun initSpinner() {
+        val weatherAdapter = WeatherSpinnerAdapter(this, WeatherUiModel.DUMMY)
+        binding.spinnerWeather.adapter = weatherAdapter
+        binding.spinnerWeather.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View?,
+                    position: Int,
+                    id: Long,
+                ) {
+                    val selectedWeather = parent.getItemAtPosition(position) as WeatherUiModel
+                    viewModel.updateWeather(selectedWeather)
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                }
+            }
+    }
 }
