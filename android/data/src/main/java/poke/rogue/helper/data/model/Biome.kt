4e01ad0ea97e7c707d6b94bfb702518b21d11b0b
sh1mj1 <7wlgns@gmail.com>
2024-08-16 20:02:59 +0900

    feat: 데이터 레이어 Biome 추가

--- android/data/src/main/java/poke/rogue/helper/data/model/Biome.kt
+++ android/data/src/main/java/poke/rogue/helper/data/model/Biome.kt
@@ -4,4 +4,25 @@ data class Biome(
     val id: String,
     val name: String,
     val imageUrl: String,
-)
+) {
+    companion object {
+        val DUMMYS: List<Biome> =
+            listOf(
+                Biome(
+                    "1",
+                    "풀숲",
+                    "https://wiki.pokerogue.net/_media/ko:biomes:ko_grassy_fields_bg.png?w=200&tok=745c5b",
+                ),
+                Biome(
+                    "2",
+                    "높은 풀숲",
+                    "https://wiki.pokerogue.net/_media/ko:biomes:ko_tall_grass_bg.png?w=200&tok=b3497c",
+                ),
+                Biome(
+                    "3",
+                    "동굴",
+                    "https://wiki.pokerogue.net/_media/ko:biomes:ko_cave_bg.png?w=200&tok=905d8b",
+                ),
+            )
+    }
+}
