71d70cdd8e3e1cea2e4c54c987b98e848b9cef8e
Yehyun Jo <81362348+JoYehyun99@users.noreply.github.com>
2024-08-16 10:45:45 +0900

    [0.2.0.alpha/AN-UI] 배틀 도우미 화면 구현 (#232)
    * feat: 배틀 페이지 이동
    
    * chore: 필요한 아이콘 추가
    
    * chore: 필요한 배경 및 스타일 지정
    
    * ui: 배틀 페이지 xml 작성
    
    * ui: 날씨 선택 스피너 구현
    
    * ui: 배틀 페이지 액티비티 구현
    
    * feat: 날씨 데이터 연결
    
    * refactor: adapter의 리스트 데이터를 업데이트할 수 있도록 변경
    
    * fix: 린트 수정 및 충돌 해결

--- android/app/src/main/java/poke/rogue/helper/presentation/home/HomeActivity.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/home/HomeActivity.kt
@@ -12,6 +12,7 @@ import poke.rogue.helper.analytics.analyticsLogger
 import poke.rogue.helper.databinding.ActivityHomeBinding
 import poke.rogue.helper.presentation.ability.AbilityActivity
 import poke.rogue.helper.presentation.base.toolbar.ToolbarActivity
+import poke.rogue.helper.presentation.battle.BattleActivity
 import poke.rogue.helper.presentation.biome.BiomeActivity
 import poke.rogue.helper.presentation.dex.PokemonListActivity
 import poke.rogue.helper.presentation.tip.TipActivity
@@ -76,7 +77,9 @@ class HomeActivity : ToolbarActivity<ActivityHomeBinding>(R.layout.activity_home
                     }
 
                     is HomeNavigateEvent.ToBattle -> {
-                        toast("Coming soon")
+                        startActivity<BattleActivity> {
+                            logger.logClickEvent(NAVIGATE_TO_BATTLE)
+                        }
                     }
 
                     is HomeNavigateEvent.ToLogo -> navigateToPokeRogue()
@@ -103,7 +106,7 @@ class HomeActivity : ToolbarActivity<ActivityHomeBinding>(R.layout.activity_home
         private const val NAVIGATE_TO_TIP = "Nav_Tip"
         private const val NAVIGATE_TO_BIOME = "Nav_Biome"
         private const val NAVIGATE_TO_ITEM = "Nav_Item"
-        private const val NAVIGATE_TO_BATTLE = "Nav_Battle" // todo 배틀 화면
+        private const val NAVIGATE_TO_BATTLE = "Nav_Battle"
 
         fun intent(context: Context): Intent {
             return Intent(context, HomeActivity::class.java)
