bfe4e53cee585fbdc931166ad0ada181ebcf2be4
Yehyun Jo <81362348+JoYehyun99@users.noreply.github.com>
2024-08-16 10:45:45 +0900

    [2.0.0.alpha/AN-UI] 배틀 도우미 화면 구현 (#232)
    * feat: 배틀 페이지 이동
    
    * chore: 필요한 아이콘 추가
    
    * chore: 필요한 배경 및 스타일 지정
    
    * ui: 배틀 페이지 xml 작성
    
    * ui: 날씨 선택 스피너 구현
    
    * ui: 배틀 페이지 액티비티 구현
    
    * feat: 날씨 데이터 연결
    
    * refactor: adapter의 리스트 데이터를 업데이트할 수 있도록 변경
    
    * fix: 린트 수정 및 충돌 해결

---
+++ android/app/src/main/java/poke/rogue/helper/presentation/battle/model/WeatherUiModel.kt
@@ -0,0 +1,37 @@
+package poke.rogue.helper.presentation.battle.model
+
+import androidx.annotation.DrawableRes
+import poke.rogue.helper.R
+
+data class WeatherUiModel(
+    val icon: WeatherIcon,
+    val description: String,
+    val effect: String,
+) {
+    companion object {
+        val DEFAULT_SELECTED = WeatherUiModel(WeatherIcon.NONE, "날씨가 없다", "")
+
+        val DUMMY =
+            listOf<WeatherUiModel>(
+                WeatherUiModel(WeatherIcon.NONE, "날씨가 없다", ""),
+                WeatherUiModel(WeatherIcon.CLEAR, "햇살이 강하다", "불꽃 타입 기술의 위력이 1.5배가 된다"),
+                WeatherUiModel(WeatherIcon.RAIN, "비가 계속 내리고 있다", "물 타입 기술의 위력이 1.5배가 된다"),
+                WeatherUiModel(WeatherIcon.SNOW, "눈이 계속 내리고 있다", "얼음 타입 포켓몬의 방어가 1.5배 올라간다"),
+            )
+    }
+}
+
+enum class WeatherIcon(
+    @DrawableRes val iconResId: Int,
+) {
+    NONE(R.drawable.icon_close),
+    CLEAR(R.drawable.icon_sun),
+    RAIN(R.drawable.icon_rain),
+    SANDSTORM(R.drawable.icon_sun),
+    HAIL(R.drawable.icon_hail),
+    SNOW(R.drawable.icon_snow),
+    FOG(R.drawable.icon_foggy),
+    HEAVY_RAIN(R.drawable.icon_rain),
+    STRONG_SUN(R.drawable.icon_sun),
+    TURBULENCE(R.drawable.icon_air),
+}
