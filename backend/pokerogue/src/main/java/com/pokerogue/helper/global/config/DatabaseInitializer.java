8d220a8914a9c3494fae1ec59f52506ed252a729
woojong <dwax1324@gmail.com>
2024-08-16 18:28:10 +0900

    fix: 잘못된 resolve 수정

--- backend/pokerogue/src/main/java/com/pokerogue/helper/global/config/DatabaseInitializer.java
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/global/config/DatabaseInitializer.java
@@ -1,15 +1,15 @@
 package com.pokerogue.helper.global.config;
 
-import com.pokerogue.helper.pokemon2.MoveRepository;
-import com.pokerogue.helper.pokemon2.Pokemon2Repository;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.StringTokenizer;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import com.pokerogue.helper.ability.domain.PokemonAbility;
+import com.pokerogue.helper.ability.repository.PokemonAbilityRepository;
+import com.pokerogue.helper.pokemon.domain.Pokemon;
+import com.pokerogue.helper.pokemon.domain.PokemonAbilityMapping;
+import com.pokerogue.helper.pokemon.domain.PokemonTypeMapping;
+import com.pokerogue.helper.pokemon.repository.PokemonAbilityMappingRepository;
+import com.pokerogue.helper.pokemon.repository.PokemonRepository;
+import com.pokerogue.helper.pokemon.repository.PokemonTypeMappingRepository;
+import com.pokerogue.helper.type.domain.PokemonType;
+import com.pokerogue.helper.type.repository.PokemonTypeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -25,70 +25,94 @@ import java.util.List;
 @RequiredArgsConstructor
 public class DatabaseInitializer implements ApplicationRunner {
 
-    private final Pokemon2Repository pokemon2Repository;
-    private final MoveRepository moveRepository;
-
-    List<String> moveKeys = List.of("id", "name", "nameAppend", "effect", "type", "defaultType", "category",
-            "moveTarget", "power", "accuracy", "pp", "chance", "priority", "generation", "flags");
-    List<String> pokemonKeys = List.of("speciesId", "name", "type1", "type2", "ability1", "ability2", "abilityHidden",
-            "abilityPassive", "generation", "legendary", "subLegendary", "mythical", "getEvolutionLevels", "baseTotal",
-            "baseStats", "height", "weight", "canChangeForm", "eggMoves", "moves", "biomes");
-
-    private Scanner scanner;
+    private final PokemonRepository pokemonRepository;
+    private final PokemonTypeRepository pokemonTypeRepository;
+    private final PokemonAbilityRepository pokemonAbilityRepository;
+    private final PokemonTypeMappingRepository pokemonTypeMappingRepository;
+    private final PokemonAbilityMappingRepository pokemonAbilityMappingRepository;
 
     @Override
     public void run(ApplicationArguments args) {
-        File file = new File("src/main/java/com/pokerogue/helper/global/config/pokemon.txt");
-        try {
-            scanner = new Scanner(file);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+        List<Pokemon> pokemons = pokemonRepository.saveAll(List.of(
+                new Pokemon(1L, "이상해씨", "이상해씨", 6.9, 0.7, 45, 45, 55, 50, 60, 70, 500, "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"),
+                new Pokemon(4L, "파이리", "파이리", 6.9, 0.7, 45, 45, 55, 50, 60, 70, 500, "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"),
+                new Pokemon(7L, "꼬부기", "꼬부기", 6.9, 0.7, 45, 45, 55, 50, 60, 70, 500, "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png"),
+                new Pokemon(25L, "피카츄", "피카츄", 6.9, 0.7, 45, 45, 55, 50, 60, 70, 500, "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"),
+                new Pokemon(133L, "이브이", "이브이", 6.9, 0.7, 45, 45, 55, 50, 60, 70, 500, "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/133.png"),
+                new Pokemon(152L, "치코리타", "치코리타", 6.9, 0.7, 45, 45, 55, 50, 60, 70, 500, "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/152.png"),
+                new Pokemon(393L, "펭도리", "펭도리", 6.9, 0.7, 45, 45, 55, 50, 60, 70, 500, "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/393.png"),
+                new Pokemon(702L, "데덴네", "데덴네", 6.9, 0.7, 45, 45, 55, 50, 60, 70, 500, "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/702.png"),
+                new Pokemon(54L, "고라파덕", "고라파덕", 6.9, 0.7, 45, 45, 55, 50, 60, 70, 500, "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/54.png")
+        ));
 
-        while (scanner.hasNextLine()) {
-            var line = scanner.nextLine();
-            List<String> values = new ArrayList<>();
-            StringTokenizer stringTokenizer = new StringTokenizer(line, "/");
+        List<PokemonType> pokemonTypes = pokemonTypeRepository.saveAll(List.of(
+                new PokemonType("grass", "풀", "grass dummy image"),
+                new PokemonType("poison", "독", "poison dummy image"),
+                new PokemonType("fire", "불", "fire dummy image"),
+                new PokemonType("water", "물", "water dummy image"),
+                new PokemonType("electric", "전기", "electric dummy image"),
+                new PokemonType("normal", "물", "normal dummy image"),
+                new PokemonType("fairy", "페어리", "fairy dummy image")
+        ));
 
-            while (stringTokenizer.hasMoreTokens()) {
-                values.add(stringTokenizer.nextToken().strip());
-            }
+        List<PokemonAbility> pokemonAbilities = pokemonAbilityRepository.saveAll(List.of(
+                new PokemonAbility("심록", "심록", "description", "detail description"),
+                new PokemonAbility("엽록소", "엽록소", "description", "detail description"),
+                new PokemonAbility("맹화", "맹화", "description", "detail description"),
+                new PokemonAbility("선파워", "선파워", "description", "detail description"),
+                new PokemonAbility("급류", "급류", "description", "detail description"),
+                new PokemonAbility("젖은접시", "젖은접시", "description", "detail description"),
+                new PokemonAbility("정전기", "정전기", "description", "detail description"),
+                new PokemonAbility("피뢰침", "피뢰침", "description", "detail description"),
+                new PokemonAbility("도주", "도주", "description", "detail description"),
+                new PokemonAbility("적응력", "적응력", "description", "detail description"),
+                new PokemonAbility("리프가드", "리프가드", "description", "detail description"),
+                new PokemonAbility("위험예지", "위험예지", "description", "detail description"),
+                new PokemonAbility("승기", "승기", "description", "detail description"),
+                new PokemonAbility("볼주머니", "볼주머니", "description", "detail description"),
+                new PokemonAbility("픽업", "픽업", "description", "detail description"),
+                new PokemonAbility("습기", "습기", "description", "detail description"),
+                new PokemonAbility("날씨부정", "날씨부정", "description", "detail description"),
+                new PokemonAbility("쓱쓱", "쓱쓱", "description", "detail description")
+        ));
 
-            if(values.size() != pokemonKeys.size()){
-                values.add("EMPTY");
-            }
-            Map<String, String> map = IntStream.range(0, pokemonKeys.size())
-                    .boxed()
-                    .collect(Collectors.toMap(pokemonKeys::get, values::get));
 
-            pokemon2Repository.save(map.get("name"), map);
-        }
-        saveMove();
-    }
+        pokemonTypeMappingRepository.saveAll(List.of(
+                new PokemonTypeMapping(pokemons.get(0), pokemonTypes.get(0)),
+                new PokemonTypeMapping(pokemons.get(0), pokemonTypes.get(1)),
+                new PokemonTypeMapping(pokemons.get(1), pokemonTypes.get(2)),
+                new PokemonTypeMapping(pokemons.get(2), pokemonTypes.get(2)),
+                new PokemonTypeMapping(pokemons.get(3), pokemonTypes.get(2)),
+                new PokemonTypeMapping(pokemons.get(3), pokemonTypes.get(3)),
+                new PokemonTypeMapping(pokemons.get(4), pokemonTypes.get(3)),
+                new PokemonTypeMapping(pokemons.get(4), pokemonTypes.get(4)),
+                new PokemonTypeMapping(pokemons.get(5), pokemonTypes.get(4)),
+                new PokemonTypeMapping(pokemons.get(6), pokemonTypes.get(5)),
+                new PokemonTypeMapping(pokemons.get(7), pokemonTypes.get(6)),
+                new PokemonTypeMapping(pokemons.get(7), pokemonTypes.get(5)),
+                new PokemonTypeMapping(pokemons.get(8), pokemonTypes.get(6))
+        ));
 
-    private void saveMove() {
-        File file = new File("src/main/java/com/pokerogue/helper/global/config/move.txt");
-
-        try {
-            scanner = new Scanner(file);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-
-        while (scanner.hasNextLine()) {
-            var line = scanner.nextLine();
-            List<String> values = new ArrayList<>();
-            StringTokenizer stringTokenizer = new StringTokenizer(line, "/");
-
-            while (stringTokenizer.hasMoreTokens()) {
-                values.add(stringTokenizer.nextToken().strip());
-            }
-
-            Map<String, String> map = IntStream.range(0, values.size())
-                    .boxed()
-                    .collect(Collectors.toMap(moveKeys::get, values::get));
-
-            moveRepository.save(map.get("name"), map);
-        }
+        pokemonAbilityMappingRepository.saveAll(List.of(
+                new PokemonAbilityMapping(pokemons.get(0), pokemonAbilities.get(0)),
+                new PokemonAbilityMapping(pokemons.get(0), pokemonAbilities.get(1)),
+                new PokemonAbilityMapping(pokemons.get(0), pokemonAbilities.get(16)),
+                new PokemonAbilityMapping(pokemons.get(1), pokemonAbilities.get(2)),
+                new PokemonAbilityMapping(pokemons.get(1), pokemonAbilities.get(17)),
+                new PokemonAbilityMapping(pokemons.get(2), pokemonAbilities.get(17)),
+                new PokemonAbilityMapping(pokemons.get(2), pokemonAbilities.get(3)),
+                new PokemonAbilityMapping(pokemons.get(3), pokemonAbilities.get(4)),
+                new PokemonAbilityMapping(pokemons.get(3), pokemonAbilities.get(5)),
+                new PokemonAbilityMapping(pokemons.get(4), pokemonAbilities.get(6)),
+                new PokemonAbilityMapping(pokemons.get(4), pokemonAbilities.get(7)),
+                new PokemonAbilityMapping(pokemons.get(5), pokemonAbilities.get(8)),
+                new PokemonAbilityMapping(pokemons.get(5), pokemonAbilities.get(15)),
+                new PokemonAbilityMapping(pokemons.get(6), pokemonAbilities.get(9)),
+                new PokemonAbilityMapping(pokemons.get(6), pokemonAbilities.get(14)),
+                new PokemonAbilityMapping(pokemons.get(7), pokemonAbilities.get(10)),
+                new PokemonAbilityMapping(pokemons.get(7), pokemonAbilities.get(11)),
+                new PokemonAbilityMapping(pokemons.get(8), pokemonAbilities.get(12)),
+                new PokemonAbilityMapping(pokemons.get(8), pokemonAbilities.get(13))
+        ));
     }
 }
