7ef02a14843230d9f6862b2d476a096c9a7b23f6
sh1mj1 <7wlgns@gmail.com>
2024-08-16 20:03:36 +0900

    feat: 포켓몬 상세 정보에서 바이옴 정보 item UI

--- android/app/src/main/java/poke/rogue/helper/presentation/dex/detail/PokemonDetailNavigateHandler.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/dex/detail/PokemonDetailNavigateHandler.kt
@@ -3,5 +3,7 @@ package poke.rogue.helper.presentation.dex.detail
 interface PokemonDetailNavigateHandler {
     fun navigateToAbilityDetail(abilityId: Long)
 
+    fun navigateToBiomeDetail(biomeId: String)
+
     fun navigateToHome()
 }
