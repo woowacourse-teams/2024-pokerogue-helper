8a261eb56ddaee4d59d9a8e697550671dbd46a70
sh1mj1 <7wlgns@gmail.com>
2024-08-18 10:29:13 +0900

    fix: Pokemon 클래스의 더미데이터 복구

--- android/data/src/main/java/poke/rogue/helper/data/model/Pokemon.kt
+++ android/data/src/main/java/poke/rogue/helper/data/model/Pokemon.kt
@@ -9,7 +9,23 @@ data class Pokemon(
     val name: String,
     val imageUrl: String,
     val types: List<Type>,
-)
+){
+    companion object {
+        private const val DUMMY_POKEMON_NAME = "이상해씨"
+        private const val DUMMY_IMAGE_URL =
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+        private val DUMMY_TYPES = listOf(Type.GRASS, Type.POISON)
+
+        val DUMMY =
+            Pokemon(
+                id = "1",
+                dexNumber = 1,
+                name = DUMMY_POKEMON_NAME,
+                imageUrl = DUMMY_IMAGE_URL,
+                types = DUMMY_TYPES,
+            )
+    }
+}
 
 fun PokemonResponse.toData(): Pokemon =
     Pokemon(
