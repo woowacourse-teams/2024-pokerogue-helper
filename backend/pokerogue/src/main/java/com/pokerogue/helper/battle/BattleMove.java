12ecdfe0ab0f11089ab24a4c884e69d07c15c462
종미(미아) <101439796+jongmee@users.noreply.github.com>
2024-08-19 14:17:25 +0900

    [BE-FEAT] 포켓몬의 기술 리스트 불러오는 API (#237)
    * refactor: WeatherRepository 컴포넌트 애너테이션 변경
    
    * feat: 기술 데이터 세팅
    
    * refactor: 기술 id 저장 데이터 변경
    
    * feat: 포켓몬의 기술 데이터 세팅
    
    * feat: 포켓몬의 기술 리스트 불러오는 api
    
    * chore: 기술 데이터 추가
    
    * feat: 기술 리스트에서 타입, 카테고리 로고 함께 반환
    
    * fix: 빈 문자열 저장하는 오류
    
    * refactor: 포켓몬 타입 enum 생성
    
    * test: 모든 도감 번호에 대해서 기술 리스트 조회 테스트
    
    * refactor: 포켓몬 타입 레포지토리 생성
    
    * refactor: 기술 카테고리 enum 생성
    
    * style: 개행 추가
    
    * style: enum 개행 추가
    
    * chore: 데이터 추가 히스토리 맞추기
    
    * fix: 중복되는 bean 이름 변경

---
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/battle/BattleMove.java
@@ -0,0 +1,20 @@
+package com.pokerogue.helper.battle;
+
+public record BattleMove(
+        String id,
+        String name,
+        String nameAppend,
+        String effect,
+        String type,
+        String defaultTypeId,
+        String category,
+        String moveTarget,
+        Integer power,
+        Integer accuracy,
+        Integer pp,
+        Integer chance,
+        Integer priority,
+        Integer generation,
+        String flags
+) {
+}
