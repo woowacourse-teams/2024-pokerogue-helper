a0a5bc22eb221a24f74fba7d02b70da04fb1cff3
sh1mj1 <7wlgns@gmail.com>
2024-08-20 01:50:39 +0900

    feat(EvolutionsUiModel): 진화 체인이 있는지 확인한다

--- android/app/src/main/java/poke/rogue/helper/presentation/dex/model/EvolutionsUiModel.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/dex/model/EvolutionsUiModel.kt
@@ -2,25 +2,39 @@ package poke.rogue.helper.presentation.dex.model
 
 import poke.rogue.helper.presentation.dex.model.SingleEvolutionUiModel.Companion.DUMMY_ALOLA_RAICHU
 import poke.rogue.helper.presentation.dex.model.SingleEvolutionUiModel.Companion.DUMMY_GIGA_PIKACHU
+import poke.rogue.helper.presentation.dex.model.SingleEvolutionUiModel.Companion.DUMMY_GOLDUCK
 import poke.rogue.helper.presentation.dex.model.SingleEvolutionUiModel.Companion.DUMMY_PICHU
 import poke.rogue.helper.presentation.dex.model.SingleEvolutionUiModel.Companion.DUMMY_PIKACHU
+import poke.rogue.helper.presentation.dex.model.SingleEvolutionUiModel.Companion.DUMMY_PSYDUCK
 import poke.rogue.helper.presentation.dex.model.SingleEvolutionUiModel.Companion.DUMMY_RAICHU
 
 data class EvolutionsUiModel(
-    val evolutions: List<SingleEvolutionUiModel>
+    val evolutions: List<SingleEvolutionUiModel>,
 ) {
+    constructor(vararg evolutions: SingleEvolutionUiModel) : this(evolutions.toList())
+
     fun evolutions(depth: Int) = evolutions.filter { it.depth == depth }
+
+    fun hasEvolutionChain(): Boolean = evolutions.size > 1
 
     companion object {
         val DUMMY_PICAKCHU_EVOLUTION =
             EvolutionsUiModel(
+                evolutions =
                 listOf(
                     DUMMY_PICHU,
                     DUMMY_PIKACHU,
                     DUMMY_RAICHU,
                     DUMMY_ALOLA_RAICHU,
                     DUMMY_GIGA_PIKACHU,
-                )
+                ),
             )
+
+        val DUMMY_PSYDUCK_EVOLUTION = EvolutionsUiModel(
+            evolutions = listOf(
+                DUMMY_PSYDUCK,
+                DUMMY_GOLDUCK,
+            )
+        )
     }
 }
