0eaa67b3853e73e2dda892204802553727b44a10
kkosang <eiwoqp77@gmail.com>
2024-08-19 23:38:06 +0900

    refactor: BiomeService 변경

--- android/remote/src/main/java/poke/rogue/helper/remote/service/BiomeService.kt
+++ android/remote/src/main/java/poke/rogue/helper/remote/service/BiomeService.kt
@@ -1,8 +1,10 @@
 package poke.rogue.helper.remote.service
 
 import poke.rogue.helper.remote.dto.base.ApiResponse
+import poke.rogue.helper.remote.dto.response.biomes.BiomeDetailResponse
 import poke.rogue.helper.remote.dto.response.biomes.BiomesResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface BiomeService {
 
@@ -10,5 +12,7 @@ interface BiomeService {
     suspend fun biomes(): ApiResponse<List<BiomesResponse>>
 
     @GET("api/v1/biome/{id}")
-    suspend fun biome(): ApiResponse<BiomesResponse>
+    suspend fun biome(
+        @Path("id") id: String,
+    ): ApiResponse<BiomeDetailResponse>
 }
