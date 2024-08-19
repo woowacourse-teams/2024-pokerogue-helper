f176eb7348d0fc2a447a04b9c7ae627707c44abc
JUNWON LEE <sis9221302@gmail.com>
2024-08-19 15:11:20 +0900

    #238 복구 (#249)
    * ui: tablayout xml 작성 및 연결
    
    * ui: viewPager fragment xml 작성
    
    * ui: tablayout(다음바이옴) xml 작성 및 연결
    
    * ui: tablayout 디자인 변경
    
    * ui: 야생 포켓몬 xml 작성
    
    * ui: 야생 포켓몬 화면 구현
    
    * ui: boss fragment 추가
    
    * ui: tablayout(gym) 추가
    
    * refactor: 야생 fragment uiModel 수정
    
    * refactor: 바이옴 포켓몬 네이밍 수정
    
    * ui: boss fragment 화면 작성 및 연결
    
    * config: coil-svg 추가
    
    * feat: svgUrl 바인딩 어댑터
    
    * ui: 체육관 관장 fragment xml 작성 및 연결
    
    * refactor: tablayout string-array 이용
    
    * refactor: 패키지 이동 및 네이밍 수정
    
    * ui: xml 수정
    
    * style: ktFormat
    
    * style: ktFormat
    
    * refactor: ui 관련 코드리뷰 반영
    
    * refactor: 다음 바이옴 네이밍 변경
    
    * style: ktFormat
    
    ---------
    
    Co-authored-by: kkosang <eiwoqp77@gmail.com>

---
+++ android/app/src/main/java/poke/rogue/helper/presentation/biome/detail/BiomPokemonAdapter.kt
@@ -0,0 +1,38 @@
+package poke.rogue.helper.presentation.biome.detail
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import poke.rogue.helper.databinding.ItemPokemonListPokemonBinding
+import poke.rogue.helper.presentation.dex.model.PokemonUiModel
+import poke.rogue.helper.presentation.util.view.ItemDiffCallback
+
+class BiomPokemonAdapter :
+    ListAdapter<PokemonUiModel, BiomePokemonViewHolder>(poketmonComparator) {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int,
+    ): BiomePokemonViewHolder =
+        BiomePokemonViewHolder(
+            ItemPokemonListPokemonBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false,
+            ),
+        )
+
+    override fun onBindViewHolder(
+        holder: BiomePokemonViewHolder,
+        position: Int,
+    ) {
+        holder.bind(getItem(position))
+    }
+
+    companion object {
+        val poketmonComparator =
+            ItemDiffCallback<PokemonUiModel>(
+                onItemsTheSame = { oldItem, newItem -> oldItem.id == newItem.id },
+                onContentsTheSame = { oldItem, newItem -> oldItem == newItem },
+            )
+    }
+}
