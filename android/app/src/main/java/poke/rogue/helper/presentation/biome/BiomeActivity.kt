46e9cc8a49d4564cca3c0f90f264d69f922b2c23
kkosang <eiwoqp77@gmail.com>
2024-08-21 01:45:10 +0900

    feat: fetch remote data

--- android/app/src/main/java/poke/rogue/helper/presentation/biome/BiomeActivity.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/biome/BiomeActivity.kt
@@ -3,43 +3,59 @@ package poke.rogue.helper.presentation.biome
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.isVisible
 import poke.rogue.helper.R
 import poke.rogue.helper.analytics.AnalyticsLogger
 import poke.rogue.helper.analytics.analyticsLogger
+import poke.rogue.helper.data.repository.DefaultBiomeRepository
 import poke.rogue.helper.databinding.ActivityBiomeBinding
 import poke.rogue.helper.presentation.base.toolbar.ToolbarActivity
 import poke.rogue.helper.presentation.biome.detail.BiomeDetailActivity
 import poke.rogue.helper.presentation.biome.model.BiomeUiModel
+import poke.rogue.helper.presentation.biome.model.toUi
 import poke.rogue.helper.presentation.util.context.startActivity
 import poke.rogue.helper.presentation.util.logClickEvent
 import poke.rogue.helper.presentation.util.repeatOnStarted
 import poke.rogue.helper.presentation.util.view.GridSpacingItemDecoration
 import poke.rogue.helper.presentation.util.view.dp
+import timber.log.Timber
 
 class BiomeActivity : ToolbarActivity<ActivityBiomeBinding>(R.layout.activity_biome) {
     private val logger: AnalyticsLogger = analyticsLogger()
-    private val viewModel by viewModels<BiomeViewModel>()
-    private val adapter: BiomeAdapter by lazy { BiomeAdapter(viewModel) }
+    private val viewModel by viewModels<BiomeViewModel> {
+        BiomeViewModel.factory(
+            DefaultBiomeRepository.instance(),
+        )
+    }
+    private val biomeAdapter: BiomeAdapter by lazy { BiomeAdapter(viewModel) }
     override val toolbar: Toolbar
         get() = binding.toolbarBiome
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding.vm = viewModel
-        binding.lifecycleOwner = this
 
+        initView()
         initAdapter()
         initObservers()
     }
 
-    private fun initAdapter() {
-        val decoration = GridSpacingItemDecoration(2, 9.dp, false)
-        binding.rvBiomeList.adapter = adapter
-        BiomeUiModel.DUMMYS.forEach {
-            adapter.submitList(BiomeUiModel.DUMMYS)
-        }
+    private fun initView() {
+        binding.vm = viewModel
+        binding.lifecycleOwner = this
+        viewModel.updateBiomeList()
+    }
 
-        binding.rvBiomeList.addItemDecoration(decoration)
+    private fun initAdapter() {
+        binding.rvBiomeList.apply {
+            adapter = biomeAdapter
+            addItemDecoration(
+                GridSpacingItemDecoration(
+                    2,
+                    9.dp,
+                    false
+                )
+            )
+        }
     }
 
     private fun initObservers() {
@@ -48,6 +64,20 @@ class BiomeActivity : ToolbarActivity<ActivityBiomeBinding>(R.layout.activity_bi
                 startActivity<BiomeDetailActivity> {
                     putExtra(BiomeDetailActivity.BIOME_ID, biomeId)
                     logger.logClickEvent(NAVIGATE_TO_BIOME_DETAIL)
+                }
+            }
+        }
+
+        repeatOnStarted {
+            viewModel.biome.collect { biome ->
+                when (biome) {
+                    is BiomeUiState.Loading -> {
+                        Timber.d("Loading")
+                    }
+
+                    is BiomeUiState.Success -> {
+                        biomeAdapter.submitList(biome.data.toUi())
+                    }
                 }
             }
         }
--- android/app/src/main/java/poke/rogue/helper/presentation/biome/BiomeViewModel.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/biome/BiomeViewModel.kt
@@ -1,23 +1,49 @@
 package poke.rogue.helper.presentation.biome
 
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import poke.rogue.helper.analytics.AnalyticsLogger
 import poke.rogue.helper.analytics.analyticsLogger
+import poke.rogue.helper.data.model.Biome
+import poke.rogue.helper.data.repository.BiomeRepository
+import poke.rogue.helper.presentation.base.BaseViewModelFactory
 import poke.rogue.helper.presentation.base.error.ErrorHandleViewModel
 
-class BiomeViewModel(logger: AnalyticsLogger = analyticsLogger()) :
+class BiomeViewModel(
+    private val biomeRepository: BiomeRepository,
+    logger: AnalyticsLogger = analyticsLogger()
+) :
     ErrorHandleViewModel(logger),
     BiomeUiEventHandler {
+    private val _biome = MutableStateFlow<BiomeUiState<List<Biome>>>(BiomeUiState.Loading)
+    val biome = _biome.asStateFlow()
+
     private val _navigationToDetailEvent = MutableSharedFlow<String>()
     val navigationToDetailEvent: SharedFlow<String> = _navigationToDetailEvent.asSharedFlow()
+
+    fun updateBiomeList() {
+        viewModelScope.launch(errorHandler) {
+            val biomes = biomeRepository.biomes()
+            _biome.value = BiomeUiState.Success(biomes)
+        }
+    }
 
     override fun navigateToDetail(biomeId: String) {
         viewModelScope.launch {
             _navigationToDetailEvent.emit(biomeId)
         }
+    }
+
+    companion object {
+        fun factory(biomeRepository: BiomeRepository): ViewModelProvider.Factory =
+            BaseViewModelFactory {
+                BiomeViewModel(biomeRepository)
+            }
     }
 }
