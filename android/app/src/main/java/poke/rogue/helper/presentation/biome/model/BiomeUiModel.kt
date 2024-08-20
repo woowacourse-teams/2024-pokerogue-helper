4492e07499f2643b703dead3eb190caff8568451
kkosang <eiwoqp77@gmail.com>
2024-08-21 01:56:02 +0900

    refactor: uiModel id값 변경

--- android/app/src/main/java/poke/rogue/helper/presentation/biome/model/BiomeUiModel.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/biome/model/BiomeUiModel.kt
@@ -14,49 +14,49 @@ data class BiomeUiModel(
         val DUMMYS: List<BiomeUiModel> =
             listOf(
                 BiomeUiModel(
-                    "1",
+                    "grass",
                     "풀숲",
                     "https://wiki.pokerogue.net/_media/ko:biomes:ko_grassy_fields_bg.png?w=200&tok=745c5b",
                     types = listOf(TypeUiModel.GRASS, TypeUiModel.POISON),
                 ),
                 BiomeUiModel(
-                    "2",
+                    "tall_grass",
                     "높은 풀숲",
                     "https://wiki.pokerogue.net/_media/ko:biomes:ko_tall_grass_bg.png?w=200&tok=b3497c",
                     types = listOf(TypeUiModel.BUG),
                 ),
                 BiomeUiModel(
-                    "3",
+                    "cave",
                     "동굴",
                     "https://wiki.pokerogue.net/_media/ko:biomes:ko_cave_bg.png?w=200&tok=905d8b",
                     types = listOf(TypeUiModel.GRASS),
                 ),
                 BiomeUiModel(
-                    "4",
+                    "badlands",
                     "악지",
                     "https://wiki.pokerogue.net/_media/ko:biomes:ko_badlands_bg.png?w=200&tok=37d070",
                     types = listOf(TypeUiModel.DARK, TypeUiModel.FIGHTING),
                 ),
                 BiomeUiModel(
-                    "5",
+                    "factory",
                     "공장",
                     "https://wiki.pokerogue.net/_media/en:biomes:en_factory_bg.png?w=200&tok=5c1cb5",
                     types = listOf(TypeUiModel.DARK, TypeUiModel.FIGHTING),
                 ),
                 BiomeUiModel(
-                    "6",
+                    "construction_site",
                     "공사장",
                     "https://wiki.pokerogue.net/_media/en:biomes:en_construction_site_bg.png?w=200&tok=8cf671",
                     types = listOf(TypeUiModel.NORMAL, TypeUiModel.GROUND),
                 ),
                 BiomeUiModel(
-                    "7",
+                    "snowy_forest",
                     "눈덮힌 숲",
                     "https://wiki.pokerogue.net/_media/en:biomes:en_snowy_forest_bg.png?w=200&tok=2fe712",
                     types = listOf(TypeUiModel.ICE, TypeUiModel.STEEL),
                 ),
                 BiomeUiModel(
-                    "8",
+                    "ice_cave",
                     "얼음동굴",
                     "https://wiki.pokerogue.net/_media/en:biomes:en_ice_cave_bg.png?w=200&tok=aa8cf1",
                     types = listOf(TypeUiModel.ICE, TypeUiModel.WATER),
