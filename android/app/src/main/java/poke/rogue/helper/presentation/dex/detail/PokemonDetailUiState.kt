9ff2ec47d1056721a8865870c1ecc57e06071c6d
sh1mj1 <7wlgns@gmail.com>
2024-08-21 11:33:13 +0900

    feat(PokemonDetailUiState2): 포켓몬 상세 ui state 2

--- android/app/src/main/java/poke/rogue/helper/presentation/dex/detail/PokemonDetailUiState.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/dex/detail/PokemonDetailUiState.kt
@@ -1,7 +1,9 @@
 package poke.rogue.helper.presentation.dex.detail
 
 import poke.rogue.helper.data.model.PokemonDetail
+import poke.rogue.helper.data.model.PokemonDetail2
 import poke.rogue.helper.data.model.PokemonDetailSkills
+import poke.rogue.helper.data.model.PokemonDetailSkills2
 import poke.rogue.helper.data.model.Stat
 import poke.rogue.helper.presentation.dex.model.PokemonDetailAbilityUiModel
 import poke.rogue.helper.presentation.dex.model.PokemonUiModel
@@ -22,8 +24,31 @@ sealed interface PokemonDetailUiState {
     data object IsLoading : PokemonDetailUiState
 }
 
+sealed interface PokemonDetailUiState2{
+    data class Success(
+        val pokemon: PokemonUiModel,
+        val stats: List<StatUiModel>,
+        val abilities: List<PokemonDetailAbilityUiModel>,
+        val skills: PokemonDetailSkills2,
+        val height: Float,
+        val weight: Float,
+    ): PokemonDetailUiState2
+
+    data object IsLoading: PokemonDetailUiState2
+}
+
 fun PokemonDetail.toUi(): PokemonDetailUiState.Success =
     PokemonDetailUiState.Success(
+        pokemon = pokemon.toUi(),
+        stats = stats.map(Stat::toUi),
+        abilities = abilities.toPokemonDetailUi(),
+        skills = skills,
+        height = height.toFloat(),
+        weight = weight.toFloat(),
+    )
+
+fun PokemonDetail2.toUi(): PokemonDetailUiState2.Success =
+    PokemonDetailUiState2.Success(
         pokemon = pokemon.toUi(),
         stats = stats.map(Stat::toUi),
         abilities = abilities.toPokemonDetailUi(),
