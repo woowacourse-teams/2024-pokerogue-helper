5053cb724bf7e07ad9e08c363c25f38653ee51e3
sh1mj1 <7wlgns@gmail.com>
2024-08-21 11:16:21 +0900

    feat(PokemonDetail2): 포켓몬 상세의 프로퍼티 수정, dto -> data 매핑

--- android/data/src/main/java/poke/rogue/helper/data/model/PokemonDetail.kt
+++ android/data/src/main/java/poke/rogue/helper/data/model/PokemonDetail.kt
@@ -1,7 +1,10 @@
 package poke.rogue.helper.data.model
 
 import poke.rogue.helper.remote.dto.response.ability.AbilityResponse
+import poke.rogue.helper.remote.dto.response.biom.PokemonBiomeResponse
 import poke.rogue.helper.remote.dto.response.pokemon.PokemonDetailResponse
+import poke.rogue.helper.remote.dto.response.pokemon.PokemonDetailResponse2
+import poke.rogue.helper.remote.dto.response.pokemon.PokemonSkillResponse
 import poke.rogue.helper.remote.dto.response.type.PokemonTypeResponse
 
 data class PokemonDetail(
@@ -16,37 +19,82 @@ data class PokemonDetail(
     val weight: Double,
 )
 
+data class PokemonDetail2(
+    val pokemon: Pokemon,
+    val abilities: List<PokemonDetailAbility>,
+    val stats: List<Stat>,
+    val pokemonCategory: PokemonCategory,
+    val evolutions: List<Evolution>,
+    val skills: PokemonDetailSKills2,
+    val biomes: List<PokemonBiome>,
+    val height: Double,
+    val weight: Double,
+)
+
 // TODO: 서버에서 String 으로 주면 id: Long -> id.String() 으로 변경
 fun PokemonDetailResponse.toData(id: Long): PokemonDetail =
     PokemonDetail(
         pokemon =
-            Pokemon(
-                id = id.toString(),
-                dexNumber = dexNumber,
-                name = name,
-                imageUrl = imageUrl,
-                types = types.map(PokemonTypeResponse::toData),
-            ),
+        Pokemon(
+            id = id.toString(),
+            dexNumber = dexNumber,
+            name = name,
+            imageUrl = imageUrl,
+            types = types.map(PokemonTypeResponse::toData),
+        ),
         abilities = abilities.map(AbilityResponse::toNewData),
         stats =
-            listOf(
-                Stat("hp", hp),
-                Stat("attack", attack),
-                Stat("defense", defense),
-                Stat("specialAttack", specialAttack),
-                Stat("specialDefense", specialDefense),
-                Stat("speed", speed),
-                Stat("total", totalStats),
-            ),
+        listOf(
+            Stat("hp", hp),
+            Stat("attack", attack),
+            Stat("defense", defense),
+            Stat("specialAttack", specialAttack),
+            Stat("specialDefense", specialDefense),
+            Stat("speed", speed),
+            Stat("total", totalStats),
+        ),
         pokemonCategory = PokemonCategory.EMPTY,
         evolutions = emptyList(),
         skills =
-            PokemonDetailSkills(
-                selfLearn = PokemonSkill.FAKE_SELF_LEARN_SKILLS,
-                tmLearn = PokemonSkill.FAKE_SELF_LEARN_SKILLS,
-                eggLearn = PokemonSkill.FAKE_EGG_LEARN_SKILLS,
-            ),
+        PokemonDetailSkills(
+            selfLearn = PokemonSkill.FAKE_SELF_LEARN_SKILLS,
+            tmLearn = PokemonSkill.FAKE_SELF_LEARN_SKILLS,
+            eggLearn = PokemonSkill.FAKE_EGG_LEARN_SKILLS,
+        ),
         biomes = PokemonBiome.DUMMYS,
+        height = height.toDouble(),
+        weight = weight.toDouble(),
+    )
+
+
+fun PokemonDetailResponse2.toData(id: String): PokemonDetail2 =
+    PokemonDetail2(
+        pokemon =
+        Pokemon(
+            id = id,
+            dexNumber = dexNumber,
+            name = name,
+            imageUrl = imageUrl,
+            types = types.map(PokemonTypeResponse::toData),
+        ),
+        abilities = abilities.toData(),
+        stats = listOf(
+            Stat("hp", hp),
+            Stat("attack", attack),
+            Stat("defense", defense),
+            Stat("specialAttack", specialAttack),
+            Stat("specialDefense", specialDefense),
+            Stat("speed", speed),
+            Stat("total", totalStats),
+        ),
+        pokemonCategory = PokemonCategory.EMPTY,
+        evolutions = evolutions.toData(),
+        skills = PokemonDetailSKills2(
+            selfLearn = selfLearnSkills.map(PokemonSkillResponse::toData),
+            tmLearn = selfLearnSkills.map(PokemonSkillResponse::toData),
+            eggLearn = eggSkills.map(PokemonSkillResponse::toData),
+        ),
+        biomes = biomes.map(PokemonBiomeResponse::toData),
         height = height.toDouble(),
         weight = weight.toDouble(),
     )
