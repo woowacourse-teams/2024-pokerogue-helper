0507bf15d7ea9c3a6bda9b1c21050ae4f3be9b3f
woojong <dwax1324@gmail.com>
2024-08-21 17:38:30 +0900

    refactor: 파라미터명 변경

--- backend/pokerogue/src/main/java/com/pokerogue/helper/type/dto/PokemonTypeResponse.java
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/type/dto/PokemonTypeResponse.java
@@ -3,7 +3,7 @@ package com.pokerogue.helper.type.dto;
 import com.pokerogue.helper.pokemon2.data.Pokemon;
 import com.pokerogue.helper.type.domain.PokemonType;
 
-public record PokemonTypeResponse(String pokemonTypeName, String pokemonTypeLogo) {
+public record PokemonTypeResponse(String typeName, String typeLogo) {
 
     public static PokemonTypeResponse from(PokemonType pokemonType) {
         return new PokemonTypeResponse(pokemonType.getName(), pokemonType.getImage());
