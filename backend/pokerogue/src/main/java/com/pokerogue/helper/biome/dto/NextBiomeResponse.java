67da632183df6c447dcd1a7a4e2e13b28fb890ca
unifolio0 <boho82ha@gmail.com>
2024-08-21 11:24:22 +0900

    style: 개행 추가

--- backend/pokerogue/src/main/java/com/pokerogue/helper/biome/dto/NextBiomeResponse.java
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/biome/dto/NextBiomeResponse.java
@@ -3,7 +3,14 @@ package com.pokerogue.helper.biome.dto;
 import com.pokerogue.helper.biome.data.Biome;
 import java.util.List;
 
-public record NextBiomeResponse(String id, String name, String image, String percent, List<BiomeTypeResponse> pokemonTypeResponses, List<BiomeTypeResponse> trainerTypeResponses) {
+public record NextBiomeResponse(
+        String id,
+        String name,
+        String image,
+        String percent,
+        List<BiomeTypeResponse> pokemonTypeResponses,
+        List<BiomeTypeResponse> trainerTypeResponses
+) {
 
     public static NextBiomeResponse of(Biome biome, String percent, List<BiomeTypeResponse> pokemonTypeResponses, List<BiomeTypeResponse> trainerTypeResponses) {
         return new NextBiomeResponse(biome.getId(), biome.getName(), biome.getImage(), percent, pokemonTypeResponses, trainerTypeResponses);
