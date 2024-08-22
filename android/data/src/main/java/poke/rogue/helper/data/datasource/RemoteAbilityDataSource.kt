b2aebbb9cab8ddcd4c7ba48f461f7465baac45a9
sh1mj1 <7wlgns@gmail.com>
2024-08-22 21:51:04 +0900

    feat(RemoteAbilityDataSource): 특성 목록 불러온다

--- android/data/src/main/java/poke/rogue/helper/data/datasource/RemoteAbilityDataSource.kt
+++ android/data/src/main/java/poke/rogue/helper/data/datasource/RemoteAbilityDataSource.kt
@@ -7,6 +7,7 @@ import poke.rogue.helper.data.exception.onFailure
 import poke.rogue.helper.data.model.Ability
 import poke.rogue.helper.data.model.AbilityDetail
 import poke.rogue.helper.data.model.toData
+import poke.rogue.helper.remote.dto.response.ability.AbilityResponse2
 import poke.rogue.helper.remote.injector.ServiceModule
 import poke.rogue.helper.remote.service.AbilityService
 
@@ -15,12 +16,12 @@ class RemoteAbilityDataSource(
     private val logger: AnalyticsLogger,
 ) {
     suspend fun abilities(): List<Ability> =
-        abilityService.abilities()
+        abilityService.abilities2()
             .onFailure {
                 logger.logError(throwable, "abilityService - abilities() 에서 발생")
             }
             .getOrThrow()
-            .map { it.toData() }
+            .map(AbilityResponse2::toData)
 
     // TODO: 서비스에서 실제로 string 값을 보내준다면 id.toLong 에서 toLong 을 제거합시다
     suspend fun abilityDetail(id: String): AbilityDetail =
--- android/data/src/main/java/poke/rogue/helper/data/model/Ability.kt
+++ android/data/src/main/java/poke/rogue/helper/data/model/Ability.kt
@@ -1,6 +1,7 @@
 package poke.rogue.helper.data.model
 
 import poke.rogue.helper.remote.dto.response.ability.AbilityResponse
+import poke.rogue.helper.remote.dto.response.ability.AbilityResponse2
 
 data class Ability(
     val id: String = "0",
@@ -13,5 +14,12 @@ fun AbilityResponse.toData(): Ability =
     Ability(
         id = id.toString(),
         title = title,
+        description = description,
+    )
+
+fun AbilityResponse2.toData(): Ability =
+    Ability(
+        id = id,
+        title = name,
         description = description,
     )
