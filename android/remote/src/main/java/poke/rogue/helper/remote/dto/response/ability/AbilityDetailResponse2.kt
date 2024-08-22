a4b2878c344857f1d88a3279cb52fe6233a3ae3b
sh1mj1 <7wlgns@gmail.com>
2024-08-22 21:42:03 +0900

    feat(AbilityDetailResponse2): property pokemons 타입 변경

--- android/remote/src/main/java/poke/rogue/helper/remote/dto/response/ability/AbilityDetailResponse2.kt
+++ android/remote/src/main/java/poke/rogue/helper/remote/dto/response/ability/AbilityDetailResponse2.kt
@@ -2,12 +2,11 @@ package poke.rogue.helper.remote.dto.response.ability
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import poke.rogue.helper.remote.dto.response.pokemon.PokemonResponse2
 
 @Serializable
 data class AbilityDetailResponse2(
     @SerialName("koName")
     val name: String,
     val description: String,
-    val pokemons: List<PokemonResponse2>,
+    val pokemons: List<AbilityPokemonResponse>,
 )
