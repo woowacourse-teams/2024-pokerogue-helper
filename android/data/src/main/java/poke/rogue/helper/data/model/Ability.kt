830cbd6a4d6a3aab27f3cfea85beba1cc9bc0295
sh1mj1 <7wlgns@gmail.com>
2024-08-22 22:18:59 +0900

    chore: 사용하지 않는 특성 응답 삭제

--- android/data/src/main/java/poke/rogue/helper/data/model/Ability.kt
+++ android/data/src/main/java/poke/rogue/helper/data/model/Ability.kt
@@ -1,6 +1,5 @@
 package poke.rogue.helper.data.model
 
-import poke.rogue.helper.remote.dto.response.ability.AbilityResponse
 import poke.rogue.helper.remote.dto.response.ability.AbilityResponse2
 
 data class Ability(
@@ -8,14 +7,6 @@ data class Ability(
     val title: String,
     val description: String,
 )
-
-// TODO: 서버에서 String 으로 주면 id.toString 제거해도 된다
-fun AbilityResponse.toData(): Ability =
-    Ability(
-        id = id.toString(),
-        title = title,
-        description = description,
-    )
 
 fun AbilityResponse2.toData(): Ability =
     Ability(
