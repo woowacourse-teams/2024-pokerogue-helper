5ad8b0bd7b1d189d51ebafb869c369b282a8fb5d
sh1mj1 <7wlgns@gmail.com>
2024-09-05 20:02:55 +0900

    feat(PokemonDetailUiState2): PokemonUiModel 을 가진다

--- android/app/src/main/java/poke/rogue/helper/presentation/dex/detail/PokemonDetailUiState.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/dex/detail/PokemonDetailUiState.kt
@@ -1,10 +1,12 @@
 package poke.rogue.helper.presentation.dex.detail
 
+import poke.rogue.helper.data.model.Biome
 import poke.rogue.helper.data.model.PokemonBiome
 import poke.rogue.helper.data.model.PokemonDetail
 import poke.rogue.helper.data.model.PokemonDetailSkills
 import poke.rogue.helper.data.model.Stat
 import poke.rogue.helper.presentation.dex.model.EvolutionsUiModel
+import poke.rogue.helper.presentation.dex.model.PokemonBiomeUiModel
 import poke.rogue.helper.presentation.dex.model.PokemonDetailAbilityUiModel
 import poke.rogue.helper.presentation.dex.model.PokemonUiModel
 import poke.rogue.helper.presentation.dex.model.StatUiModel
@@ -26,6 +28,22 @@ sealed interface PokemonDetailUiState {
     data object IsLoading : PokemonDetailUiState
 }
 
+sealed interface PokemonDetailUiState2 {
+    data class Success(
+        val pokemon: PokemonUiModel,
+        val stats: List<StatUiModel>,
+        val abilities: List<PokemonDetailAbilityUiModel>,
+        val evolutions: EvolutionsUiModel,
+        val skills: PokemonDetailSkills,
+        val height: Float,
+        val weight: Float,
+        val biomes: List<PokemonBiomeUiModel>,
+    ) : PokemonDetailUiState2
+
+    data object IsLoading : PokemonDetailUiState2
+}
+
+
 fun PokemonDetail.toUi(): PokemonDetailUiState.Success =
     PokemonDetailUiState.Success(
         pokemon = pokemon.toUi(),
@@ -37,3 +55,14 @@ fun PokemonDetail.toUi(): PokemonDetailUiState.Success =
         weight = weight.toFloat(),
         biomes = biomes,
     )
+
+fun PokemonDetail.toUi(allBiomes: List<Biome>): PokemonDetailUiState2.Success = PokemonDetailUiState2.Success(
+    pokemon = pokemon.toUi(),
+    stats = stats.map(Stat::toUi),
+    abilities = abilities.toPokemonDetailUi(),
+    evolutions = evolutions.toUi(),
+    skills = skills,
+    height = height.toFloat(),
+    weight = weight.toFloat(),
+    biomes = biomes.toUi(allBiomes),
+)
