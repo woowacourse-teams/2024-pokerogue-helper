803d7a04344879e836b953691baaecf0c72e63eb
sh1mj1 <7wlgns@gmail.com>
2024-09-05 20:05:41 +0900

    feat(PokemonDetailBiomeAdapter): PokemonDetailUiState2 에 대한 함수 추가

--- android/app/src/main/java/poke/rogue/helper/presentation/dex/detail/information/PokemonDetailBiomeViewHolder.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/dex/detail/information/PokemonDetailBiomeViewHolder.kt
@@ -4,12 +4,13 @@ import androidx.recyclerview.widget.RecyclerView
 import poke.rogue.helper.data.model.PokemonBiome
 import poke.rogue.helper.databinding.ItemPokemonDetailInformationBiomeBinding
 import poke.rogue.helper.presentation.dex.detail.PokemonDetailNavigateHandler
+import poke.rogue.helper.presentation.dex.model.PokemonBiomeUiModel
 
 class PokemonDetailBiomeViewHolder(
     private val binding: ItemPokemonDetailInformationBiomeBinding,
     private val onClickBiomeItem: PokemonDetailNavigateHandler,
 ) : RecyclerView.ViewHolder(binding.root) {
-    fun bind(biome: PokemonBiome) {
+    fun bind(biome: PokemonBiomeUiModel) {
         binding.apply {
             this.biome = biome
             uiEventHandler = onClickBiomeItem
