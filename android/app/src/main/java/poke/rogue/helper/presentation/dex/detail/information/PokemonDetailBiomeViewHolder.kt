a40f6e315068411afec914249744828fe47b0281
sh1mj1 <7wlgns@gmail.com>
2024-09-06 16:43:21 +0900

    ui(PokemonDetailBiomeViewHolder): 바이옴의 타입 정보 추가

--- android/app/src/main/java/poke/rogue/helper/presentation/dex/detail/information/PokemonDetailBiomeViewHolder.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/dex/detail/information/PokemonDetailBiomeViewHolder.kt
@@ -4,6 +4,7 @@ import androidx.recyclerview.widget.RecyclerView
 import poke.rogue.helper.databinding.ItemPokemonDetailInformationBiomeBinding
 import poke.rogue.helper.presentation.dex.detail.PokemonDetailNavigateHandler
 import poke.rogue.helper.presentation.dex.model.PokemonBiomeUiModel
+import poke.rogue.helper.presentation.util.view.dp
 
 class PokemonDetailBiomeViewHolder(
     private val binding: ItemPokemonDetailInformationBiomeBinding,
@@ -14,5 +15,22 @@ class PokemonDetailBiomeViewHolder(
             this.biome = biome
             uiEventHandler = onClickBiomeItem
         }
+
+        val typesLayout = binding.flBiomeTypeIcons
+        val biomeTypesAdapter =
+            PokemonDetailBiomeTypesAdapter(
+                context = binding.root.context,
+                viewGroup = typesLayout,
+            )
+        biomeTypesAdapter.addTypes(
+            types = biome.types,
+            spacingBetweenTypes = TYPES_SPACING,
+            iconSize = TYPE_ICON_SIZE,
+        )
+    }
+
+    companion object {
+        private val TYPES_SPACING = 5.dp
+        private val TYPE_ICON_SIZE = 18.dp
     }
 }
