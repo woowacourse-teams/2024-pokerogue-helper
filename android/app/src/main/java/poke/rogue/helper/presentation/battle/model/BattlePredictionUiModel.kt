8586f3cc369ca407a176f8db1a7c9c9a2dd78cdc
JoYehyun <jo990714@naver.com>
2024-09-09 18:57:07 +0900

    feat: 배수 값에 따라 색상 다르게 설정

--- android/app/src/main/java/poke/rogue/helper/presentation/battle/model/BattlePredictionUiModel.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/battle/model/BattlePredictionUiModel.kt
@@ -1,22 +1,40 @@
 package poke.rogue.helper.presentation.battle.model
 
+import androidx.annotation.ColorRes
+import poke.rogue.helper.R
 import poke.rogue.helper.data.model.BattlePrediction
+import poke.rogue.helper.presentation.battle.model.BattlePredictionUiModel.Companion.DEFAULT_NUMBER_FORMAT
 import poke.rogue.helper.presentation.battle.model.BattlePredictionUiModel.Companion.NO_EFFECT_VALUE
 
-data class BattlePredictionUiModel(val power: String, val accuracy: String, val multiplier: String, val calculatedResult: String) {
+data class BattlePredictionUiModel(
+    val power: String,
+    val accuracy: String,
+    val multiplier: String,
+    val calculatedResult: String,
+    @ColorRes val colorRes: Int,
+) {
     companion object {
         const val NO_EFFECT_VALUE = "-"
+        const val DEFAULT_NUMBER_FORMAT = "%.1f"
     }
 }
 
-fun BattlePrediction.toUi(format: String = "%.1f"): BattlePredictionUiModel {
+fun BattlePrediction.toUi(format: String = DEFAULT_NUMBER_FORMAT): BattlePredictionUiModel {
     val formattedPower = if (power < 0) NO_EFFECT_VALUE else power.toString()
     val formattedResult = if (calculatedResult < 0) NO_EFFECT_VALUE else String.format(format, calculatedResult)
+    val color =
+        when {
+            multiplier < 1.0 -> R.color.poke_grey_65
+            multiplier in 1.0..2.9 -> R.color.poke_red_20
+            multiplier >= 3 -> R.color.poke_green_20
+            else -> R.color.poke_white
+        }
 
     return BattlePredictionUiModel(
         power = formattedPower,
         accuracy = String.format(format, accuracy),
         multiplier = String.format(format, multiplier),
         calculatedResult = formattedResult,
+        colorRes = color,
     )
 }
