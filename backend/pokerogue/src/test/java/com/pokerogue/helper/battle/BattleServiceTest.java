5de11ba1f87b9db36ef9c4ea36d12a32825cf16f
unifolio0 <boho82ha@gmail.com>
2024-09-09 13:47:29 +0900

    test: findMovesByPokemon 테스트

--- backend/pokerogue/src/test/java/com/pokerogue/helper/battle/BattleServiceTest.java
+++ backend/pokerogue/src/test/java/com/pokerogue/helper/battle/BattleServiceTest.java
@@ -4,6 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.pokerogue.environment.service.ServiceTest;
+import com.pokerogue.helper.pokemon2.data.Pokemon;
+import com.pokerogue.helper.pokemon2.repository.Pokemon2Repository;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +15,23 @@ class BattleServiceTest extends ServiceTest {
 
     @Autowired
     private BattleService battleService;
+
+    @Autowired
+    private Pokemon2Repository pokemon2Repository;
+
+    @Test
+    @DisplayName("포켓몬의 기술(자력 기술, 머신 기술, 알 기술) 리스트를 조회한다.")
+    void findMovesByPokemon() {
+        List<String> pokemonIds = pokemon2Repository.findAll().values()
+                .stream()
+                .map(Pokemon::id)
+                .toList();
+
+        pokemonIds.forEach(pokemonId -> {
+            List<MoveResponse> moveResponses = battleService.findMovesByPokemon(pokemonId);
+            assertThat(moveResponses).isNotEmpty();
+        });
+    }
 
     @Test
     @DisplayName("배틀 예상 결과를 계산한다.")
