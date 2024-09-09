87b1600f276953871ef4a57dbde521da03920b1f
JoYehyun <jo990714@naver.com>
2024-09-09 17:52:29 +0900

    feat: 기술 위력이 마이너스 값일 경우, 화면 표시 변경

--- android/app/src/main/java/poke/rogue/helper/presentation/battle/model/BattlePredictionUiModel.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/battle/model/BattlePredictionUiModel.kt
@@ -1,13 +1,22 @@
 package poke.rogue.helper.presentation.battle.model
 
 import poke.rogue.helper.data.model.BattlePrediction
+import poke.rogue.helper.presentation.battle.model.BattlePredictionUiModel.Companion.NO_EFFECT_VALUE
 
-data class BattlePredictionUiModel(val power: String, val accuracy: String, val multiplier: String, val calculatedResult: String)
+data class BattlePredictionUiModel(val power: String, val accuracy: String, val multiplier: String, val calculatedResult: String) {
+    companion object {
+        const val NO_EFFECT_VALUE = "-"
+    }
+}
 
-fun BattlePrediction.toUi(format: String = "%.1f"): BattlePredictionUiModel =
-    BattlePredictionUiModel(
-        power = power.toString(),
+fun BattlePrediction.toUi(format: String = "%.1f"): BattlePredictionUiModel {
+    val formattedPower = if (power < 0) NO_EFFECT_VALUE else power.toString()
+    val formattedResult = if (calculatedResult < 0) NO_EFFECT_VALUE else String.format(format, calculatedResult)
+
+    return BattlePredictionUiModel(
+        power = formattedPower,
         accuracy = String.format(format, accuracy),
         multiplier = String.format(format, multiplier),
-        calculatedResult = String.format(format, calculatedResult),
+        calculatedResult = formattedResult,
     )
+}
