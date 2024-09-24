2656c990263e05c6a16ecfec957a8bb870d02b85
jongmee <ra123221@gmail.com>
2024-09-24 12:52:24 +0900

    remove: 남아 있는 jpa entity 삭제

--- backend/pokerogue/src/main/java/com/pokerogue/helper/article/repository/ArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pokerogue.helper.article.repository;
-
-import com.pokerogue.helper.article.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-}
--- backend/pokerogue/src/main/java/com/pokerogue/helper/type/dto/PokemonMatchingAndTypeResponse.java
+++
@@ -1,6 +0,0 @@
-package com.pokerogue.helper.type.dto;
-
-import java.util.List;
-
-public record PokemonMatchingAndTypeResponse(List<PokemonMatchingResponse> matching, List<PokemonTypeResponse> images) {
-}
