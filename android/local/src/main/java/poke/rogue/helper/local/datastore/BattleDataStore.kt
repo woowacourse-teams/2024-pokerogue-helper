8cec8bc84b9149a0107edf763ef94b0e55f5626b
JoYehyun <jo990714@naver.com>
2024-09-24 16:05:56 +0900

    refactor: 아이디 저장값 분리

--- android/local/src/main/java/poke/rogue/helper/local/datastore/BattleDataStore.kt
+++ android/local/src/main/java/poke/rogue/helper/local/datastore/BattleDataStore.kt
@@ -9,11 +9,7 @@ import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 
-@Serializable
 data class SavedPokemonWithSkill(val pokemonId: String, val skillId: String)
 
 class BattleDataStore(private val context: Context) {
@@ -23,37 +19,38 @@ class BattleDataStore(private val context: Context) {
         pokemonId: String,
         skillId: String,
     ) {
-        val data = SavedPokemonWithSkill(pokemonId, skillId)
-        val saved = Json.encodeToString(data)
         context.dataStore.edit {
-            it[POKEMON_WITH_SKILL_SELECTION_KEY] = saved
+            it[PAIR_POKEMON_SELECTION_KEY] = pokemonId
+            it[PAIR_SKILL_SELECTION_KEY] = skillId
         }
     }
 
     suspend fun savePokemon(pokemonId: String) {
         context.dataStore.edit {
-            it[POKEMON_SELECTION_KEY] = pokemonId
+            it[SINGLE_POKEMON_SELECTION_KEY] = pokemonId
         }
     }
 
     fun pokemonWithSkillId(): Flow<SavedPokemonWithSkill?> =
         context.dataStore.data.map { preference ->
-            val data = preference[POKEMON_WITH_SKILL_SELECTION_KEY]
-            if (data != null) {
-                Json.decodeFromString<SavedPokemonWithSkill>(data)
-            } else {
+            val pokemonId = preference[PAIR_POKEMON_SELECTION_KEY]
+            val skillId = preference[PAIR_SKILL_SELECTION_KEY]
+            if (pokemonId == null || skillId == null) {
                 null
+            } else {
+                SavedPokemonWithSkill(pokemonId, skillId)
             }
         }
 
     fun pokemonId(): Flow<String?> =
         context.dataStore.data.map { preferences ->
-            preferences[POKEMON_SELECTION_KEY]
+            preferences[SINGLE_POKEMON_SELECTION_KEY]
         }
 
     private companion object {
         const val BATTLE_PREFERENCE_NAME = "battle"
-        val POKEMON_WITH_SKILL_SELECTION_KEY = stringPreferencesKey("pokemon_with_skill_selection")
-        val POKEMON_SELECTION_KEY = stringPreferencesKey("pokemon_selection")
+        val PAIR_POKEMON_SELECTION_KEY = stringPreferencesKey("pair_pokemon_selection")
+        val PAIR_SKILL_SELECTION_KEY = stringPreferencesKey("pair_skill_selection")
+        val SINGLE_POKEMON_SELECTION_KEY = stringPreferencesKey("single_pokemon_selection")
     }
 }
