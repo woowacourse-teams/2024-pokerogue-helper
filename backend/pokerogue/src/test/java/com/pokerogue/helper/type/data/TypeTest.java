7b637487917b59b2014e78c8e062f373b18cbe34
Eugene Jang <kyunellroll@gmail.com>
2024-09-26 03:12:13 +0900

    feat(Type): 들어온 값을 찾아주는 메서드 생성
    - 정상적으로 찾아주는지 확인하는 테스트 작성

---
+++ backend/pokerogue/src/test/java/com/pokerogue/helper/type/data/TypeTest.java
@@ -0,0 +1,29 @@
+package com.pokerogue.helper.type.data;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.pokerogue.helper.global.exception.ErrorMessage;
+import com.pokerogue.helper.global.exception.GlobalCustomException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class TypeTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"aaa", "polla", "jdj"})
+    @DisplayName("존재하지 않는 타입인 경우 에러를 발생한다.")
+    void convertFrom_WhenNotExist(String inputType) {
+        assertThatThrownBy(() -> Type.convertFrom(inputType))
+                .isInstanceOf(GlobalCustomException.class)
+                .hasMessage(ErrorMessage.POKEMON_TYPE_NOT_FOUND.getMessage());
+    }
+
+    @Test
+    @DisplayName("일치하는 타입을 찾아온다.")
+    void convertFrom() {
+        assertThat(Type.convertFrom("dragon")).isEqualTo(Type.DRAGON);
+    }
+}
