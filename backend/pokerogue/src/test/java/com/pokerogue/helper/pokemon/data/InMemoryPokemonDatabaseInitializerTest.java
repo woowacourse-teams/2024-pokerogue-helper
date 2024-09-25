a67fd60b39add7b2ded5c6b0b5931700f0b80124
woojong <dwax1324@gmail.com>
2024-09-25 21:55:02 +0900

    refactor: db 이니셜라이저 테스트 삭제

--- backend/pokerogue/src/test/java/com/pokerogue/helper/pokemon/data/InMemoryPokemonDatabaseInitializerTest.java
+++ backend/pokerogue/src/test/java/com/pokerogue/helper/pokemon/data/InMemoryPokemonDatabaseInitializerTest.java
@@ -1,47 +1,47 @@
-package com.pokerogue.helper.pokemon.data;
-
-import com.pokerogue.helper.ability.repository.InMemoryAbilityRepository;
-import com.pokerogue.helper.pokemon.config.PokemonDatabaseInitializer;
-import com.pokerogue.helper.pokemon.repository.EvolutionRepository;
-import com.pokerogue.helper.pokemon.repository.InMemoryPokemonRepository;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.boot.DefaultApplicationArguments;
-
-class InMemoryPokemonDatabaseInitializerTest {
-
-    @Test
-    @DisplayName("포켓몬 정보가 지정된 개수만큼 저장된다")
-    void savePokemonCount() {
-        InMemoryPokemonRepository inMemoryPokemonRepository = new InMemoryPokemonRepository();
-        PokemonDatabaseInitializer pokemonDatabaseInitializer = new PokemonDatabaseInitializer(
-                inMemoryPokemonRepository,
-                new EvolutionRepository(),
-                new InMemoryAbilityRepository()
-        );
-
-        pokemonDatabaseInitializer.run(new DefaultApplicationArguments());
-
-        Assertions.assertThat(inMemoryPokemonRepository.findAll()).hasSize(1268);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"bulbasaur", "chikorita", "wailmer", "virizion", "golisopod", "melmetal", "spidops",
-            "hydrapple", "alola_exeggutor"})
-    @DisplayName("포켓몬의 저장된 이름을 확인한다")
-    void savePokemonNames(String name) {
-        InMemoryPokemonRepository inMemoryPokemonRepository = new InMemoryPokemonRepository();
-        PokemonDatabaseInitializer pokemonDatabaseInitializer = new PokemonDatabaseInitializer(
-                inMemoryPokemonRepository,
-                new EvolutionRepository(),
-                new InMemoryAbilityRepository()
-        );
-
-        pokemonDatabaseInitializer.run(new DefaultApplicationArguments());
-
-        Assertions.assertThat(inMemoryPokemonRepository.findById(name)).isNotNull();
-    }
-}
+//package com.pokerogue.helper.pokemon.data;
+//
+//import com.pokerogue.helper.ability.repository.InMemoryAbilityRepository;
+//import com.pokerogue.helper.pokemon.config.PokemonDatabaseInitializer;
+//import com.pokerogue.helper.pokemon.repository.EvolutionRepository;
+//import com.pokerogue.helper.pokemon.repository.InMemoryPokemonRepository;
+//import org.assertj.core.api.Assertions;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.params.ParameterizedTest;
+//import org.junit.jupiter.params.provider.ValueSource;
+//import org.springframework.boot.DefaultApplicationArguments;
+//
+//class InMemoryPokemonDatabaseInitializerTest {
+//
+//    @Test
+//    @DisplayName("포켓몬 정보가 지정된 개수만큼 저장된다")
+//    void savePokemonCount() {
+//        InMemoryPokemonRepository inMemoryPokemonRepository = new InMemoryPokemonRepository();
+//        PokemonDatabaseInitializer pokemonDatabaseInitializer = new PokemonDatabaseInitializer(
+//                inMemoryPokemonRepository,
+//                new EvolutionRepository(),
+//                new InMemoryAbilityRepository()
+//        );
+//
+//        pokemonDatabaseInitializer.run(new DefaultApplicationArguments());
+//
+//        Assertions.assertThat(inMemoryPokemonRepository.findAll()).hasSize(1268);
+//    }
+//
+//    @ParameterizedTest
+//    @ValueSource(strings = {"bulbasaur", "chikorita", "wailmer", "virizion", "golisopod", "melmetal", "spidops",
+//            "hydrapple", "alola_exeggutor"})
+//    @DisplayName("포켓몬의 저장된 이름을 확인한다")
+//    void savePokemonNames(String name) {
+//        InMemoryPokemonRepository inMemoryPokemonRepository = new InMemoryPokemonRepository();
+//        PokemonDatabaseInitializer pokemonDatabaseInitializer = new PokemonDatabaseInitializer(
+//                inMemoryPokemonRepository,
+//                new EvolutionRepository(),
+//                new InMemoryAbilityRepository()
+//        );
+//
+//        pokemonDatabaseInitializer.run(new DefaultApplicationArguments());
+//
+//        Assertions.assertThat(inMemoryPokemonRepository.findById(name)).isNotNull();
+//    }
+//}
