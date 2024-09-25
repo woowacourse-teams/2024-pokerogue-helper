4e644e1627bef76ae9785c900749cf7924e8c148
unifolio0 <boho82ha@gmail.com>
2024-09-25 17:39:40 +0900

    test: 테스트 작성

--- backend/pokerogue/src/test/java/com/pokerogue/helper/biome/service/BiomeServiceTest.java
+++ backend/pokerogue/src/test/java/com/pokerogue/helper/biome/service/BiomeServiceTest.java
@@ -1,8 +1,10 @@
 package com.pokerogue.helper.biome.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.pokerogue.environment.service.ServiceTest;
+import com.pokerogue.helper.biome.dto.BiomeDetailResponse;
 import com.pokerogue.helper.biome.dto.BiomeResponse;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -20,5 +22,21 @@ class BiomeServiceTest extends ServiceTest {
         List<BiomeResponse> biomes = biomeService.findBiomes();
 
         assertThat(biomes.size()).isEqualTo(35);
+    }
+
+    @Test
+    @DisplayName("단일 바이옴 정보를 불러온다")
+    void findBiome() {
+        BiomeDetailResponse biomeDetailResponse = biomeService.findBiome("fairy_cave");
+
+        assertAll(
+                () -> assertThat(biomeDetailResponse.id()).isEqualTo("fairy_cave"),
+                () -> assertThat(biomeDetailResponse.name()).isEqualTo("페어리 동굴"),
+                () -> assertThat(biomeDetailResponse.image()).contains("/biome/fairy_cave.png"),
+                () -> assertThat(biomeDetailResponse.wildPokemons()).hasSize(5),
+                () -> assertThat(biomeDetailResponse.bossPokemons()).hasSize(4),
+                () -> assertThat(biomeDetailResponse.trainerPokemons()).hasSize(3),
+                () -> assertThat(biomeDetailResponse.nextBiomes()).hasSize(2)
+        );
     }
 }
--- backend/pokerogue/src/test/java/com/pokerogue/helper/move/service/MoveServiceTest.java
+++ backend/pokerogue/src/test/java/com/pokerogue/helper/move/service/MoveServiceTest.java
@@ -1,6 +1,7 @@
 package com.pokerogue.helper.move.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.pokerogue.environment.service.ServiceTest;
 import com.pokerogue.helper.move.dto.MoveResponse;
@@ -20,5 +21,22 @@ class MoveServiceTest extends ServiceTest {
         List<MoveResponse> movesByPokemon = moveService.findMovesByPokemon(1);
 
         assertThat(movesByPokemon.size()).isEqualTo(72);
+    }
+
+    @Test
+    void name() {
+        MoveResponse moveResponse = moveService.findMove("earth_power");
+
+        assertAll(
+                () -> assertThat(moveResponse.id()).isEqualTo("earth_power"),
+                () -> assertThat(moveResponse.name()).isEqualTo("대지의힘"),
+                () -> assertThat(moveResponse.typeEngName()).isEqualTo("ground"),
+                () -> assertThat(moveResponse.typeLogo()).contains("type/ground"),
+                () -> assertThat(moveResponse.categoryEngName()).isEqualTo("special"),
+                () -> assertThat(moveResponse.categoryLogo()).contains("move-category/special.png"),
+                () -> assertThat(moveResponse.power()).isEqualTo(90),
+                () -> assertThat(moveResponse.accuracy()).isEqualTo(100),
+                () -> assertThat(moveResponse.effect()).isEqualTo("상대의 발밑에 대지의 힘을 방출한다. 상대의 특수방어를 떨어뜨릴 때가 있다.")
+        );
     }
 }
