be873926d7063078f226254a10f153b85848333d
woojong <dwax1324@gmail.com>
2024-09-26 20:47:09 +0900

    refactor: biomeDto 빈값일시 Empty반환

--- backend/pokerogue/src/main/java/com/pokerogue/helper/pokemon/dto/PokemonBiomeResponse.java
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/pokemon/dto/PokemonBiomeResponse.java
@@ -1,5 +1,17 @@
 package com.pokerogue.helper.pokemon.dto;
 
+import com.pokerogue.helper.biome.data.Biome;
+import java.util.Optional;
+
 public record PokemonBiomeResponse(String id, String name, String image) {
-    public static final PokemonBiomeResponse EMPTY = new PokemonBiomeResponse("", "", "");
+    private static final PokemonBiomeResponse EMPTY = new PokemonBiomeResponse("", "", "");
+
+    public static PokemonBiomeResponse from(Optional<Biome> optionalBiome) {
+        if (optionalBiome.isEmpty()) {
+            return EMPTY;
+        }
+        Biome biome = optionalBiome.get();
+
+        return new PokemonBiomeResponse(biome.getId(), biome.getKoName(), biome.getImage());
+    }
 }
