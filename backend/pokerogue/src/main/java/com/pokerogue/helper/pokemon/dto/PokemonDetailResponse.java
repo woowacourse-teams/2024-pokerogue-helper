acec3a9487c63068972ed72aa7771f614893a4a9
woojong <dwax1324@gmail.com>
2024-09-26 20:47:44 +0900

    refactor: 포켓몬 dto 정적 팩토리 메서드 추가

--- backend/pokerogue/src/main/java/com/pokerogue/helper/pokemon/dto/PokemonDetailResponse.java
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/pokemon/dto/PokemonDetailResponse.java
@@ -1,5 +1,6 @@
 package com.pokerogue.helper.pokemon.dto;
 
+import com.pokerogue.helper.pokemon.data.Pokemon;
 import com.pokerogue.helper.type.dto.PokemonTypeResponse;
 import java.util.List;
 
@@ -25,7 +26,42 @@ public record PokemonDetailResponse(
         Double height,
         EvolutionResponses evolutions,
         List<MoveResponse> moves,
-        List<MoveResponse> eggMoveResponses,
+        List<EggMoveResponse> eggMoveResponses,
         List<PokemonBiomeResponse> biomes
 ) {
+    public static PokemonDetailResponse from(
+            Pokemon pokemon,
+            List<PokemonTypeResponse> pokemonTypeResponses,
+            List<PokemonAbilityResponse> pokemonAbilityResponses,
+            List<MoveResponse> moveResponse,
+            List<EggMoveResponse> eggMoveResponse,
+            List<PokemonBiomeResponse> biomeResponse,
+            EvolutionResponses evolutionResponses
+    ) {
+        return new PokemonDetailResponse(
+                pokemon.getId(),
+                (long) pokemon.getPokedexNumber(),
+                pokemon.getKoName(),
+                pokemon.getImageId(),
+                pokemonTypeResponses,
+                pokemonAbilityResponses,
+                pokemon.getBaseTotal(),
+                pokemon.getHp(),
+                pokemon.getAttack(),
+                pokemon.getDefense(),
+                pokemon.getSpecialAttack(),
+                pokemon.getSpecialDefense(),
+                pokemon.getSpeed(),
+                pokemon.isLegendary(),
+                pokemon.isSubLegendary(),
+                pokemon.isMythical(),
+                pokemon.isCanChangeForm(),
+                pokemon.getWeight(),
+                pokemon.getHeight(),
+                evolutionResponses,
+                moveResponse,
+                eggMoveResponse,
+                biomeResponse
+        );
+    }
 }
