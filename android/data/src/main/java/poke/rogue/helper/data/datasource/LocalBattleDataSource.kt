59efa72c15e411d87d9841db3787d13ae829225d
Yehyun Jo <81362348+JoYehyun99@users.noreply.github.com>
2024-09-26 10:03:50 +0900

    [1.1.0/AN_FEAT] 배틀 선택 데이터 저장 (#324)
    * chore: DataStore 의존성 설정
    
    * feat: Datastore 저장 로직 추가
    
    * feat: 저장된 선택 데이터 UI 에 반영
    
    * refactor: Skill 캐싱
    
    * refactor: Test 객체 수정
    
    * refactor: context 참조 수정
    
    * refactor: 리팩토링 리뷰 반영
    
    * refactor: 아이디 저장값 분리
    
    * refactor: Map 형태 변경

---
+++ android/data/src/main/java/poke/rogue/helper/data/datasource/LocalBattleDataSource.kt
@@ -0,0 +1,37 @@
+package poke.rogue.helper.data.datasource
+
+import android.content.Context
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import poke.rogue.helper.data.model.PokemonWithSkillIds
+import poke.rogue.helper.data.model.toData
+import poke.rogue.helper.local.datastore.BattleDataStore
+
+class LocalBattleDataSource(private val battleDataStore: BattleDataStore) {
+    suspend fun savePokemonWithSkill(
+        pokemonId: String,
+        skillId: String,
+    ) {
+        battleDataStore.savePokemonWithSkill(pokemonId, skillId)
+    }
+
+    suspend fun savePokemon(pokemonId: String) {
+        battleDataStore.savePokemon(pokemonId)
+    }
+
+    fun pokemonWithSkill(): Flow<PokemonWithSkillIds?> = battleDataStore.pokemonWithSkillId().map { it?.toData() }
+
+    fun pokemonId(): Flow<String?> = battleDataStore.pokemonId()
+
+    companion object {
+        private var instance: LocalBattleDataSource? = null
+
+        fun instance(context: Context): LocalBattleDataSource {
+            return instance ?: LocalBattleDataSource(
+                BattleDataStore(context),
+            ).also {
+                instance = it
+            }
+        }
+    }
+}
--- android/data/src/main/java/poke/rogue/helper/data/repository/DefaultBattleRepository.kt
+++ android/data/src/main/java/poke/rogue/helper/data/repository/DefaultBattleRepository.kt
@@ -1,14 +1,31 @@
 package poke.rogue.helper.data.repository
 
+import android.content.Context
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import poke.rogue.helper.data.datasource.LocalBattleDataSource
 import poke.rogue.helper.data.datasource.RemoteBattleDataSource
 import poke.rogue.helper.data.model.BattlePrediction
 import poke.rogue.helper.data.model.BattleSkill
+import poke.rogue.helper.data.model.Pokemon
+import poke.rogue.helper.data.model.PokemonWithSkill
 import poke.rogue.helper.data.model.Weather
 
-class DefaultBattleRepository(private val remoteBattleDataSource: RemoteBattleDataSource) : BattleRepository {
+class DefaultBattleRepository(
+    private val localBattleDataSource: LocalBattleDataSource,
+    private val remoteBattleDataSource: RemoteBattleDataSource,
+    private val pokemonRepository: DexRepository,
+) : BattleRepository {
+    private val cachedSkills: MutableMap<Long, List<BattleSkill>> = mutableMapOf()
+
     override suspend fun weathers(): List<Weather> = remoteBattleDataSource.weathers()
 
-    override suspend fun availableSkills(dexNumber: Long): List<BattleSkill> = remoteBattleDataSource.availableSkills(dexNumber).distinct()
+    override suspend fun availableSkills(dexNumber: Long): List<BattleSkill> =
+        cachedSkills[dexNumber] ?: run {
+            val skills = remoteBattleDataSource.availableSkills(dexNumber).distinct()
+            cachedSkills[dexNumber] = skills
+            skills
+        }
 
     override suspend fun calculatedBattlePrediction(
         weatherId: String,
@@ -23,11 +40,46 @@ class DefaultBattleRepository(private val remoteBattleDataSource: RemoteBattleDa
             opponentPokemonId = opponentPokemonId,
         )
 
+    override suspend fun savePokemon(pokemonId: String) {
+        localBattleDataSource.savePokemon(pokemonId)
+    }
+
+    override suspend fun savePokemonWithSkill(
+        pokemonId: String,
+        skillId: String,
+    ) {
+        localBattleDataSource.savePokemonWithSkill(pokemonId, skillId)
+    }
+
+    override suspend fun savedPokemon(): Flow<Pokemon?> =
+        localBattleDataSource.pokemonId().map { it?.let { pokemonRepository.pokemon(it) } }
+
+    override suspend fun savedPokemonWithSkill(): Flow<PokemonWithSkill?> =
+        localBattleDataSource.pokemonWithSkill().map {
+            it?.let { pokemonWithSkill ->
+                val pokemon = pokemonRepository.pokemon(pokemonWithSkill.pokemonId)
+                val skill = skill(pokemon.dexNumber, pokemonWithSkill.skillId)
+                PokemonWithSkill(pokemon, skill)
+            }
+        }
+
+    private suspend fun skill(
+        dexNumber: Long,
+        skillId: String,
+    ): BattleSkill =
+        availableSkills(dexNumber).find {
+            it.id == skillId
+        } ?: error("아이디에 해당하는 스킬이 존재하지 않습니다. id: $skillId")
+
     companion object {
         private var instance: BattleRepository? = null
 
-        fun instance(): BattleRepository {
-            return instance ?: DefaultBattleRepository(RemoteBattleDataSource.instance()).also {
+        fun instance(context: Context): BattleRepository {
+            return instance ?: DefaultBattleRepository(
+                LocalBattleDataSource.instance(context),
+                RemoteBattleDataSource.instance(),
+                DefaultDexRepository.instance(),
+            ).also {
                 instance = it
             }
         }
