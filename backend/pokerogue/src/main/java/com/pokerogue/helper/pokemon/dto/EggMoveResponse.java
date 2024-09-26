0272412cb513e5ee82260eba36ab54c3cd7e8f35
woojong <dwax1324@gmail.com>
2024-09-26 20:45:05 +0900

    refactor: EggMoveResponse 추가

---
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/pokemon/dto/EggMoveResponse.java
@@ -0,0 +1,39 @@
+package com.pokerogue.helper.pokemon.dto;
+
+import com.pokerogue.helper.battle.BattleMove;
+import com.pokerogue.helper.battle.MoveCategory;
+import com.pokerogue.helper.global.exception.ErrorMessage;
+import com.pokerogue.helper.global.exception.GlobalCustomException;
+import com.pokerogue.helper.type.data.Type;
+
+public record EggMoveResponse(
+        String id,
+        String name,
+        Integer level,
+        Integer power,
+        Integer accuracy,
+        String type,
+        String typeLogo,
+        String category,
+        String categoryLogo
+) {
+
+    public static EggMoveResponse from(BattleMove battleMove) {
+        MoveCategory moveCategory = battleMove.category();
+        Type firstType = Type.findByName(battleMove.type().getKoName())
+                .orElseThrow(() -> new GlobalCustomException(ErrorMessage.TYPE_MATCHING_ERROR));
+
+        return new EggMoveResponse(
+                battleMove.id(),
+                battleMove.name(),
+                1,
+                battleMove.power(),
+                battleMove.accuracy(),
+                firstType.getName(),
+                firstType.getImage(),
+                moveCategory.getName(),
+                moveCategory.getImage()
+        );
+    }
+
+}
