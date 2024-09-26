46f5ad940ffc9b4c4db57342a38855d03123ab55
unifolio0 <boho82ha@gmail.com>
2024-09-26 21:20:56 +0900

    refactor: 테스트 깨지는 오류 수정

--- backend/pokerogue/src/test/java/com/pokerogue/helper/biome/service/BiomeServiceTest.java
+++ backend/pokerogue/src/test/java/com/pokerogue/helper/biome/service/BiomeServiceTest.java
@@ -2,8 +2,10 @@ package com.pokerogue.helper.biome.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.pokerogue.environment.service.ServiceTest;
+import com.pokerogue.helper.biome.dto.BiomeDetailResponse;
 import com.pokerogue.helper.biome.dto.BiomeResponse;
 import com.pokerogue.helper.global.exception.ErrorMessage;
 import com.pokerogue.helper.global.exception.GlobalCustomException;
@@ -25,7 +27,7 @@ class BiomeServiceTest extends ServiceTest {
         assertThat(biomes.size()).isEqualTo(35);
     }
 
-    /*@Test
+    @Test
     @DisplayName("단일 바이옴 정보를 불러온다")
     void findBiome() {
         BiomeDetailResponse biomeDetailResponse = biomeService.findBiome("fairy_cave");
@@ -33,13 +35,12 @@ class BiomeServiceTest extends ServiceTest {
         assertAll(
                 () -> assertThat(biomeDetailResponse.id()).isEqualTo("fairy_cave"),
                 () -> assertThat(biomeDetailResponse.name()).isEqualTo("페어리 동굴"),
-                () -> assertThat(biomeDetailResponse.image()).contains("/biome/fairy_cave.png"),
                 () -> assertThat(biomeDetailResponse.wildPokemons()).hasSize(5),
                 () -> assertThat(biomeDetailResponse.bossPokemons()).hasSize(4),
                 () -> assertThat(biomeDetailResponse.trainerPokemons()).hasSize(3),
                 () -> assertThat(biomeDetailResponse.nextBiomes()).hasSize(2)
         );
-    }*/
+    }
 
     @Test
     @DisplayName("해당 id의 바이옴이 없는 경우 예외를 발생시킨다")
