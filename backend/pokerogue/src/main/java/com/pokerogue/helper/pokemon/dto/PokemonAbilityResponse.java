7fa1c9c3cc54d96653be4d71ab387b68be918ef9
woojong <dwax1324@gmail.com>
2024-09-26 20:46:47 +0900

    refactor: ability dto에서 생성

--- backend/pokerogue/src/main/java/com/pokerogue/helper/pokemon/dto/PokemonAbilityResponse.java
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/pokemon/dto/PokemonAbilityResponse.java
@@ -1,4 +1,44 @@
 package com.pokerogue.helper.pokemon.dto;
 
+import com.pokerogue.helper.ability.data.Ability;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public record PokemonAbilityResponse(String id, String name, String description, Boolean passive, Boolean hidden) {
+
+    public static PokemonAbilityResponse from(Ability ability, boolean isPassive, boolean isHidden) {
+        return new PokemonAbilityResponse(
+                ability.getId(),
+                ability.getKoName(),
+                ability.getDescription(),
+                isPassive,
+                isHidden
+        );
+    }
+
+
+    public static List<PokemonAbilityResponse> createListFrom(List<Optional<Ability>> abilities) {
+        final int HIDDEN_INDEX = abilities.size() - 1;
+        final int PASSIVE_INDEX = abilities.size() - 2;
+
+        List<Optional<Ability>> normals = abilities.subList(0, HIDDEN_INDEX);
+        Optional<Ability> hidden = abilities.get(HIDDEN_INDEX);
+        Optional<Ability> passive = abilities.get(PASSIVE_INDEX);
+
+        List<PokemonAbilityResponse> normalResponse = normals.stream()
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .map(normal -> from(normal, false, false))
+                .toList();
+
+        Optional<PokemonAbilityResponse> passiveResponse = passive.map(ability -> from(ability, true, false));
+        Optional<PokemonAbilityResponse> hiddenResponse = hidden.map(ability -> from(ability, false, true));
+
+        List<PokemonAbilityResponse> totalResponse = new ArrayList<>(normalResponse);
+        passiveResponse.ifPresent(totalResponse::add);
+        hiddenResponse.ifPresent(totalResponse::add);
+
+        return totalResponse;
+    }
 }
