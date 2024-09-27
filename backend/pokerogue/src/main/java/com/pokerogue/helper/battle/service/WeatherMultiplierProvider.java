9c9f3e0c1b14a0d2da651f2bab2d39274fed1370
jongmee <ra123221@gmail.com>
2024-09-27 10:43:10 +0900

    refactor: WeatherMultiplierProvider 수정

--- backend/pokerogue/src/main/java/com/pokerogue/helper/battle/service/WeatherMultiplierProvider.java
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/battle/service/WeatherMultiplierProvider.java
@@ -2,69 +2,30 @@ package com.pokerogue.helper.battle.service;
 
 import com.pokerogue.helper.battle.data.Weather;
 import com.pokerogue.helper.type.data.Type;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import org.springframework.stereotype.Component;
 
 @Component
 public class WeatherMultiplierProvider {
 
+    private static final Map<Integer, BattleMultiplier> WEATHER_MULTIPLIERS = new HashMap<>();
+
+    static {
+        WEATHER_MULTIPLIERS.put(Objects.hash(Weather.SUNNY, Type.FIRE), BattleMultiplier.STRONG_MULTIPLIER);
+        WEATHER_MULTIPLIERS.put(Objects.hash(Weather.SUNNY, Type.WATER), BattleMultiplier.WEAK_MULTIPLIER);
+        WEATHER_MULTIPLIERS.put(Objects.hash(Weather.HARSH_SUN, Type.FIRE), BattleMultiplier.STRONG_MULTIPLIER);
+        WEATHER_MULTIPLIERS.put(Objects.hash(Weather.HARSH_SUN, Type.WATER), BattleMultiplier.ZERO_MULTIPLIER);
+        WEATHER_MULTIPLIERS.put(Objects.hash(Weather.RAIN, Type.WATER), BattleMultiplier.STRONG_MULTIPLIER);
+        WEATHER_MULTIPLIERS.put(Objects.hash(Weather.RAIN, Type.FIRE), BattleMultiplier.WEAK_MULTIPLIER);
+        WEATHER_MULTIPLIERS.put(Objects.hash(Weather.HEAVY_RAIN, Type.WATER), BattleMultiplier.STRONG_MULTIPLIER);
+        WEATHER_MULTIPLIERS.put(Objects.hash(Weather.HEAVY_RAIN, Type.FIRE), BattleMultiplier.ZERO_MULTIPLIER);
+    }
+
     public BattleMultiplier getByAttackMoveType(Weather weather, Type attackMoveType) {
-        if (weather == Weather.SUNNY) {
-            return getSunnyAttackTypeMultiplier(attackMoveType);
-        }
-        if (weather == Weather.HARSH_SUN) {
-            return getHarshSunAttackTypeMultiplier(attackMoveType);
-        }
-        if (weather == Weather.RAIN) {
-            return getRainAttackTypeMultiplier(attackMoveType);
-        }
-        if (weather == Weather.HEAVY_RAIN) {
-            return getHeavyRainAttackTypeMultiplier(attackMoveType);
-        }
+        int hashCode = Objects.hash(weather, attackMoveType);
 
-        return BattleMultiplier.DEFAULT_MULTIPLIER;
-    }
-
-    private BattleMultiplier getSunnyAttackTypeMultiplier(Type attackMoveType) {
-        if (attackMoveType == Type.FIRE) {
-            return BattleMultiplier.STRONG_MULTIPLIER;
-        }
-        if (attackMoveType == Type.WATER) {
-            return BattleMultiplier.WEAK_MULTIPLIER;
-        }
-
-        return BattleMultiplier.DEFAULT_MULTIPLIER;
-    }
-
-    private BattleMultiplier getHarshSunAttackTypeMultiplier(Type attackMoveType) {
-        if (attackMoveType == Type.FIRE) {
-            return BattleMultiplier.STRONG_MULTIPLIER;
-        }
-        if (attackMoveType == Type.WATER) {
-            return BattleMultiplier.ZERO_MULTIPLIER;
-        }
-
-        return BattleMultiplier.DEFAULT_MULTIPLIER;
-    }
-
-    private BattleMultiplier getRainAttackTypeMultiplier(Type attackMoveType) {
-        if (attackMoveType == Type.WATER) {
-            return BattleMultiplier.STRONG_MULTIPLIER;
-        }
-        if (attackMoveType == Type.FIRE) {
-            return BattleMultiplier.WEAK_MULTIPLIER;
-        }
-
-        return BattleMultiplier.DEFAULT_MULTIPLIER;
-    }
-
-    private BattleMultiplier getHeavyRainAttackTypeMultiplier(Type attackMoveType) {
-        if (attackMoveType == Type.WATER) {
-            return BattleMultiplier.STRONG_MULTIPLIER;
-        }
-        if (attackMoveType == Type.FIRE) {
-            return BattleMultiplier.ZERO_MULTIPLIER;
-        }
-
-        return BattleMultiplier.DEFAULT_MULTIPLIER;
+        return WEATHER_MULTIPLIERS.getOrDefault(hashCode, BattleMultiplier.DEFAULT_MULTIPLIER);
     }
 }
--- backend/pokerogue/src/test/java/com/pokerogue/helper/battle/service/WeatherMultiplierProviderTest.java
+++ backend/pokerogue/src/test/java/com/pokerogue/helper/battle/service/WeatherMultiplierProviderTest.java
@@ -1,7 +1,6 @@
 package com.pokerogue.helper.battle.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import com.pokerogue.helper.battle.data.Weather;
 import com.pokerogue.helper.type.data.Type;
