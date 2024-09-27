c1f69733617c07a6d50efcb50a91d84e60a8f4d7
unifolio0 <boho82ha@gmail.com>
2024-09-27 11:41:20 +0900

    refactor: 중복 클래스 삭제

--- backend/pokerogue/src/main/java/com/pokerogue/helper/move/MoveRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pokerogue.helper.move;
-
-import com.pokerogue.helper.move.data.Move;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MoveRepository extends MongoRepository<Move, String> {
-}
--- backend/pokerogue/src/test/java/com/pokerogue/helper/move/repository/MoveRepositoryTest.java
+++ backend/pokerogue/src/test/java/com/pokerogue/helper/move/repository/MoveRepositoryTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.pokerogue.environment.repository.MongoRepositoryTest;
-import com.pokerogue.helper.move.MoveRepository;
 import com.pokerogue.helper.move.data.Move;
 import com.pokerogue.helper.move.data.MoveCategory;
 import com.pokerogue.helper.move.data.MoveFlag;
