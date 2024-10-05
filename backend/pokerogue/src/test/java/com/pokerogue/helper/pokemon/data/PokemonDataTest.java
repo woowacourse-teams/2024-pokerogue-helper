04a4412324369629814098f3fda302f783e1667e
woojong <dwax1324@gmail.com>
2024-10-06 00:30:49 +0900

    test: 포켓몬 데이터의 세대가 유효한지 확인한다.

--- backend/pokerogue/src/test/java/com/pokerogue/helper/pokemon/data/PokemonDataTest.java
+++ backend/pokerogue/src/test/java/com/pokerogue/helper/pokemon/data/PokemonDataTest.java
@@ -21,15 +21,15 @@ public class PokemonDataTest extends RepositoryTest {
     @DisplayName("포켓몬 데이터의 개수를 확인한다.")
     @Test
     void pokemonCount() {
-        List<Pokemon> actual = pokemonRepository.findAll();
+        int actual = pokemonRepository.findAll().size();
 
         ThrowingCallable validator = () -> validatePokemonSize(actual);
 
         Assertions.assertThatCode(validator).doesNotThrowAnyException();
     }
 
+    @Disabled("파싱코드의 replace를 한 문자가 아닌 스트링의 전체를 replace하도록 바꿔야 해서 disalbed")
     @DisplayName("포켓몬 데이터의 아이디 형식을 확인한다.")
-    @Disabled("-로 나오는 아이디가 존재, 파싱코드의 replace를 전체로 replace하도록 바꿔야 함")
     @Test
     void pokemonIdFormat() {
         List<Pokemon> actual = pokemonRepository.findAll();
@@ -39,12 +39,13 @@ public class PokemonDataTest extends RepositoryTest {
         Assertions.assertThatCode(validator).doesNotThrowAnyException();
     }
 
-    @DisplayName("포켓몬 데이터의 종족값은 기본 능력치의 합이다.")
     @Disabled("""
             ID actualTotal expectedTotal
             charizard_gigantamax 634 644,
             kingler_gigantamax 575 58
-            두 건의 데이터에 대해 종족값이 일치하지 않음""")
+            
+            두 건의 데이터에 대해 종족값이 일치하지 않아서 disalbed""")
+    @DisplayName("포켓몬 데이터의 종족값은 기본 능력치의 합이다.")
     @Test
     void pokemonTotalStats() {
         List<Pokemon> actual = pokemonRepository.findAll();
@@ -54,5 +55,36 @@ public class PokemonDataTest extends RepositoryTest {
         Assertions.assertThatCode(callable).doesNotThrowAnyException();
     }
 
+    @DisplayName("포켓몬 데이터의 세대가 유효한지 확인한다.")
+    @Test
+    void pokemonGeneration() {
+        List<Pokemon> actual = pokemonRepository.findAll();
+
+        ThrowingCallable validator = () -> PokemonValidator.validatePokemonGeneration(actual);
+
+        Assertions.assertThatCode(validator).doesNotThrowAnyException();
+    }
+
+    @Disabled("포켓몬 변환 정보가 주어지지 않는 데이터가 존재하여 disalbed.")
+    @DisplayName("포켓몬 폼변환이 가능하면 폼변환 정보가 주어진다.")
+    @Test
+    void pokemonGeneration2() {
+        List<Pokemon> actual = pokemonRepository.findAll();
+
+        ThrowingCallable validator = () -> PokemonValidator.validatePokemonFormChanges(actual);
+
+        Assertions.assertThatCode(validator).doesNotThrowAnyException();
+    }
+
+
+    @DisplayName("legendary, subLegendary, mythical 셋 중 하나가 true거나 모두 false여야한다.")
+    @Test
+    void pokemonGeneration3() {
+        List<Pokemon> actual = pokemonRepository.findAll();
+
+        ThrowingCallable validator = () -> PokemonValidator.validatePokemonRarity(actual);
+
+        Assertions.assertThatCode(validator).doesNotThrowAnyException();
+    }
 
 }
