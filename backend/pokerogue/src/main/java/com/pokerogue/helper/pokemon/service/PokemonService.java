001dbff28e8c409625ce47d1a0ea321722b4c7fc
woojong <dwax1324@gmail.com>
2024-10-06 12:30:22 +0900

    refactor: 진화 서비스 뎁스 기반으로 재구현

--- backend/pokerogue/src/main/java/com/pokerogue/helper/pokemon/service/PokemonService.java
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/pokemon/service/PokemonService.java
@@ -1,7 +1,6 @@
 package com.pokerogue.helper.pokemon.service;
 
 
-import com.pokerogue.external.s3.service.S3Service;
 import com.pokerogue.helper.ability.data.Ability;
 import com.pokerogue.helper.ability.repository.InMemoryAbilityRepository;
 import com.pokerogue.helper.battle.BattleMove;
@@ -9,11 +8,9 @@ import com.pokerogue.helper.battle.BattleMoveRepository;
 import com.pokerogue.helper.biome.repository.InMemoryBiomeRepository;
 import com.pokerogue.helper.global.exception.ErrorMessage;
 import com.pokerogue.helper.global.exception.GlobalCustomException;
-import com.pokerogue.helper.pokemon.data.Evolution;
 import com.pokerogue.helper.pokemon.data.LevelMove;
 import com.pokerogue.helper.pokemon.data.Pokemon;
 import com.pokerogue.helper.pokemon.dto.EggMoveResponse;
-import com.pokerogue.helper.pokemon.dto.EvolutionResponse;
 import com.pokerogue.helper.pokemon.dto.EvolutionResponses;
 import com.pokerogue.helper.pokemon.dto.MoveResponse;
 import com.pokerogue.helper.pokemon.dto.PokemonAbilityResponse;
@@ -23,10 +20,8 @@ import com.pokerogue.helper.pokemon.dto.PokemonResponse;
 import com.pokerogue.helper.pokemon.repository.PokemonRepository;
 import com.pokerogue.helper.type.data.Type;
 import com.pokerogue.helper.type.dto.PokemonTypeResponse;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.IntStream;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -34,12 +29,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PokemonService {
 
-    private final S3Service s3Service;
-
     private final PokemonRepository pokemonRepository;
     private final BattleMoveRepository battleMoveRepository;
     private final InMemoryBiomeRepository inMemoryBiomeRepository;
     private final InMemoryAbilityRepository inMemoryAbilityRepository;
+    private final EvolutionService evolutionService;
 
     public PokemonDetailResponse findById(String id) {
         Pokemon pokemon = pokemonRepository.findById(id)
@@ -56,13 +50,13 @@ public class PokemonService {
                 .toList();
     }
 
-    public PokemonDetailResponse createPokemonDetailResponse(Pokemon pokemon) {
+    private PokemonDetailResponse createPokemonDetailResponse(Pokemon pokemon) {
         List<PokemonTypeResponse> pokemonTypeResponses = createTypeResponse(pokemon);
         List<PokemonAbilityResponse> pokemonAbilityResponses = createAbilityResponse(pokemon);
         List<MoveResponse> moveResponse = createMoveResponse(pokemon);
         List<EggMoveResponse> eggMoveResponses = createEggMoveResponse(pokemon);
         List<PokemonBiomeResponse> biomeResponses = createBiomeResponse(pokemon);
-        EvolutionResponses evolutionResponses = createEvolutionResponse(pokemon);
+        EvolutionResponses evolutionResponses = evolutionService.getEvolutionResponses(pokemon);
 
         return PokemonDetailResponse.from(
                 pokemon,
@@ -83,7 +77,7 @@ public class PokemonService {
                 .toList();
     }
 
-    public List<PokemonAbilityResponse> createAbilityResponse(Pokemon pokemon) {
+    private List<PokemonAbilityResponse> createAbilityResponse(Pokemon pokemon) {
         List<String> abilityIds = pokemon.getNormalAbilityIds();
         abilityIds.add(pokemon.getPassiveAbilityId());
         abilityIds.add(pokemon.getHiddenAbilityId());
@@ -95,7 +89,7 @@ public class PokemonService {
         return PokemonAbilityResponse.createListFrom(abilities);
     }
 
-    public List<PokemonBiomeResponse> createBiomeResponse(Pokemon pokemon) {
+    private List<PokemonBiomeResponse> createBiomeResponse(Pokemon pokemon) {
         List<String> biomes = pokemon.getBiomeIds();
 
         return biomes.stream()
@@ -104,7 +98,7 @@ public class PokemonService {
                 .toList();
     }
 
-    public List<EggMoveResponse> createEggMoveResponse(Pokemon pokemon) {
+    private List<EggMoveResponse> createEggMoveResponse(Pokemon pokemon) {
         List<String> moves = pokemon.getEggMoveIds();
 
         return moves.stream()
@@ -115,7 +109,7 @@ public class PokemonService {
                 .toList();
     }
 
-    public List<MoveResponse> createMoveResponse(Pokemon pokemon) {
+    private List<MoveResponse> createMoveResponse(Pokemon pokemon) {
         List<LevelMove> levelMoves = pokemon.getLevelMoves();
 
         return levelMoves.stream()
@@ -126,33 +120,5 @@ public class PokemonService {
     private BattleMove getMoveById(LevelMove levelMove) {
         return battleMoveRepository.findById(levelMove.getMoveId())
                 .orElseThrow(() -> new GlobalCustomException(ErrorMessage.MOVE_NOT_FOUND));
-    }
-
-    public EvolutionResponses createEvolutionResponse(Pokemon pokemon) {
-        List<Evolution> evolutions = pokemon.getEvolutions();
-        final int currentDepth = IntStream.range(0, evolutions.size())
-                .filter(depth -> evolutions.get(depth).getFrom().equals(pokemon.getId()))
-                .findAny()
-                .orElse(0);
-        return new EvolutionResponses(currentDepth, getEvolutionResponses(evolutions, pokemon.getId(),
-                s3Service.getPokemonImageFromS3(pokemon.getImageId())));
-    }
-
-    public List<EvolutionResponse> getEvolutionResponses(
-            List<Evolution> evolutions,
-            String pokemonId,
-            String imageId
-    ) {
-        List<EvolutionResponse> evolutionResponses = new ArrayList<>();
-        for (int depth = 0; depth < evolutions.size(); depth++) {
-            evolutionResponses.add(EvolutionResponse.from(
-                    evolutions.get(depth),
-                    depth,
-                    pokemonId,
-                    s3Service.getPokemonImageFromS3(imageId)
-            ));
-
-        }
-        return evolutionResponses;
     }
 }
