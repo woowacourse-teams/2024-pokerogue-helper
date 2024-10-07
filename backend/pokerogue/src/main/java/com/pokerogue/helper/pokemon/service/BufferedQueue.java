be483e06dbbbdd2767783c3d0727802b84b6f62b
woojong <dwax1324@gmail.com>
2024-10-07 20:14:52 +0900

    test: 버퍼 호출 전에 저장된 값만 꺼낼 수 있다.

--- backend/pokerogue/src/main/java/com/pokerogue/helper/pokemon/service/BufferedQueue.java
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/pokemon/service/BufferedQueue.java
@@ -3,37 +3,45 @@ package com.pokerogue.helper.pokemon.service;
 import java.util.Queue;
 
 public class BufferedQueue<T> {
-    private final Queue<T> waitingQueue;
+    private final Queue<T> value;
     private int bufferSize;
-    private int captureCount;
+    private int bufferCount;
 
     public BufferedQueue(Queue<T> waitingQueue) {
-        this.waitingQueue = waitingQueue;
+        this.value = waitingQueue;
     }
 
     public boolean hasNext() {
-        return !waitingQueue.isEmpty();
+        return !value.isEmpty();
     }
 
     public T poll() {
-        bufferSize = waitingQueue.size() - 1;
-        return waitingQueue.poll();
+        T removedValue = value.remove();
+        bufferSize = Math.max(bufferSize - 1, 0);
+        if (bufferSize == 0) {
+            bufferCount = 0;
+        }
+        return removedValue;
     }
 
     public void add(T s) {
-        waitingQueue.add(s);
+        value.add(s);
     }
 
     public boolean hasBufferedNext() {
         return bufferSize > 0;
     }
 
-    public void capture() {
-        this.bufferSize = waitingQueue.size();
-        captureCount++;
+    public void buffer() {
+        bufferSize = value.size();
+        bufferCount++;
     }
 
-    public int getCaptureCount() {
-        return captureCount;
+    public int getBufferCount() {
+        return bufferCount;
+    }
+
+    public int size() {
+        return value.size();
     }
 }
