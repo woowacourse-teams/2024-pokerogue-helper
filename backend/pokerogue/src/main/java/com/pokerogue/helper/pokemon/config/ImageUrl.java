f4ed49f651a25bb03ed0170d0581a410e00f47f9
woojong <dwax1324@gmail.com>
2024-10-07 21:54:57 +0900

    refactor: 간단한 이미지 상수 클래스 추가

---
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/pokemon/config/ImageUrl.java
@@ -0,0 +1,25 @@
+package com.pokerogue.helper.pokemon.config;
+
+public enum ImageUrl {
+    BASE_URL("https://dl70s9ccojnge.cloudfront.net/pokerogue-helper/pokerogue"),
+    POKEMON_FRONT("/pokemon/front/"),
+    POKEMON_BACK("/pokemon/back/"),
+    TYPE("/type/"),
+    MOVE_CATEGORY("/move-category/"),
+    BIOME("/biome/"),
+    ;
+
+    private final String url;
+
+    ImageUrl(String url) {
+        this.url = url;
+    }
+
+    public static String getPokemonImage(String id) {
+        return BASE_URL.url + POKEMON_FRONT.url + id + ".png";
+    }
+
+    public static String getPokemonBackImage(String id) {
+        return BASE_URL.url + POKEMON_BACK.url + id + ".png";
+    }
+}
