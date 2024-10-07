5e04bfbad4eb6b5ee460eb66582ad26c217f9381
woojong <dwax1324@gmail.com>
2024-10-07 20:59:17 +0900

    test: 포켓몬 진화 정보를 가져올 수 있다.

--- backend/pokerogue/src/test/java/com/pokerogue/helper/pokemon/service/EvolutionContextTest.java
+++ backend/pokerogue/src/test/java/com/pokerogue/helper/pokemon/service/EvolutionContextTest.java
@@ -11,13 +11,13 @@ class EvolutionContextTest {
 
     @DisplayName("각 노드의 깊이 정보를 가져올 수 있다.")
     @Test
-    void getEvolutionOf() {
+    void getDepthOf() {
         Evolution evolution = FakeEvolution.from("A", "B");
         Evolution evolution2 = FakeEvolution.from("B", "C");
         Evolution evolution3 = FakeEvolution.from("C", "D");
         Evolution evolution4 = FakeEvolution.from("D", "E");
 
-        EvolutionContext context = new EvolutionContext(List.of(evolution, evolution2, evolution3,evolution4));
+        EvolutionContext context = new EvolutionContext(List.of(evolution, evolution2, evolution3, evolution4));
 
         Assertions.assertThat(context.getDepthOf("A")).isEqualTo(0);
         Assertions.assertThat(context.getDepthOf("B")).isEqualTo(1);
@@ -26,7 +26,14 @@ class EvolutionContextTest {
         Assertions.assertThat(context.getDepthOf("E")).isEqualTo(4);
     }
 
+    @DisplayName("포켓몬의 진화 정보를 가져올 수 있다.")
     @Test
-    void getDepthOf() {
+    void getEvolutionOf() {
+        Evolution actual = FakeEvolution.from("A", "B");
+
+        EvolutionContext context = new EvolutionContext(List.of(actual));
+        Evolution expected = context.getEvolutionOf("A");
+
+        Assertions.assertThat(expected).isEqualTo(actual);
     }
 }
