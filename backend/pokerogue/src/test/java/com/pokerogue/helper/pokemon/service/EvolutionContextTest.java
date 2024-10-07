59f4b68a233bf5a5cc46122a95f698e13d712fe0
woojong <dwax1324@gmail.com>
2024-10-07 20:56:10 +0900

    test: 각 노드의 깊이 정보를 가져올 수 있다.

---
+++ backend/pokerogue/src/test/java/com/pokerogue/helper/pokemon/service/EvolutionContextTest.java
@@ -0,0 +1,32 @@
+package com.pokerogue.helper.pokemon.service;
+
+import com.pokerogue.helper.pokemon.data.Evolution;
+import com.pokerogue.helper.pokemon.data.FakeEvolution;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class EvolutionContextTest {
+
+    @DisplayName("각 노드의 깊이 정보를 가져올 수 있다.")
+    @Test
+    void getEvolutionOf() {
+        Evolution evolution = FakeEvolution.from("A", "B");
+        Evolution evolution2 = FakeEvolution.from("B", "C");
+        Evolution evolution3 = FakeEvolution.from("C", "D");
+        Evolution evolution4 = FakeEvolution.from("D", "E");
+
+        EvolutionContext context = new EvolutionContext(List.of(evolution, evolution2, evolution3,evolution4));
+
+        Assertions.assertThat(context.getDepthOf("A")).isEqualTo(0);
+        Assertions.assertThat(context.getDepthOf("B")).isEqualTo(1);
+        Assertions.assertThat(context.getDepthOf("C")).isEqualTo(2);
+        Assertions.assertThat(context.getDepthOf("D")).isEqualTo(3);
+        Assertions.assertThat(context.getDepthOf("E")).isEqualTo(4);
+    }
+
+    @Test
+    void getDepthOf() {
+    }
+}
