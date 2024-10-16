34941d68040d09757b0286289c7feb5df9027f6c
sh1mj1 <7wlgns@gmail.com>
2024-10-16 11:43:03 +0900

    feat: OuterEvolutionAdapter, OuterEvolutionViewHolder

---
+++ android/app/src/main/java/poke/rogue/helper/presentation/dex/detail/evolution/OuterEvolutionAdapter.kt
@@ -0,0 +1,26 @@
+package poke.rogue.helper.presentation.dex.detail.evolution
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import poke.rogue.helper.presentation.dex.detail.PokemonDetailNavigateHandler
+import poke.rogue.helper.presentation.dex.model.EvolutionsUiModel
+import poke.rogue.helper.presentation.util.view.ItemDiffCallback
+
+class OuterEvolutionAdapter(
+    private val navigateHandler: PokemonDetailNavigateHandler,
+) : ListAdapter<EvolutionsUiModel, OuterEvolutionViewHolder>(comparator) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OuterEvolutionViewHolder =
+        OuterEvolutionViewHolder.inflated(parent, navigateHandler)
+
+    override fun onBindViewHolder(holder: OuterEvolutionViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object {
+        val comparator = ItemDiffCallback<EvolutionsUiModel>(
+            onItemsTheSame = { oldItem, newItem -> oldItem.evolutions == newItem.evolutions },
+            onContentsTheSame = { oldItem, newItem -> oldItem == newItem },
+        )
+    }
+
+}
---
+++ android/app/src/main/java/poke/rogue/helper/presentation/dex/detail/evolution/OuterEvolutionViewHolder.kt
@@ -0,0 +1,31 @@
+package poke.rogue.helper.presentation.dex.detail.evolution
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import poke.rogue.helper.databinding.ViewGroupPokemonEvolutionBinding
+import poke.rogue.helper.presentation.dex.detail.PokemonDetailNavigateHandler
+import poke.rogue.helper.presentation.dex.model.EvolutionsUiModel
+
+class OuterEvolutionViewHolder(
+    private val binding: ViewGroupPokemonEvolutionBinding,
+    private val navigateHandler: PokemonDetailNavigateHandler,
+) : ViewHolder(binding.root) {
+    private val evolutionAdapter by lazy { EvolutionAdapter(navigateHandler) }
+
+    fun bind(evolutionsUiModel: EvolutionsUiModel) {
+        binding.recyclerView.adapter = evolutionAdapter
+        evolutionsUiModel.evolutions.let(evolutionAdapter::submitList)
+    }
+
+    companion object {
+        fun inflated(parent: ViewGroup, navigateHandler: PokemonDetailNavigateHandler) = OuterEvolutionViewHolder(
+            binding = ViewGroupPokemonEvolutionBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false,
+            ),
+            navigateHandler = navigateHandler,
+        )
+    }
+}
