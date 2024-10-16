f1a4bb40e9cbeb53a2992ad4436dbcad89dcf3ea
jongmee <ra123221@gmail.com>
2024-10-16 14:45:04 +0900

    refactor: MongoDB config

--- backend/pokerogue/src/main/java/com/pokerogue/PokerogueApplication.java
+++ backend/pokerogue/src/main/java/com/pokerogue/PokerogueApplication.java
@@ -2,14 +2,12 @@ package com.pokerogue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories
 public class PokerogueApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PokerogueApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PokerogueApplication.class, args);
+    }
 
 }
