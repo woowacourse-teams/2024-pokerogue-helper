a46170a290c3bea5f19f00abf8790940fe4d0f01
JoYehyun <jo990714@naver.com>
2024-10-20 20:36:56 +0900

    feat: 새로운 datastore 파일 Koin 적용

--- android/local/src/main/java/poke/rogue/helper/local/di/DataStoreModule.kt
+++ android/local/src/main/java/poke/rogue/helper/local/di/DataStoreModule.kt
@@ -3,9 +3,11 @@ package poke.rogue.helper.local.di
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 import poke.rogue.helper.local.datastore.BattleDataStore
+import poke.rogue.helper.local.datastore.NavigationModeDataStore
 
 internal val dataStoreModule
     get() =
         module {
             singleOf(::BattleDataStore)
+            singleOf(::NavigationModeDataStore)
         }
