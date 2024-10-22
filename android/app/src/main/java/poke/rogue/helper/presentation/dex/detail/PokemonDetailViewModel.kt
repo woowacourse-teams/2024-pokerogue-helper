561375dd059a72aff638f6353812104592fdfb9e
sh1mj1 <7wlgns@gmail.com>
2024-10-22 15:21:56 +0900

    refactor(PokemonDetailViewModel): eventflow

--- android/app/src/main/java/poke/rogue/helper/presentation/dex/detail/PokemonDetailViewModel.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/dex/detail/PokemonDetailViewModel.kt
@@ -2,12 +2,9 @@ package poke.rogue.helper.presentation.dex.detail
 
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.filterIsInstance
 import kotlinx.coroutines.flow.first
@@ -19,6 +16,8 @@ import poke.rogue.helper.analytics.analyticsLogger
 import poke.rogue.helper.data.repository.DexRepository
 import poke.rogue.helper.presentation.base.BaseViewModelFactory
 import poke.rogue.helper.presentation.base.error.ErrorHandleViewModel
+import poke.rogue.helper.presentation.util.event.MutableEventFlow
+import poke.rogue.helper.presentation.util.event.asEventFlow
 
 class PokemonDetailViewModel(
     private val dexRepository: DexRepository,
@@ -33,20 +32,20 @@ class PokemonDetailViewModel(
         uiState.map { it is PokemonDetailUiState.IsLoading }
             .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000L), true)
 
-    private val _navigationToAbilityDetailEvent = MutableSharedFlow<String>()
-    val navigationToAbilityDetailEvent: SharedFlow<String> = _navigationToAbilityDetailEvent.asSharedFlow()
+    private val _navigationToAbilityDetailEvent = MutableEventFlow<String>()
+    val navigationToAbilityDetailEvent = _navigationToAbilityDetailEvent.asEventFlow()
 
-    private val _navigationToBiomeDetailEvent = MutableSharedFlow<String>()
-    val navigationToBiomeDetailEvent: SharedFlow<String> = _navigationToBiomeDetailEvent.asSharedFlow()
+    private val _navigationToBiomeDetailEvent = MutableEventFlow<String>()
+    val navigationToBiomeDetailEvent = _navigationToBiomeDetailEvent.asEventFlow()
 
-    private val _navigateToHomeEvent = MutableSharedFlow<Boolean>()
-    val navigateToHomeEvent = _navigateToHomeEvent.asSharedFlow()
+    private val _navigateToHomeEvent = MutableEventFlow<Boolean>()
+    val navigateToHomeEvent = _navigateToHomeEvent.asEventFlow()
 
-    private val _navigateToPokemonDetailEvent = MutableSharedFlow<String>()
-    val navigateToPokemonDetailEvent = _navigateToPokemonDetailEvent.asSharedFlow()
+    private val _navigateToPokemonDetailEvent = MutableEventFlow<String>()
+    val navigateToPokemonDetailEvent = _navigateToPokemonDetailEvent.asEventFlow()
 
-    private val _navigateToBattleEvent = MutableSharedFlow<NavigateToBattleEvent>()
-    val navigateToBattleEvent = _navigateToBattleEvent.asSharedFlow()
+    private val _navigateToBattleEvent = MutableEventFlow<NavigateToBattleEvent>()
+    val navigateToBattleEvent = _navigateToBattleEvent.asEventFlow()
 
     fun updatePokemonDetail(pokemonId: String?) {
         requireNotNull(pokemonId) { "Pokemon ID must not be null" }
