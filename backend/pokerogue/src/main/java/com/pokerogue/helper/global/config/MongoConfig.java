80a98bc5c741b7de23ee89172543d9de81d7cc9e
unifolio0 <boho82ha@gmail.com>
2024-10-22 21:02:28 +0900

    feat: test

--- backend/pokerogue/src/main/java/com/pokerogue/helper/global/config/MongoConfig.java
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/global/config/MongoConfig.java
@@ -5,15 +5,19 @@ import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.connection.TransportSettings;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MongoConfig {
 
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
+
     @Bean
     public MongoClient mongoClient() {
-        String uri = "mongodb://root:example@10.0.100.218:3306/pokerogue?authSource=admin"; // MongoDB URI를 여기에 입력
+        String uri = mongoUri; // MongoDB URI를 여기에 입력
         ConnectionString connectionString = new ConnectionString(uri);
 
         // TransportSettings를 사용하여 Netty 설정 적용
