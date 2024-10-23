35e1c26ef6817a8770ee8d9bd4a13ca615a87b30
kkosang <eiwoqp77@gmail.com>
2024-10-23 16:16:18 +0900

    refactor: Home 화면 Koin 적용

--- android/app/src/main/java/poke/rogue/helper/di/ViewModelModule.kt
+++ android/app/src/main/java/poke/rogue/helper/di/ViewModelModule.kt
@@ -4,12 +4,14 @@ import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 import poke.rogue.helper.presentation.dex.PokemonListViewModel
 import poke.rogue.helper.presentation.ability.AbilityViewModel
+import poke.rogue.helper.presentation.home.HomeViewModel
 import poke.rogue.helper.presentation.ability.detail.AbilityDetailViewModel
 
 val viewModelModule
     get() =
         module {
             viewModelOf(::PokemonListViewModel)
+            viewModelOf(::HomeViewModel)
             viewModelOf(::AbilityViewModel)
             viewModelOf(::AbilityDetailViewModel)
         }
--- android/app/src/test/java/poke/rogue/helper/presentation/di/TestViewModelModule.kt
+++ android/app/src/test/java/poke/rogue/helper/presentation/di/TestViewModelModule.kt
@@ -4,6 +4,7 @@ import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 import poke.rogue.helper.presentation.dex.PokemonListViewModel
 import poke.rogue.helper.presentation.ability.AbilityViewModel
+import poke.rogue.helper.presentation.home.HomeViewModel
 import poke.rogue.helper.presentation.ability.detail.AbilityDetailViewModel
 import poke.rogue.helper.testing.di.testingModule
 
@@ -12,6 +13,7 @@ val testViewModelModule =
         includes(testingModule)
 
         singleOf(::PokemonListViewModel)
+        singleOf(::HomeViewModel)
         singleOf(::AbilityViewModel)
         singleOf(::AbilityDetailViewModel)
     }
