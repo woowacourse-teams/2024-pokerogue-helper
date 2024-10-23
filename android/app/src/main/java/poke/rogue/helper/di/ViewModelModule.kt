3ac3a6b8e32b4758d059049cc164600c2c1ba2d4
kkosang <eiwoqp77@gmail.com>
2024-10-23 10:46:32 +0900

    refactor: AbilityViewModel 의존성 주입

--- android/app/src/main/java/poke/rogue/helper/di/ViewModelModule.kt
+++ android/app/src/main/java/poke/rogue/helper/di/ViewModelModule.kt
@@ -3,9 +3,11 @@ package poke.rogue.helper.di
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 import poke.rogue.helper.presentation.dex.PokemonListViewModel
+import poke.rogue.helper.presentation.ability.AbilityViewModel
 
 val viewModelModule
     get() =
         module {
             viewModelOf(::PokemonListViewModel)
+            viewModelOf(::AbilityViewModel)
         }
