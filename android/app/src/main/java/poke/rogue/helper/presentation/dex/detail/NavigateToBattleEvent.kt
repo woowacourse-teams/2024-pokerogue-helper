6c28cd94cbc47c15330aa42da622696f95d29577
JUNWON LEE <sis9221302@gmail.com>
2024-10-23 13:44:01 +0900

    Revert "[1.1.1/AN-FEAT] 포켓몬 상세에서 배틀 도우미로 가는 floating 버튼 추가 (#378)" (#394)
    This reverts commit 7174c8df22fc90e431519c28e5cd8e5e379f7c8e.

--- android/app/src/main/java/poke/rogue/helper/presentation/dex/detail/NavigateToBattleEvent.kt
+++
@@ -1,9 +0,0 @@
-package poke.rogue.helper.presentation.dex.detail
-
-import poke.rogue.helper.presentation.dex.model.PokemonUiModel
-
-sealed class NavigateToBattleEvent {
-    data class WithMyPokemon(val pokemon: PokemonUiModel) : NavigateToBattleEvent()
-
-    data class WithOpponentPokemon(val pokemon: PokemonUiModel) : NavigateToBattleEvent()
-}
