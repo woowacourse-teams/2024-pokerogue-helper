38290615e6f921fba8d0064117600be06d18cac5
sh1mj1 <7wlgns@gmail.com>
2024-10-23 14:38:15 +0900

    test(PokemonDetailActivityTest): 액티비티 실행, 구성변경 테스트

--- android/app/src/test/java/poke/rogue/helper/presentation/dex/detail/PokemonDetailActivityTest.kt
+++ android/app/src/test/java/poke/rogue/helper/presentation/dex/detail/PokemonDetailActivityTest.kt
@@ -3,9 +3,11 @@ package poke.rogue.helper.presentation.dex.detail
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.androidx.viewmodel.ext.android.getViewModel
 import org.robolectric.annotation.Config
 import poke.rogue.helper.presentation.di.testViewModelModule
 import poke.rogue.helper.testing.TestApplication
@@ -25,9 +27,26 @@ class PokemonDetailActivityTest {
         )
 
     @Test
-    fun `Activity 실행 테스트`() {
+    fun `액티비티가 정상적으로 실행된다`() {
         scenario.onActivity { activity ->
             activity.shouldNotBeNull()
+        }
+    }
+
+    @Test
+    fun `액티비티가 구성 변경되어도 같은 뷰모델 인스턴스 사용`() {
+        // given
+        var previewVm: PokemonDetailViewModel? = null
+        scenario.onActivity { activity ->
+            previewVm = activity.getViewModel()
+        }
+
+        // when
+        scenario.recreate()
+
+        // then
+        scenario.onActivity {
+            it.getViewModel<PokemonDetailViewModel>() shouldBe previewVm
         }
     }
 }
