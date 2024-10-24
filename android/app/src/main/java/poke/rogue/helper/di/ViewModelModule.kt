d0b5d914edd7391ebaa75b22e072ba8f6ffcb52c
kkosang <eiwoqp77@gmail.com>
2024-10-24 22:10:37 +0900

    style: ktFormat

--- android/app/src/main/java/poke/rogue/helper/di/ViewModelModule.kt
+++ android/app/src/main/java/poke/rogue/helper/di/ViewModelModule.kt
@@ -3,12 +3,12 @@ package poke.rogue.helper.di
 import org.koin.core.module.dsl.viewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
-import poke.rogue.helper.presentation.biome.BiomeViewModel
-import poke.rogue.helper.presentation.biome.detail.BiomeDetailViewModel
 import poke.rogue.helper.presentation.battle.BattleViewModel
 import poke.rogue.helper.presentation.battle.selection.BattleSelectionViewModel
 import poke.rogue.helper.presentation.battle.selection.pokemon.PokemonSelectionViewModel
 import poke.rogue.helper.presentation.battle.selection.skill.SkillSelectionViewModel
+import poke.rogue.helper.presentation.biome.BiomeViewModel
+import poke.rogue.helper.presentation.biome.detail.BiomeDetailViewModel
 import poke.rogue.helper.presentation.dex.PokemonListViewModel
 import poke.rogue.helper.presentation.type.TypeViewModel
 
