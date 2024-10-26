6bba6928fe471ed76354ed55f3aef7f3bfed4c62
unifolio0 <boho82ha@gmail.com>
2024-10-26 17:50:24 +0900

    test: cdsa

--- backend/pokerogue/src/main/java/com/pokerogue/PokerogueApplication.java
+++ backend/pokerogue/src/main/java/com/pokerogue/PokerogueApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PokerogueApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PokerogueApplication.class, args);
     }
