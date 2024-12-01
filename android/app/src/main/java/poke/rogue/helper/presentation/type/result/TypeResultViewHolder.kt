052dedc00f7f331559a8627c507543cb060e0527
sh1mj1 <7wlgns@gmail.com>
2024-12-01 09:22:35 +0900

    refactor(TypeResultViewHolder): 함수, 클래스 분리

--- android/app/src/main/java/poke/rogue/helper/presentation/type/result/TypeResultViewHolder.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/type/result/TypeResultViewHolder.kt
@@ -1,8 +1,9 @@
 package poke.rogue.helper.presentation.type.result
 
-import android.content.Context
-import android.text.SpannableString
+import android.graphics.drawable.Drawable
+import android.text.SpannedString
 import android.widget.TextView
+import androidx.annotation.ColorRes
 import androidx.recyclerview.widget.RecyclerView
 import poke.rogue.helper.R
 import poke.rogue.helper.databinding.ItemTypeResultBinding
@@ -17,77 +18,97 @@ import poke.rogue.helper.presentation.util.style
 class TypeResultViewHolder(private val binding: ItemTypeResultBinding) :
     RecyclerView.ViewHolder(binding.root) {
     fun bind(typeMatchedResult: MatchedTypesUiModel) {
-        val context = binding.root.context
-        bindTypeResult(typeMatchedResult, context)
+        bindTypeResult(typeMatchedResult)
         binding.typeResult = typeMatchedResult
         binding.rvResultMatchedTypes.adapter = TypeResultItemAdapter(typeMatchedResult.matchedItem)
     }
 
-    private fun bindTypeResult(
-        typeMatchedResult: MatchedTypesUiModel,
-        context: Context,
-    ) {
-        val isMyType = typeMatchedResult.isMyType
-        val typeName = context.stringOf(typeMatchedResult.selectedType.typeName)
-        val matchedResultText = context.stringOf(typeMatchedResult.matchedResultUi.descriptionRes)
-        val matchedResultTextColor = context.colorOf(typeMatchedResult.matchedResultUi.colorRes)
-        val iconResource = typeMatchedResult.selectedType.typeIconResId
+    private fun bindTypeResult(typeMatchedResult: MatchedTypesUiModel) {
+        val matchedTypeResource = matchedTypeResource(typeMatchedResult)
+        val typeResultView =
+            matchedTypeResource.typeResultView(
+                isMyType = typeMatchedResult.isMyType,
+                myTypeView = binding.tvMyTypeResult,
+                opponentTypeView = binding.tvOpponentTypeResult,
+            )
 
-        val (typeResult: String, typeResultView: TextView) =
-            if (isMyType) {
-                Pair(
-                    context.stringOf(
-                        resId = R.string.type_my_type_result,
-                        formatArgs =
-                        arrayOf(
-                            typeName,
-                            matchedResultText,
-                        ),
-                    ),
-                    binding.tvMyTypeResult,
-                )
-            } else {
-                Pair(
-                    context.stringOf(
-                        resId = R.string.type_opponent_type_result,
-                        formatArgs =
-                        arrayOf(
-                            typeName,
-                            matchedResultText,
-                        ),
-                    ),
-                    binding.tvOpponentTypeResult,
-                )
-            }
-
-        styleText(
-            textView = typeResultView,
-            fullText = typeResult,
-            iconRes = iconResource,
-            colorTargetWord = matchedResultText,
-            fontStyleTargetWord = typeName,
-            color = matchedResultTextColor,
-        )
+        typeResultView.textView.text = bindTypeResultView(typeResultView, matchedTypeResource)
     }
 
-    private fun styleText(
-        textView: TextView,
-        fullText: String,
-        iconRes: Int,
-        colorTargetWord: String,
-        fontStyleTargetWord: String,
-        color: Int,
-    ) {
-        val spannableString =
-            SpannableString(fullText).apply {
-                drawable(
-                    iconDrawable = textView.context.drawableOf(iconRes),
-                    iconSize = (textView.textSize * 1.2).toInt(),
-                    fullText = fullText,
-                )
-                color(colorTargetWord, color, fullText)
-                style(fontStyleTargetWord, fullText)
-            }
-        textView.text = spannableString
-    }
+    private fun matchedTypeResource(typeMatchedResult: MatchedTypesUiModel): MatchedTypeResource =
+        with(binding.root.context) {
+            MatchedTypeResource(
+                typeName = stringOf(typeMatchedResult.selectedType.typeName),
+                matchedResult = stringOf(typeMatchedResult.matchedResultUi.descriptionRes),
+                matchedResultColor = colorOf(typeMatchedResult.matchedResultUi.colorRes),
+                iconDrawable =
+                    drawableOf(typeMatchedResult.selectedType.typeIconResId)
+                        ?: error("해당하는 타입 아이콘이 없습니다."),
+            )
+        }
+
+    private fun bindTypeResultView(
+        typeResultView: TypeResultView,
+        matchedTypeResource: MatchedTypeResource,
+    ) = SpannedString(typeResultView.text).drawable(
+        iconDrawable = matchedTypeResource.iconDrawable,
+        iconSize = (typeResultView.textView.textSize * 1.2).toInt(),
+        fullText = typeResultView.text,
+    ).style(
+        targetWord = matchedTypeResource.typeName,
+        fullText = typeResultView.text,
+    ).color(
+        fullText = typeResultView.text,
+        targetWord = matchedTypeResource.matchedResult,
+        color = matchedTypeResource.matchedResultColor,
+    )
 }
+
+private data class MatchedTypeResource(
+    val typeName: String,
+    val matchedResult: String,
+    @ColorRes val matchedResultColor: Int,
+    val iconDrawable: Drawable,
+) {
+    fun typeResultView(
+        isMyType: Boolean,
+        myTypeView: TextView,
+        opponentTypeView: TextView,
+    ): TypeResultView {
+        if (isMyType) return myType(myTypeView)
+        return opponentType(opponentTypeView)
+    }
+
+    private fun myType(view: TextView): TypeResultView =
+        TypeResultView(
+            text =
+                view.context.stringOf(
+                    resId = R.string.type_my_type_result,
+                    formatArgs =
+                        arrayOf(
+                            typeName,
+                            matchedResult,
+                        ),
+                ),
+            textView = view,
+        )
+
+    private fun opponentType(view: TextView): TypeResultView =
+        TypeResultView(
+            text =
+                view.context.stringOf(
+                    resId = R.string.type_opponent_type_result,
+                    formatArgs =
+                        arrayOf(
+                            typeName,
+                            matchedResult,
+                        ),
+                ),
+            textView = view,
+        )
+}
+
+private data class TypeResultView(
+    val text: String,
+    val textView: TextView,
+)
--- android/app/src/main/java/poke/rogue/helper/presentation/util/SpannableStringExtensions.kt
+++ android/app/src/main/java/poke/rogue/helper/presentation/util/SpannableStringExtensions.kt
@@ -4,6 +4,8 @@ import android.graphics.Typeface
 import android.graphics.drawable.Drawable
 import android.text.Spannable
 import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.SpannedString
 import android.text.style.ForegroundColorSpan
 import android.text.style.ImageSpan
 import android.text.style.StyleSpan
@@ -55,4 +57,89 @@ fun SpannableString.style(
         )
     }
     return this
+}
+
+fun SpannableStringBuilder.color(
+    fullText: String,
+    colorTargetWord: String,
+    color: Int,
+) {
+    val colorStartIndex = fullText.indexOf(colorTargetWord)
+    if (colorStartIndex != -1) {
+        setSpan(
+            ForegroundColorSpan(color),
+            colorStartIndex,
+            colorStartIndex + colorTargetWord.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE,
+        )
+    }
+}
+
+fun SpannableStringBuilder.style(
+    fullText: String,
+    fontStyleTargetWord: String,
+) {
+    val fontStyleStartIndex = fullText.indexOf(fontStyleTargetWord)
+    if (fontStyleStartIndex != -1) {
+        setSpan(
+            StyleSpan(Typeface.BOLD),
+            fontStyleStartIndex,
+            fontStyleStartIndex + fontStyleTargetWord.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE,
+        )
+    }
+}
+
+fun SpannedString.drawable(
+    iconDrawable: Drawable?,
+    iconSize: Int,
+    fullText: String,
+): SpannedString {
+    val spannable = SpannableString(this)
+    val iconIndex = fullText.indexOf("|")
+    if (iconIndex != -1) {
+        iconDrawable?.setBounds(0, 0, iconSize, iconSize)
+        spannable.setSpan(
+            ImageSpan(iconDrawable!!, ImageSpan.ALIGN_BOTTOM),
+            iconIndex,
+            iconIndex + 1,
+            SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE,
+        )
+    }
+    return SpannedString(spannable)
+}
+
+fun SpannedString.color(
+    targetWord: String,
+    color: Int,
+    fullText: String,
+): SpannedString {
+    val spannable = SpannableString(this)
+    val startIndex = fullText.indexOf(targetWord)
+    if (startIndex != -1) {
+        spannable.setSpan(
+            ForegroundColorSpan(color),
+            startIndex,
+            startIndex + targetWord.length,
+            SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE,
+        )
+    }
+    return SpannedString(spannable)
+}
+
+fun SpannedString.style(
+    targetWord: String,
+    fullText: String,
+): SpannedString {
+    val spannable = SpannableString(this)
+    val startIndex = fullText.indexOf(targetWord)
+    if (startIndex != -1) {
+        spannable.setSpan(
+            StyleSpan(Typeface.BOLD),
+            startIndex,
+            startIndex + targetWord.length,
+            SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE,
+        )
+    }
+    return SpannedString(spannable)
 }
