46c5c5b02b303b631079179fd8985e6ccda28a47
sh1mj1 <7wlgns@gmail.com>
2025-04-17 17:16:39 +0900

    test(LocaleInterceptor): enhance tests for Accept-Language header with parameterized cases

--- android/remote/src/test/java/poke/rogue/helper/remote/interceptor/LocaleInterceptorTest.kt
+++ android/remote/src/test/java/poke/rogue/helper/remote/interceptor/LocaleInterceptorTest.kt
@@ -1,6 +1,10 @@
 package poke.rogue.helper.remote.interceptor
 
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.data.forAll
+import io.kotest.data.headers
+import io.kotest.data.row
+import io.kotest.data.table
 import io.kotest.matchers.shouldBe
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -20,29 +24,34 @@ class LocaleInterceptorTest : BehaviorSpec({
         mockWebServer.shutdown()
     }
 
-    Given("디바이스가 언어는 한국어로, 지역은 한국으로 설정되어 있다.") {
-        val locale = Locale("ko", "KR")
-        Locale.setDefault(locale)
+    forAll(
+        table(
+            headers("description", "testLocale", "expectedHeader"),
+            row("ko-KR", Locale("ko", "KR"), "ko-KR"),
+            row("en-US", Locale("en", "US"), "en-US"),
+        ),
+    ) { description, testLocale, expectedHeader ->
+        Given("디바이스 로케일이 $description 일 때") {
+            Locale.setDefault(testLocale)
 
-        val client =
-            OkHttpClient.Builder()
-                .addInterceptor(LocaleInterceptor())
-                .build()
+            val client =
+                OkHttpClient.Builder()
+                    .addInterceptor(LocaleInterceptor())
+                    .build()
+            val request =
+                Request.Builder()
+                    .url(mockWebServer.url("/test"))
+                    .build()
+            mockWebServer.enqueue(MockResponse().setResponseCode(200))
 
-        val request =
-            Request.Builder()
-                .url(mockWebServer.url("/test"))
-                .build()
+            When("api 요청이 불릴 때") {
+                client.newCall(request).execute()
 
-        mockWebServer.enqueue(MockResponse().setResponseCode(200))
-
-        When("요청이 불릴 때") {
-            client.newCall(request).execute()
-
-            Then("헤더에 들어가는 Accept-Language 는 ko-KR 이다.") {
-                val recordedRequest = mockWebServer.takeRequest()
-                val headerValue = recordedRequest.getHeader("Accept-Language")
-                headerValue shouldBe "ko-KR"
+                Then("헤더에 들어가는 Accept-Language 는 $expectedHeader 이다.") {
+                    val recordedRequest = mockWebServer.takeRequest()
+                    val headerValue = recordedRequest.getHeader("Accept-Language")
+                    headerValue shouldBe expectedHeader
+                }
             }
         }
     }
