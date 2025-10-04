c12a547a3d4e39e989cc4337e78a830325b4ce39
unifolio0 <boho82ha@gmail.com>
2025-10-04 17:52:25 +0900

    refactor: aop를 활용한 로깅 일관성

--- backend/pokerogue/src/main/java/com/pokerogue/helper/ability/controller/AbilityController.java
+++ backend/pokerogue/src/main/java/com/pokerogue/helper/ability/controller/AbilityController.java
@@ -6,12 +6,10 @@ import com.pokerogue.helper.ability.service.AbilityService;
 import com.pokerogue.helper.util.dto.ApiResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class AbilityController {
@@ -25,13 +23,6 @@ public class AbilityController {
 
     @GetMapping("/api/v1/ability2/{id}")
     public ApiResponse<AbilityDetailResponse> abilityDetails(@PathVariable("id") String id) {
-        log.info(
-                "---- URI : {}, Param : {}, ThreadName : {}",
-                "/api/v1/ability/{id}",
-                id,
-                Thread.currentThread().getName()
-        );
-
         return new ApiResponse<>("특성 정보 불러오기에 성공했습니다.", abilityService.findAbilityDetails(id));
     }
 }
